bfb2f84857a3d88f5c3aa1f7e88f15e733c700e9
Dan Lorenc <lorenc.d@gmail.com>
2016-04-26 11:00:07 -0700

    Add more testing of cluster.go.
    Refactor a bit to make testing easier.

--- cli/cluster/cluster_test.go
+++ cli/cluster/cluster_test.go
@@ -1,8 +1,10 @@
 package cluster
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/docker/machine/libmachine/host"
 	"github.com/docker/machine/libmachine/state"
 	"github.com/kubernetes/minikube/cli/constants"
 	"github.com/kubernetes/minikube/cli/tests"
@@ -35,5 +37,75 @@ func TestCreateHost(t *testing.T) {
 
 	if h.DriverName != "virtualbox" {
 		t.Fatalf("Wrong driver name: %v. Should be virtualbox.", h.DriverName)
+	}
+}
+
+type mockHost struct {
+	Commands []string
+}
+
+func (m mockHost) RunSSHCommand(cmd string) (string, error) {
+	m.Commands = append(m.Commands, cmd)
+	return "", nil
+}
+
+func TestStartCluster(t *testing.T) {
+	h := mockHost{}
+	err := StartCluster(h)
+	if err != nil {
+		t.Fatalf("Error starting cluster: %s", err)
+	}
+}
+
+type mockHostError struct{}
+
+func (m mockHostError) RunSSHCommand(cmd string) (string, error) {
+	return "", fmt.Errorf("Error calling command: %s", cmd)
+}
+
+func TestStartClusterError(t *testing.T) {
+	h := mockHostError{}
+	err := StartCluster(h)
+	if err == nil {
+		t.Fatal("Error not thrown starting cluster.")
+	}
+}
+
+func TestStartHostExists(t *testing.T) {
+	api := &tests.MockAPI{}
+	// Create an initial host.
+	_, err := createHost(api)
+	if err != nil {
+		t.Fatalf("Error creating host: %v", err)
+	}
+
+	// Make sure the next call to Create will fail, to assert it doesn't get called again.
+	api.CreateError = true
+	if err := api.Create(&host.Host{}); err == nil {
+		t.Fatal("api.Create did not fail, but should have.")
+	}
+
+	// This should pass without calling Create because the host exists already.
+	h, err := StartHost(api)
+	if err != nil {
+		t.Fatal("Error starting host.")
+	}
+	if h.Name != constants.MachineName {
+		t.Fatalf("Machine created with incorrect name: %s", h.Name)
+	}
+}
+
+func TestStartHost(t *testing.T) {
+	api := &tests.MockAPI{}
+
+	h, err := StartHost(api)
+	if err != nil {
+		t.Fatal("Error starting host.")
+	}
+	if h.Name != constants.MachineName {
+		t.Fatalf("Machine created with incorrect name: %s", h.Name)
+	}
+	if exists, _ := api.Exists(h.Name); !exists {
+		t.Fatal("Machine not saved.")
 	}
 }
