b1ebe65c1d2245bd05418f24a7b38b2fc5d08c19
Dan Gillespie <dan@redspread.com>
2016-05-13 09:49:06 -0700

    added kubeconfig writing and reading

---
+++ pkg/minikube/kubeconfig/config.go
@@ -0,0 +1,83 @@
+package kubeconfig
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api"
+	"k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api/latest"
+	"k8s.io/kubernetes/pkg/runtime"
+)
+
+// ReadConfig retrieves Kubernetes client configuration from a file.
+func ReadConfig(filename string) (*api.Config, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("could not read config: %v", err)
+	}
+
+	// decode config, empty if no bytes
+	config, err := decode(data)
+	if err != nil {
+		return nil, fmt.Errorf("could not read config: %v", err)
+	}
+
+	// initialize nil maps
+	if config.AuthInfos == nil {
+		config.AuthInfos = map[string]*api.AuthInfo{}
+	}
+	if config.Clusters == nil {
+		config.Clusters = map[string]*api.Cluster{}
+	}
+	if config.Contexts == nil {
+		config.Contexts = map[string]*api.Context{}
+	}
+
+	return config, nil
+}
+
+// WriteConfig encodes the configuration and writes it to the given file.
+// If the file exists, it's contents will be overwritten.
+func WriteConfig(config *api.Config, filename string) error {
+	if config == nil {
+		fmt.Errorf("could not write to '%s': config can't be nil", filename)
+	}
+
+	// encode config to YAML
+	data, err := runtime.Encode(latest.Codec, config)
+	if err != nil {
+		return fmt.Errorf("could not write to '%s': failed to encode config: %v", filename, err)
+	}
+
+	// create parent dir if doesn't exist
+	dir := filepath.Dir(filename)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err = os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+	}
+
+	// write with restricted permissions
+	if err := ioutil.WriteFile(filename, data, 0600); err != nil {
+		return err
+	}
+	return nil
+}
+
+// decode reads a Config object from bytes.
+// Returns empty config if no bytes.
+func decode(data []byte) (*api.Config, error) {
+	// if no data, return empty config
+	if len(data) == 0 {
+		return api.NewConfig(), nil
+	}
+
+	config, _, err := latest.Codec.Decode(data, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return config.(*api.Config), nil
+}
