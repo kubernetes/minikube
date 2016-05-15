e0c13f9ce4e6432b3be06fda0f7bcec80e2a22de
Dan Lorenc <lorenc.d@gmail.com>
2016-05-14 19:03:04 -0700

    Add ethtool to the built image.

--- iso/build.sh
+++ iso/build.sh
@@ -16,6 +16,11 @@ docker build -t socat -f Dockerfile.socat .
 docker run socat cat socat > $tmpdir/socat
 chmod +x $tmpdir/socat
 
+# Get ethotool
+docker build -t ethtool -f Dockerfile.ethtool .
+docker run ethtool cat ethtool > $tmpdir/ethtool
+chmod +x $tmpdir/ethtool
+
 # Do the build.
 docker build -t iso .
 
