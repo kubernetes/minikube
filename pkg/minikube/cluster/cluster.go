5397d9efd5d854de7c2e37888875fa751dc5db93
abrand <abrand@apprenda.com>
2016-08-01 19:18:12 -0400

    Add ability to specify CIDR for Virtualbox minikube

--- pkg/minikube/cluster/cluster.go
+++ pkg/minikube/cluster/cluster.go
@@ -178,6 +178,7 @@ type MachineConfig struct {
 	VMDriver         string
 	DockerEnv        []string // Each entry is formatted as KEY=VALUE.
 	InsecureRegistry []string
+	HostOnlyCIDR     string // Only used by the virtualbox driver
 }
 
 // KubernetesConfig contains the parameters used to configure the VM Kubernetes.
@@ -354,6 +355,7 @@ func createVirtualboxHost(config MachineConfig) drivers.Driver {
 	d.Memory = config.Memory
 	d.CPU = config.CPUs
 	d.DiskSize = int(config.DiskSize)
+	d.HostOnlyCIDR = config.HostOnlyCIDR
 	return d
 }
 
