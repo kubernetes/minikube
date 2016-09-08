d24dfd4c967b4791d6de38c9877080f67b915990
Sergiusz Urbaniak <sur@coreos.com>
2016-09-08 13:34:36 +0200

    cni: add missing plugings

--- package/cni-bin/cni-bin.mk
+++ package/cni-bin/cni-bin.mk
@@ -11,7 +11,27 @@ CNI_BIN_SOURCE = cni-v$(CNI_BIN_VERSION).tgz
 define CNI_BIN_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 \
 		$(@D)/bridge \
-		$(TARGET_DIR)/bin/bridge
+		$(TARGET_DIR)/opt/cni/bin/bridge
+
+	ln -sf \
+		../../opt/cni/bin/bridge \
+		$(TARGET_DIR)/usr/bin/bridge
+
+	$(INSTALL) -D -m 0755 \
+		$(@D)/host-local \
+		$(TARGET_DIR)/opt/cni/bin/host-local
+
+	ln -sf \
+		../../opt/cni/bin/host-local \
+		$(TARGET_DIR)/usr/bin/host-local
+
+	$(INSTALL) -D -m 0755 \
+		$(@D)/loopback \
+		$(TARGET_DIR)/opt/cni/bin/loopback
+
+	ln -sf \
+		../../opt/cni/bin/loopback \
+		$(TARGET_DIR)/usr/bin/loopback
 endef
 
 $(eval $(generic-package))
