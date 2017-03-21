b47f85ef792b2dc6741f763b0155788202eb64ee
Matt Rickard <mrick@google.com>
2017-03-21 10:06:11 -0700

    Add build date to localkube

--- hack/get_k8s_version.py
+++ hack/get_k8s_version.py
@@ -19,6 +19,7 @@
 import json
 import subprocess
 import sys
+from datetime import datetime
 
 K8S_PACKAGE = 'k8s.io/kubernetes/'
 X_ARG_BASE = '-X k8s.io/minikube/vendor/k8s.io/kubernetes/pkg/version.'
@@ -44,10 +45,13 @@ def get_tree_state():
     result = 'clean'
   return 'gitTreeState=%s' % result
 
+def get_build_date():
+  return 'buildDate=%s' % datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+
 def main():
   if len(sys.argv) > 1 and sys.argv[1] == "--k8s-version-only":
       return get_from_godep('Comment')
-  args = [get_rev(), get_version(), get_tree_state()]
+  args = [get_rev(), get_version(), get_tree_state(), get_build_date()]
   return ' '.join([X_ARG_BASE + arg for arg in args])
 
 if __name__ == '__main__':
