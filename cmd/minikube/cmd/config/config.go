1346d99699e1c172d8cb4f091e865b4add313fce
dlorenc <dlorenc@google.com>
2017-03-27 14:37:25 -0700

    Update the addon manager.
    This gets us closer to being able to disable the storage provisioner.

--- cmd/minikube/cmd/config/config.go
+++ cmd/minikube/cmd/config/config.go
@@ -128,6 +128,12 @@ var settings = []Setting{
 		callbacks:   []setFn{EnableOrDisableAddon},
 	},
 	{
+		name:        "default-storageclass",
+		set:         SetBool,
+		validations: []setFn{IsValidAddon},
+		callbacks:   []setFn{EnableOrDisableAddon},
+	},
+	{
 		name:        "kube-dns",
 		set:         SetBool,
 		validations: []setFn{IsValidAddon},
