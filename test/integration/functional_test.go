5d66ac22f038447cb3f8843e168e51f3f83f8166
Nick Kubala <nkubala@google.com>
2017-05-30 13:23:24 -0700

    use go build tag over make flag

--- test/integration/functional_test.go
+++ test/integration/functional_test.go
@@ -19,56 +19,26 @@ limitations under the License.
 package integration
 
 import (
-	"fmt"
 	"runtime"
 	"strings"
 	"testing"
 
-	"k8s.io/minikube/pkg/minikube/constants"
-	"k8s.io/minikube/pkg/minikube/kubernetes_versions"
 	"k8s.io/minikube/test/integration/util"
 )
 
-func RunVersionedFunctional(t *testing.T, minikubeRunner util.MinikubeRunner) {
+func TestFunctional(t *testing.T) {
+	minikubeRunner := util.MinikubeRunner{
+		BinaryPath: *binaryPath,
+		Args:       *args,
+		T:          t}
 	minikubeRunner.EnsureRunning()
 
 	// This one is not parallel, and ensures the cluster comes up
 	// before we run any other tests.
 	t.Run("Status", testClusterStatus)
 	t.Run("DNS", testClusterDNS)
-	t.Run("Addons", testAddons)
-}
-
-func TestFunctional(t *testing.T) {
-	var minikubeRunner util.MinikubeRunner
-
-	if *versioned {
-		t.Logf("Running versioned integration tests")
-		k8sVersions, err := kubernetes_versions.GetK8sVersionsFromURL(constants.KubernetesVersionGCSURL)
-		if err != nil {
-			t.Fatalf(err.Error())
-		}
-		for _, version := range k8sVersions {
-			vArgs := fmt.Sprintf("%s --kubernetes-version %s", *args, version.Version)
-			minikubeRunner = util.MinikubeRunner{
-				BinaryPath: *binaryPath,
-				Args:       vArgs,
-				T:          t}
-			RunVersionedFunctional(t, minikubeRunner)
-		}
-	}
-
-	minikubeRunner = util.MinikubeRunner{
-		BinaryPath: *binaryPath,
-		Args:       *args,
-		T:          t}
-
-	RunVersionedFunctional(t, minikubeRunner)
-
-	t.Run("EnvVars", testClusterEnv)
 	t.Run("Logs", testClusterLogs)
-	t.Run("SSH", testClusterSSH)
-	t.Run("Systemd", testVMSystemd)
+	t.Run("Addons", testAddons)
 	t.Run("Dashboard", testDashboard)
 	t.Run("ServicesList", testServicesList)
 	t.Run("Provisioning", testProvisioning)
