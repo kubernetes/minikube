9988223777d4794fbbeb1671efcc59064b123f4a
Abby Tisdale <atisdale@atisdale.sfo.corp.google.com>
2017-06-06 16:20:21 -0700

    Added update-context and kubeconfig to status.

--- cmd/minikube/cmd/status.go
+++ cmd/minikube/cmd/status.go
@@ -27,14 +27,16 @@ import (
 	cmdUtil "k8s.io/minikube/cmd/util"
 	"k8s.io/minikube/pkg/minikube/cluster"
 	"k8s.io/minikube/pkg/minikube/constants"
+	kcfg "k8s.io/minikube/pkg/minikube/kubeconfig"
 	"k8s.io/minikube/pkg/minikube/machine"
 )
 
 var statusFormat string
 
 type Status struct {
-	MinikubeStatus  string
-	LocalkubeStatus string
+	MinikubeStatus   string
+	LocalkubeStatus  string
+	KubeconfigStatus string
 }
 
 // statusCmd represents the status command
@@ -57,14 +59,32 @@ var statusCmd = &cobra.Command{
 		}
 
 		ls := state.None.String()
+		ks := state.None.String()
 		if ms == state.Running.String() {
 			ls, err = cluster.GetLocalkubeStatus(api)
 			if err != nil {
 				glog.Errorln("Error localkube status:", err)
 				cmdUtil.MaybeReportErrorAndExit(err)
 			}
+			ip, err := cluster.GetHostDriverIP(api)
+			if err != nil {
+				glog.Errorln("Error host driver ip status:", err)
+				cmdUtil.MaybeReportErrorAndExit(err)
+			}
+			kstatus, err := kcfg.GetKubeConfigStatus(ip, constants.KubeconfigPath)
+			if err != nil {
+				glog.Errorln("Error kubeconfig status:", err)
+				cmdUtil.MaybeReportErrorAndExit(err)
+			}
+			if kstatus {
+				ks = "Correctly Configured: pointing to minikube-vm at " + ip.String()
+			} else {
+				ks = "Misconfigured: pointing to stale minikube-vm." +
+					"\nTo fix the kubectl context, run minikube update-context"
+			}
 		}
-		status := Status{ms, ls}
+
+		status := Status{ms, ls, ks}
 
 		tmpl, err := template.New("status").Parse(statusFormat)
 		if err != nil {
