f711504f6b6b09f1ae610d2f39ba81d8727ab4d7
Jakub Gocławski <jakub@goclawski.net>
2017-06-28 14:33:25 +0200

    Upgrade Docker to 1.12.6.

--- deploy/iso/minikube-iso/package/docker-bin/docker-bin.mk
+++ deploy/iso/minikube-iso/package/docker-bin/docker-bin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_BIN_VERSION = 1.11.1
+DOCKER_BIN_VERSION = 1.12.6
 DOCKER_BIN_SITE = https://get.docker.com/builds/Linux/x86_64
 DOCKER_BIN_SOURCE = docker-$(DOCKER_BIN_VERSION).tgz
 
@@ -32,6 +32,14 @@ define DOCKER_BIN_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 \
 		$(@D)/docker-containerd-ctr \
 		$(TARGET_DIR)/bin/docker-containerd-ctr
+
+	$(INSTALL) -D -m 0755 \
+		$(@D)/dockerd \
+		$(TARGET_DIR)/bin/dockerd
+
+	$(INSTALL) -D -m 0755 \
+		$(@D)/docker-proxy \
+		$(TARGET_DIR)/bin/docker-proxy
 endef
 
 define DOCKER_BIN_INSTALL_INIT_SYSTEMD
