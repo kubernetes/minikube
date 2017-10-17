09e736ac91885b07a5132e345f069a7078a9149b
dlorenc <dlorenc@google.com>
2017-10-17 10:00:03 -0700

    Fix non-systemd none driver.

--- pkg/minikube/bootstrapper/localkube/commands.go
+++ pkg/minikube/bootstrapper/localkube/commands.go
@@ -34,7 +34,7 @@ var localkubeStartCmdTemplate = "/usr/local/bin/localkube {{.Flags}} --generate-
 
 var startCommandNoSystemdTemplate = `
 # Run with nohup so it stays up. Redirect logs to useful places.
-sudo sh -c 'PATH=/usr/local/sbin:$PATH nohup GODEBUG=netdns=go {{.LocalkubeStartCmd}} > {{.Stdout}} 2> {{.Stderr}} < /dev/null & echo $! > {{.Pidfile}} &'
+sudo sh -c 'PATH=/usr/local/sbin:$PATH GODEBUG=netdns=go nohup {{.LocalkubeStartCmd}} > {{.Stdout}} 2> {{.Stderr}} < /dev/null & echo $! > {{.Pidfile}} &'
 `
 
 var localkubeSystemdTmpl = `[Unit]
