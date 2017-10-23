99d9f73e589bc559439be2edbc855b71b94954a5
Matt Rickard <mrick@google.com>
2017-10-23 15:21:52 -0700

    Transfer ~/.minikube/files last
    To allow users to overwrite some of the files that we download
    (kubelet, kubeadm, etc.)

--- pkg/minikube/bootstrapper/kubeadm/kubeadm.go
+++ pkg/minikube/bootstrapper/kubeadm/kubeadm.go
@@ -261,15 +261,6 @@ func (k *KubeadmBootstrapper) UpdateCluster(cfg bootstrapper.KubernetesConfig) e
 		assets.NewMemoryAssetTarget([]byte(kubeadmCfg), constants.KubeadmConfigFile, "0640"),
 	}
 
-	if err := addAddons(&files); err != nil {
-		return errors.Wrap(err, "adding addons to copyable files")
-	}
-
-	for _, f := range files {
-		if err := k.c.Copy(f); err != nil {
-			return errors.Wrapf(err, "transferring kubeadm file: %+v", f)
-		}
-	}
 	var g errgroup.Group
 	for _, bin := range []string{"kubelet", "kubeadm"} {
 		bin := bin
@@ -290,6 +281,16 @@ func (k *KubeadmBootstrapper) UpdateCluster(cfg bootstrapper.KubernetesConfig) e
 	}
 	if err := g.Wait(); err != nil {
 		return errors.Wrap(err, "downloading binaries")
+	}
+
+	if err := addAddons(&files); err != nil {
+		return errors.Wrap(err, "adding addons to copyable files")
+	}
+
+	for _, f := range files {
+		if err := k.c.Copy(f); err != nil {
+			return errors.Wrapf(err, "transferring kubeadm file: %+v", f)
+		}
 	}
 
 	err = k.c.Run(`
