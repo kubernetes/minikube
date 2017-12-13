18d67d07baa42df244eff94389afca6559a07f87
kairen <kyle.b@inwinstack.com>
2017-12-14 00:05:56 +0800

    Fix the image cache delete

--- pkg/minikube/machine/cache_images.go
+++ pkg/minikube/machine/cache_images.go
@@ -17,13 +17,14 @@ limitations under the License.
 package machine
 
 import (
-	"golang.org/x/sync/errgroup"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
+
+	"golang.org/x/sync/errgroup"
 
 	"k8s.io/minikube/pkg/minikube/assets"
 	"k8s.io/minikube/pkg/minikube/bootstrapper"
@@ -223,6 +224,7 @@ func LoadFromCacheBlocking(cmd bootstrapper.CommandRunner, src string) error {
 func DeleteFromImageCacheDir(images []string) error {
 	for _, image := range images {
 		path := filepath.Join(constants.ImageCacheDir, image)
+		path = sanitizeCacheDir(path)
 		glog.Infoln("Deleting image in cache at ", path)
 		if err := os.Remove(path); err != nil {
 			return err
