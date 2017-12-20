697b3fbae1883ac99f35a66391e6bedbb9d5a98a
dlorenc <lorenc.d@gmail.com>
2017-12-20 08:40:46 -0800

    Remove from set_pending.

--- hack/jenkins/linux_integration_tests_none.sh
+++ hack/jenkins/linux_integration_tests_none.sh
@@ -29,7 +29,7 @@ set -e
 OS_ARCH="linux-amd64"
 VM_DRIVER="none"
 JOB_NAME="Linux-None"
-EXTRA_ARGS="--bootstrapper=kubeadm"
+EXTRA_ARGS="--bootstrapper=localkube"
 EXTRA_START_ARGS="--kubernetes-version=file://$PWD/out/localkube"
 
 SUDO_PREFIX="sudo -E "
