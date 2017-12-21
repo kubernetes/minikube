eb3e6a63b4312855bf333270647cc32cc380a279
Matt Rickard <mrick@google.com>
2017-12-21 13:28:55 -0800

    Switch none test to use kubeadm

--- hack/jenkins/linux_integration_tests_none.sh
+++ hack/jenkins/linux_integration_tests_none.sh
@@ -29,8 +29,7 @@ set -e
 OS_ARCH="linux-amd64"
 VM_DRIVER="none"
 JOB_NAME="Linux-None"
-EXTRA_ARGS="--bootstrapper=localkube"
-EXTRA_START_ARGS="--kubernetes-version=file://$PWD/out/localkube"
+EXTRA_ARGS="--bootstrapper=kubeadm"
 
 SUDO_PREFIX="sudo -E "
 export KUBECONFIG="/root/.kube/config"
