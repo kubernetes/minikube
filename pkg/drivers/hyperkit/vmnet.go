e3f3727b4dd04cedaff7ff7dbe13db38fa3f2e12
dlorenc <dlorenc@google.com>
2017-12-27 13:00:07 -0800

    Move GetNetAddr.
    The build constraints in vmnet.go cause issues.

--- pkg/drivers/hyperkit/vmnet.go
+++ pkg/drivers/hyperkit/vmnet.go
@@ -19,37 +19,9 @@ limitations under the License.
 package hyperkit
 
 import (
-	"fmt"
-	"net"
-	"os"
-	"os/exec"
-	"strings"
-
 	vmnet "github.com/zchee/go-vmnet"
-)
-
-const (
-	CONFIG_PLIST = "/Library/Preferences/SystemConfiguration/com.apple.vmnet"
-	NET_ADDR_KEY = "Shared_Net_Address"
 )
 
 func GetMACAddressFromUUID(UUID string) (string, error) {
 	return vmnet.GetMACAddressFromUUID(UUID)
-}
-
-func GetNetAddr() (net.IP, error) {
-	_, err := os.Stat(CONFIG_PLIST + ".plist")
-	if err != nil {
-		return nil, fmt.Errorf("Does not exist %s", CONFIG_PLIST+".plist")
-	}
-
-	out, err := exec.Command("defaults", "read", CONFIG_PLIST, NET_ADDR_KEY).Output()
-	if err != nil {
-		return nil, err
-	}
-	ip := net.ParseIP(strings.TrimSpace(string(out)))
-	if ip == nil {
-		return nil, fmt.Errorf("Could not get the network address for vmnet")
-	}
-	return ip, nil
 }
--- pkg/drivers/hyperkit/vmnet_stub.go
+++ pkg/drivers/hyperkit/vmnet_stub.go
@@ -18,7 +18,9 @@ limitations under the License.
 
 package hyperkit
 
-import "errors"
+import (
+	"errors"
+)
 
 func GetMACAddressFromUUID(UUID string) (string, error) {
 	return "", errors.New("Function not supported on CGO_ENABLED=0 binaries")
