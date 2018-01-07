f4628053c572e9b2ba84079c586e4520b6032c0c
Anders F Björklund <anders.f.bjorklund@gmail.com>
2018-01-07 19:45:42 +0100

    Add crictl from cri-tools, to minikube.iso
    We don't need critest, because it requires go anyway.
    But kubeadm insists on crictl being available in path.

---
+++ deploy/iso/minikube-iso/package/cri-tools/cri-tools.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# cri-tools
+#
+################################################################################
+
+CRI_TOOLS_VERSION = v1.0.0-beta.0
+CRI_TOOLS_SITE = https://github.com/kubernetes-incubator/cri-tools/archive
+CRI_TOOLS_SOURCE = $(CRI_TOOLS_VERSION).tar.gz
+CRI_TOOLS_LICENSE = Apache-2.0
+CRI_TOOLS_LICENSE_FILES = LICENSE
+CRI_TOOLS_DEPENDENCIES =
+CRI_TOOLS_GOPATH = $(@D)/_output
+CRI_TOOLS_ENV = \
+	CGO_ENABLED=1 \
+	GOPATH="$(CRI_TOOLS_GOPATH)" \
+	PATH=$(CRI_TOOLS_GOPATH)/bin:$(BR_PATH)
+
+
+define CRI_TOOLS_CONFIGURE_CMDS
+	mkdir -p $(CRI_TOOLS_GOPATH)/src/github.com/kubernetes-incubator
+	ln -sf $(@D) $(CRI_TOOLS_GOPATH)/src/github.com/kubernetes-incubator/cri-tools
+endef
+
+define CRI_TOOLS_BUILD_CMDS
+	$(CRI_TOOLS_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) crictl
+endef
+
+define CRI_TOOLS_INSTALL_TARGET_CMDS
+	$(INSTALL) -Dm755 \
+		$(CRI_TOOLS_GOPATH)/bin/crictl \
+		$(TARGET_DIR)/usr/bin/crictl
+endef
+
+$(eval $(generic-package))
