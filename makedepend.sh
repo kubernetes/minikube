c6fa0be721d56ba29733a27c15a4810018032295
Anders F Björklund <anders.f.bjorklund@gmail.com>
2018-01-20 14:03:41 +0100

    Generate make depend files from go list
    Instead of running `go list` every time (very slow),
    cache the results in the build folder and include it.
    
    Add a shell helper script, to make the Makefile more
    readable. Use regular "makedepend" syntax for output.

---
+++ makedepend.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Generate go dependencies, for make. Uses `go list".
+# Usage: makedepend.sh [-t] output package path [extra]
+
+PATH_FORMAT='{{join .Deps "\n"}}'
+FILE_FORMAT='{{ range $file := .GoFiles }} {{$.Dir}}/{{$file}}{{"\n"}}{{end}}'
+
+if [ "$1" = "-t" ]
+then
+  PATH_FORMAT='{{ if .TestGoFiles }} {{.ImportPath}} {{end}}'
+  shift
+fi
+
+out=$1
+pkg=$2
+path=$3
+extra=$4
+
+# check for mandatory parameters
+test -n "$out$pkg$path" || exit 1
+
+echo "$out: $extra\\"
+go list -f "$PATH_FORMAT" $path |
+  grep "$pkg" |
+  xargs go list -f "$FILE_FORMAT" |
+  sed -e "s|^ ${GOPATH}| \$(GOPATH)|;s/$/ \\\\/"
+echo " #"
