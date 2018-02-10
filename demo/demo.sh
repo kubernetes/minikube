08d420f1daa16839eee73e37bf72587b6c6e680d
pbitty <pbitty@gmail.com>
2018-02-10 11:48:34 -0500

    Add demo code

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,55 +1,74 @@
 #!/usr/bin/env bash
+set -e
 
-. demo-magic.sh -n
+cd "$(dirname $0)" || exit
+
+source demo-magic.sh -n
 
 TYPE_SPEED=15
-DEMO_PROMPT="${GREEN}➜ ${CYAN}\W $ "
+DEMO_PROMPT="${CYAN}$ "
 
 p "# In this demo I will start up a 4-node minikube cluster."
 p "# Three workers and one server"
 p ""
 
 p "# Start minikube master ..."
-pe "out/minikube start"
+pe "./minikube start"
 
-pe "out/minikube nodes list"
+p ""
+pe "./minikube node list"
 
+p ""
 p "# Add some nodes ..."
-pe "out/minikube node add"
-pe "out/minikube node add"
-pe "out/minikube node add"
+pe "./minikube node add"
+pe "./minikube node add"
+pe "./minikube node add"
 
-pe "out/minikube nodes list"
+p ""
+pe "./minikube node list"
 
-
+p ""
 p "# Start nodes ..."
-pe "out/minikube node start"
+pe "./minikube node start"
 
-pe "out/minikube nodes list"
+p ""
+pe "./minikube node list"
 
-PROMPT_TIMEOUT=10
-wait
+sleep 10
 
+p ""
 pe "kubectl get nodes"
 
+p ""
 p "# Installing Pod network ..."
 pe "kubectl apply -f kube-flannel.yml"
 
+p ""
 p "# Wait for flannel to start ..."
 pe "kubectl --namespace=kube-system rollout status ds/kube-flannel-ds"
 
+p ""
 p "# Deploy our pods ..."
-pe "cat hello-from.yml"
-pe "kubectl apply -f hello-from.yml"
+pe "cat hello-deployment.yml"
+pe "kubectl apply -f hello-deployment.yml"
 
-pe "kubectl rollout status deployment/hello-from"
+p ""
+pe "kubectl rollout status deployment/hello"
 
+p ""
+p "# Deploy our service ..."
+pe "cat hello-svc.yml"
+pe "kubectl apply -f hello-svc.yml"
+
+p ""
 pe "# Note Pod IPs ..."
 pe "kubectl get pod -o wide"
 
-pe "out/minikube service list"
+p ""
+pe "./minikube service list"
 
-ip=$(out/minikube ip)
-pe "for i in \$(seq 1 10); do curl http://$ip:31000; echo; done"
+p ""
+ip=$(./minikube ip)
+pe "for i in \$(seq 1 10); do curl http://$ip:31000; echo; sleep 0.4; done"
 
 p "# Yay"
