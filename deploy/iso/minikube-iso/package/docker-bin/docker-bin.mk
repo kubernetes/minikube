9dec86b1a7053267209081d55284134fa7e2da27
Anders F Björklund <anders.f.bjorklund@gmail.com>
2018-03-11 18:45:07 +0100

    Upgrade Docker, from 17.09.0-ce to 17.12.1-ce

--- deploy/iso/minikube-iso/package/docker-bin/docker-bin.mk
+++ deploy/iso/minikube-iso/package/docker-bin/docker-bin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_BIN_VERSION = 17.09.0-ce
+DOCKER_BIN_VERSION = 17.12.1-ce
 DOCKER_BIN_SITE = https://download.docker.com/linux/static/stable/x86_64
 DOCKER_BIN_SOURCE = docker-$(DOCKER_BIN_VERSION).tgz
 
