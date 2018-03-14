56ff7ae22bcd606ef5565048a0ecaa6b1fc930fd
Fabio Rapposelli <fabio@vmware.com>
2018-03-14 17:28:15 +0100

    Add VMware unified driver to supported list

---
+++ pkg/minikube/drivers/vmware/driver.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2018 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package vmware
+
+import (
+	vmwcfg "github.com/machine-drivers/docker-machine-driver-vmware/pkg/drivers/vmware/config"
+	cfg "k8s.io/minikube/pkg/minikube/config"
+	"k8s.io/minikube/pkg/minikube/constants"
+	"k8s.io/minikube/pkg/minikube/registry"
+)
+
+func init() {
+	registry.Register(registry.DriverDef{
+		Name:          "vmware",
+		Builtin:       false,
+		ConfigCreator: createVMwareHost,
+	})
+}
+
+func createVMwareHost(config cfg.MachineConfig) interface{} {
+	d := vmwcfg.NewConfig(cfg.GetMachineName(), constants.GetMinipath())
+	d.Boot2DockerURL = config.Downloader.GetISOFileURI(config.MinikubeISO)
+	d.Memory = config.Memory
+	d.CPU = config.CPUs
+	d.DiskSize = config.DiskSize
+
+	// TODO(frapposelli): push these defaults upstream to fixup this driver
+	d.SSHPort = 22
+	d.ISO = d.ResolveStorePath("boot2docker.iso")
+	return d
+}
