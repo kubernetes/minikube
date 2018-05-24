ee73cbde6bfdbdf2af9dbaf2a650b511f7af83d7
seborama <seb.d.albion@outlook.com>
2018-05-24 22:59:54 +0100

    Added hyperkit options for enterprise VPN support
    The purpose of these changes is to enhance Hyperkit support from the
    minikube command line for better integration with enterprise networks
    behind a VPN.
    
    uuid: Provide VM UUID to restore MAC address (only supported with
          Hyperkit driver).
    vpnkitSock: Location of the VPNKit socket used for networking. If empty,
                disables Hyperkit VPNKitSock, if 'auto' uses Docker for Mac
                VPNKit connection, otherwise uses the specified VSock."
    vsockPorts: List of guest VSock ports that should be exposed as sockets
                on the host (Only supported on with hyperkit now).
    
    Note:
    tests pass but file:
    `vendor/github.com/google/certificate-transparency/go/x509/root_darwin.go`
    has to be edited to correct an issue - not committed since this is in
    the vendor directory.

--- pkg/minikube/config/types.go
+++ pkg/minikube/config/types.go
@@ -35,6 +35,8 @@ type MachineConfig struct {
 	CPUs                int
 	DiskSize            int
 	VMDriver            string
+	HyperkitVpnKitSock  string   // Only used by the Hyperkit driver
+	HyperkitVSockPorts  []string // Only used by the Hyperkit driver
 	XhyveDiskDriver     string   // Only used by the xhyve driver
 	DockerEnv           []string // Each entry is formatted as KEY=VALUE.
 	InsecureRegistry    []string
