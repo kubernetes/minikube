6655f5250e3b53f4a348afa3b791b4ff0bc2fe67
dlorenc <lorenc.d@gmail.com>
2018-06-05 08:57:28 -0700

    Switch all remaining e2e tests to kubeadm.

--- hack/jenkins/linux_integration_tests_virtualbox.sh
+++ hack/jenkins/linux_integration_tests_virtualbox.sh
@@ -28,8 +28,6 @@ set -e
 OS_ARCH="linux-amd64"
 VM_DRIVER="virtualbox"
 JOB_NAME="Linux-VirtualBox"
-EXTRA_ARGS="--bootstrapper=localkube"
-EXTRA_START_ARGS="--kubernetes-version=file://$PWD/out/localkube"
 
 # Download files and set permissions
 source common.sh
