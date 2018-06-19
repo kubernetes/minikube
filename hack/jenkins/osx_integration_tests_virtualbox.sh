97a9375f894aefddb11ff3b5bb5de279be27d3d9
Aaron Prindle <aprindle@google.com>
2018-06-19 11:49:21 -0700

    remove last localkube bootstrapper test reference

--- hack/jenkins/osx_integration_tests_virtualbox.sh
+++ hack/jenkins/osx_integration_tests_virtualbox.sh
@@ -21,15 +21,14 @@
 # MINIKUBE_LOCATION: GIT_COMMIT from upstream build.
 # COMMIT: Actual commit ID from upstream build
 # EXTRA_BUILD_ARGS (optional): Extra args to be passed into the minikube integrations tests
-# access_token: The Github API access token. Injected by the Jenkins credential provider. 
+# access_token: The Github API access token. Injected by the Jenkins credential provider.
 
 
 set -e
 OS_ARCH="darwin-amd64"
 VM_DRIVER="virtualbox"
 JOB_NAME="OSX-Virtualbox"
-EXTRA_ARGS="--bootstrapper=localkube"
-EXTRA_START_ARGS="--kubernetes-version=file://$PWD/out/localkube"
+EXTRA_ARGS="--bootstrapper=kubeadm"
 
 # Download files and set permissions
 source common.sh
