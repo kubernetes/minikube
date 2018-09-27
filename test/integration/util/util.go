31a0d778784983a03e70076721e38223fd4dc366
Thomas Stromberg <tstromberg@google.com>
2018-09-26 17:53:42 -0700

    Fix broken TestFunctional/EnvVars test when run under non-bash shell.
    Make future failures to this test easier to debug. Here's an example of
    the test failure I ran into:
    
        --- FAIL: TestFunctional/EnvVars (0.58s)
            cluster_env_test.go:36: SetEnvFromEnvCmdOutput: Error: No variables were parsed from docker-env output:
                set -gx DOCKER_TLS_VERIFY "1";
                set -gx DOCKER_HOST "tcp://192.168.39.199:2376";
                set -gx DOCKER_CERT_PATH "/usr/local/google/home/tstromberg/.minikube/certs";
                set -gx DOCKER_API_VERSION "1.35";
                # Run this command to configure your shell:
                # eval (minikube docker-env)

--- test/integration/util/util.go
+++ test/integration/util/util.go
@@ -21,7 +21,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
-	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
@@ -114,19 +113,14 @@ func (m *MinikubeRunner) EnsureRunning() {
 	m.CheckStatus("Running")
 }
 
-func (m *MinikubeRunner) SetEnvFromEnvCmdOutput(dockerEnvVars string) error {
+// ParseEnvCmdOutput parses the output of `env` (assumes bash)
+func (m *MinikubeRunner) ParseEnvCmdOutput(out string) map[string]string {
+	env := map[string]string{}
 	re := regexp.MustCompile(`(\w+?) ?= ?"?(.+?)"?\n`)
-	matches := re.FindAllStringSubmatch(dockerEnvVars, -1)
-	seenEnvVar := false
-	for _, m := range matches {
-		seenEnvVar = true
-		key, val := m[1], m[2]
-		os.Setenv(key, val)
+	for _, m := range re.FindAllStringSubmatch(out, -1) {
+		env[m[1]] = m[2]
 	}
-	if !seenEnvVar {
-		return fmt.Errorf("Error: No environment variables were found in docker-env command output: %s", dockerEnvVars)
-	}
-	return nil
+	return env
 }
 
 func (m *MinikubeRunner) GetStatus() string {
