f3912d4f72bbb64ae78f7dbc5e4c664f7e58490a
Thomas Stromberg <tstromberg@google.com>
2018-10-04 14:40:48 -0700

    Fix shell boilerplate comment.

--- hack/release_notes.sh
+++ hack/release_notes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright 2018 The Kubernetes Authors 
+#
+# Copyright 2018 The Kubernetes Authors All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 set -eux -o pipefail
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
