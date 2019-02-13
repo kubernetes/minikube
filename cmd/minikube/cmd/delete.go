e74db397833fbad8dd1738933c0e9f4587b7a59c
Thomas Stromberg <tstromberg@google.com>
2019-02-13 09:21:58 -0800

    Issue a poweroff request before deleting a VM. Avoids HyperV hang

--- cmd/minikube/cmd/delete.go
+++ cmd/minikube/cmd/delete.go
@@ -54,10 +54,13 @@ associated files.`,
 		cc, err := pkg_config.Load()
 		if err != nil && !os.IsNotExist(err) {
 			console.ErrLn("Error loading profile config: %v", err)
-		} else if err == nil {
+		}
+
+		// In the case of "none", we want to uninstall Kubernetes as there is no VM to delete
+		if err == nil && cc.MachineConfig.VMDriver == "none" {
 			kc := cc.KubernetesConfig
-			bsName := viper.GetString(cmdcfg.Bootstrapper) // Name ?
-			console.OutStyle("resetting", "Reverting Kubernetes %s using %s ...", kc.KubernetesVersion, bsName)
+			bsName := viper.GetString(cmdcfg.Bootstrapper)
+			console.OutStyle("resetting", "Uninstalling Kubernetes %s using %s ...", kc.KubernetesVersion, bsName)
 			clusterBootstrapper, err := GetClusterBootstrapper(api, viper.GetString(cmdcfg.Bootstrapper))
 			if err == nil {
 				if err = clusterBootstrapper.DeleteCluster(kc); err != nil {
@@ -66,7 +69,6 @@ associated files.`,
 			}
 		}
 
-		console.OutStyle("deleting-vm", "Deleting %q Kubernetes VM ...", profile)
 		if err = cluster.DeleteHost(api); err != nil {
 			switch err := errors.Cause(err).(type) {
 			case mcnerror.ErrHostDoesNotExist:
