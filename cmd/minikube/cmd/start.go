9b2fad571033d8fbfbe81864d4391433ba287c3c
Anders F Björklund <anders.f.bjorklund@gmail.com>
2019-03-02 19:42:28 +0100

    Show current version of the container runtime

--- cmd/minikube/cmd/start.go
+++ cmd/minikube/cmd/start.go
@@ -479,6 +479,10 @@ func configureRuntimes(h *host.Host, runner bootstrapper.CommandRunner) cruntime
 	if err != nil {
 		exit.WithError("Failed to enable container runtime", err)
 	}
+	version := cr.Version()
+	if version != "" {
+		console.OutStyle(cr.Name(), "Version of container runtime is %s", version)
+	}
 	return cr
 }
 
