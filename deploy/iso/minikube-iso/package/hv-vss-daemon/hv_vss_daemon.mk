c1f04b897f9e5e09cdef6be8f0305582e2c41a47
Zhongcheng Lao <Zhongcheng.Lao@microsoft.com>
2019-03-30 15:49:21 +0800

    Install Hyper-V VSS daemon
    Signed-off-by: Zhongcheng Lao <Zhongcheng.Lao@microsoft.com>

---
+++ deploy/iso/minikube-iso/package/hv-vss-daemon/hv_vss_daemon.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# hv-vss-daemon
+#
+################################################################################
+
+HV_VSS_DAEMON_VERSION = 4.15.1
+HV_VSS_DAEMON_SITE = https://www.kernel.org/pub/linux/kernel/v${HV_VSS_DAEMON_VERSION%%.*}.x
+HV_VSS_DAEMON_SOURCE = linux-$(HV_VSS_DAEMON_VERSION).tar.xz
+
+define HV_VSS_DAEMON_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/tools/hv/
+endef
+
+define HV_VSS_DAEMON_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 \
+		$(@D)/tools/hv/hv_vss_daemon \
+		$(TARGET_DIR)/usr/sbin/hv_vss_daemon
+endef
+
+define HV_VSS_DAEMON_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 \
+		$(BR2_EXTERNAL_MINIKUBE_PATH)/package/hv-vss-daemon/hv_vss_daemon.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/hv_vss_daemon.service
+
+	ln -fs /usr/lib/systemd/system/hv_vss_daemon.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/hv_vss_daemon.service
+endef
+
+$(eval $(generic-package))
