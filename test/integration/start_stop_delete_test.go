896d00b3c196873c24b8a70c043191e2f0f1de99
josedonizetti <jdbjunior@gmail.com>
2019-05-22 17:25:42 -0300

    Add integration test to flag --kvm-network

--- test/integration/start_stop_delete_test.go
+++ test/integration/start_stop_delete_test.go
@@ -38,6 +38,7 @@ func TestStartStop(t *testing.T) {
 		{"nocache_oldest", []string{
 			"--cache-images=false",
 			fmt.Sprintf("--kubernetes-version=%s", constants.OldestKubernetesVersion),
+			"--kvm-network=default",
 		}},
 		{"feature_gates_newest_cni", []string{
 			"--feature-gates",
