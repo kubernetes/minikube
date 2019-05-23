c7bace6903bb68f16f8b20833522224014bc6715
josedonizetti <jdbjunior@gmail.com>
2019-05-23 02:30:21 -0300

    Fix --kvm-network integration test

--- test/integration/start_stop_delete_test.go
+++ test/integration/start_stop_delete_test.go
@@ -38,7 +38,9 @@ func TestStartStop(t *testing.T) {
 		{"nocache_oldest", []string{
 			"--cache-images=false",
 			fmt.Sprintf("--kubernetes-version=%s", constants.OldestKubernetesVersion),
-			"--kvm-network=test-net",
+			// default is the network created by libvirt, if we change the name minikube won't boot
+			// because the given network doesn't exist
+			"--kvm-network=default",
 		}},
 		{"feature_gates_newest_cni", []string{
 			"--feature-gates",
