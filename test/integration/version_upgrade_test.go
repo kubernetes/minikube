fe33c3576ea1f9b9a0fe648c5d90e1006d9918b2
Thomas Stromberg <tstromberg@google.com>
2019-06-24 11:42:05 +0800

    Add missing import and parens

--- test/integration/version_upgrade_test.go
+++ test/integration/version_upgrade_test.go
@@ -22,6 +22,7 @@ import (
 	"io/ioutil"
 	"os"
 	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/docker/machine/libmachine/state"
@@ -82,8 +83,8 @@ func TestVersionUpgrade(t *testing.T) {
 	releaseRunner.RunCommand("stop", true)
 	releaseRunner.CheckStatus(state.Stopped.String())
 
-	// Trim the leading "v" prefix to assert that we handle  it properly.
-	currentRunner.Start(fmt.Sprintf("--kubernetes-version=%s", strings.TrimPrefix(constants.NewestKubernetesVersion, "v"))
+	// Trim the leading "v" prefix to assert that we handle it properly.
+	currentRunner.Start(fmt.Sprintf("--kubernetes-version=%s", strings.TrimPrefix(constants.NewestKubernetesVersion, "v")))
 	currentRunner.CheckStatus(state.Running.String())
 	currentRunner.RunCommand("delete", true)
 	currentRunner.CheckStatus(state.None.String())
