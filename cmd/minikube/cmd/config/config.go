05ac96480dec14f682af842817d1c844477f0ae6
RA489 <rohit.anand@india.nec.com>
2019-07-04 13:39:01 +0530

    [FEATURE] allow to set container-runtime via config

--- cmd/minikube/cmd/config/config.go
+++ cmd/minikube/cmd/config/config.go
@@ -176,6 +176,12 @@ var settings = []Setting{
 		callbacks:   []setFn{EnableOrDisableAddon},
 	},
 	{
+		name:        "insecure-registry",
+		set:         SetBool,
+		validations: []setFn{IsValidAddon},
+		callbacks:   []setFn{EnableOrDisableAddon},
+	},
+	{
 		name:        "registry",
 		set:         SetBool,
 		validations: []setFn{IsValidAddon},
