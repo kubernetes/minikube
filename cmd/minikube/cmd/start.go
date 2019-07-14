63e6c66413ded7a816a5931c47ec7fcecfaa9607
Anders F Björklund <anders.f.bjorklund@gmail.com>
2019-07-14 16:03:28 +0200

    Don't download ISO for the generic VM driver

--- cmd/minikube/cmd/start.go
+++ cmd/minikube/cmd/start.go
@@ -299,7 +299,7 @@ func provisionWithDriver(cmd *cobra.Command, ds registry.DriverState, existing *
 		os.Exit(0)
 	}
 
-	if driver.IsVM(driverName) {
+	if driver.IsVM(driverName) && driverName != driver.Generic {
 		url, err := download.ISO(viper.GetStringSlice(isoURL), cmd.Flags().Changed(isoURL))
 		if err != nil {
 			return node.Starter{}, errors.Wrap(err, "Failed to cache ISO")
