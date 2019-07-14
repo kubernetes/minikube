f7eaeb0be3bf7939b65caacb897ef9e1559d5cda
Anders F Björklund <anders.f.bjorklund@gmail.com>
2019-07-14 16:02:37 +0200

    Only provision buildroot with BuildrootProvisioner

--- pkg/minikube/cluster/cluster.go
+++ pkg/minikube/cluster/cluster.go
@@ -160,7 +160,7 @@ func configureHost(h *host.Host, e *engine.Options) error {
 		if err != nil {
 			return errors.Wrap(err, "detecting provisioner")
 		}
-		glog.Infof("Provisioning: %+v", *h.HostOptions)
+		glog.Infof("Provisioning with %s: %+v", provisioner.String(), *h.HostOptions)
 		if err := provisioner.Provision(*h.HostOptions.SwarmOptions, *h.HostOptions.AuthOptions, *h.HostOptions.EngineOptions); err != nil {
 			return errors.Wrap(err, "provision")
 		}
