83d6fcd6710354d04b79abac35b9583dc8ccafd9
Sharif Elgamal <selgamal@google.com>
2019-07-19 13:45:33 -0700

    comment fix

--- pkg/minikube/out/out.go
+++ pkg/minikube/out/out.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package out provides a mechanism for sending localized, stylized output to the out.
+// Package out provides a mechanism for sending localized, stylized output to the console.
 package out
 
 import (
