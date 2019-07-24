2c2d8ea61d5bdc6ff726c7ec8aff3d40c3c26e66
Medya Gh <medya@google.com>
2019-07-24 00:26:57 -0700

    remove extra context passing

--- test/integration/util/kubectl_runner.go
+++ test/integration/util/kubectl_runner.go
@@ -51,12 +51,9 @@ func NewKubectlRunner(t *testing.T, profile ...string) *KubectlRunner {
 }
 
 // RunCommandParseOutput runs a command and parses the JSON output
-func (k *KubectlRunner) RunCommandParseOutput(args []string, outputObj interface{}, setKubeContext ...bool) error {
-	kubecContextArg := fmt.Sprintf(" --context=%s", k.Profile)
-	args = append([]string{kubecContextArg}, args...) // prepending --context so it can be with with -- space
-
+func (k *KubectlRunner) RunCommandParseOutput(args []string, outputObj interface{}, useKubeContext ...bool) error {
 	args = append(args, "-o=json")
-	output, err := k.RunCommand(args)
+	output, err := k.RunCommand(args, useKubeContext...)
 	if err != nil {
 		return err
 	}
