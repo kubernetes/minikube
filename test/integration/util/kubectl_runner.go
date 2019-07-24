f5bc20c171b880f0652c8ca0b4f4c685a1f490bc
Medya Gh <medya@google.com>
2019-07-23 23:52:45 -0700

    Adding optional set kubecontext

--- test/integration/util/kubectl_runner.go
+++ test/integration/util/kubectl_runner.go
@@ -68,9 +68,15 @@ func (k *KubectlRunner) RunCommandParseOutput(args []string, outputObj interface
 }
 
 // RunCommand runs a command, returning stdout
-func (k *KubectlRunner) RunCommand(args []string) (stdout []byte, err error) {
-	kubecContextArg := fmt.Sprintf(" --context=%s", k.Profile)
-	args = append([]string{kubecContextArg}, args...) // prepending --context so it can be with with -- space
+func (k *KubectlRunner) RunCommand(args []string, useKubeContext ...bool) (stdout []byte, err error) {
+
+	if useKubeContext == nil {
+		useKubeContext = []bool{true}
+	}
+	if useKubeContext[0] == true {
+		kubecContextArg := fmt.Sprintf(" --context=%s", k.Profile)
+		args = append([]string{kubecContextArg}, args...) // prepending --context so it can be with with -- space
+	}
 
 	inner := func() error {
 		cmd := exec.Command(k.BinaryPath, args...)
