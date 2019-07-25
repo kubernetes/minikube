cbefe9936e09fbd1c726c3ba5325f6058fb8ecb2
josedonizetti <jdbjunior@gmail.com>
2019-07-25 11:09:46 -0300

    Fix lint

--- pkg/initflag/initflag.go
+++ pkg/initflag/initflag.go
@@ -7,5 +7,5 @@ import (
 func init() {
 	// Workaround for "ERROR: logging before flag.Parse"
 	// See: https://github.com/kubernetes/kubernetes/issues/17162
-	flag.CommandLine.Parse([]string{})
+	_ = flag.CommandLine.Parse([]string{})
 }
