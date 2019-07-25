e81e052863d0ce14451f2da96b612976693ead9a
Sharif Elgamal <selgamal@google.com>
2019-07-25 10:58:35 -0700

    remove commented code

--- cmd/minikube/cmd/root.go
+++ cmd/minikube/cmd/root.go
@@ -126,31 +126,6 @@ func Execute() {
 // explicitly using the raw string instead of calling c.UsageTemplate()
 // so the extractor can find this monstrosity of a string
 func usageTemplate() string {
-	/*return translate.T(`Usage:{{if .Runnable}}
-		{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
-		{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
-
-	  Aliases:
-		{{.NameAndAliases}}{{end}}{{if .HasExample}}
-
-	  Examples:
-	  {{.Example}}{{end}}{{if .HasAvailableSubCommands}}
-
-	  Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
-		{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
-
-	  Flags:
-	  {{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
-
-	  Global Flags:
-	  {{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
-
-	  Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
-		{{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
-
-	  Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
-	  `)*/
-
 	return fmt.Sprintf(`%s:{{if .Runnable}}
   {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
   {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
