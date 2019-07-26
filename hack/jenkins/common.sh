a99bd3e409cfe51cd527b9940400fa8755efd9b8
Medya Gh <medya@google.com>
2019-07-26 10:00:51 -0700

    move file download logic to common.sh

--- hack/jenkins/common.sh
+++ hack/jenkins/common.sh
@@ -74,6 +74,12 @@ gsutil -qm cp \
   "gs://minikube-builds/${MINIKUBE_LOCATION}/e2e-${OS_ARCH}" out
 
 gsutil -qm cp "gs://minikube-builds/${MINIKUBE_LOCATION}/testdata"/* testdata/
+gsutil -qm cp "gs://minikube-builds/${MINIKUBE_LOCATION}/testdata"/* testdata/
+
+# to be used by TestVersionUpgrade
+gsutil -qm cp gs://minikube/releases/latest/minikube-${OS_ARCH} testdata/minikube-${OS_ARCH}-latest-stable
+chmod +x "testdata/minikube-${OS_ARCH}-latest-stable"
+
 
 # Set the executable bit on the e2e binary and out binary
 export MINIKUBE_BIN="out/minikube-${OS_ARCH}"
