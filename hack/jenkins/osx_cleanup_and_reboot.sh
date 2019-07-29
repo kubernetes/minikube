0376174f777746f0e407850c421663c84267544f
Sharif Elgamal <selgamal@google.com>
2019-07-29 14:53:36 -0700

    spelling

--- hack/jenkins/osx_cleanup_and_reboot.sh
+++ hack/jenkins/osx_cleanup_and_reboot.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Periodically cleanip and reboot if no Jenkins subprocesses are running.
+# Periodically cleanup and reboot if no Jenkins subprocesses are running.
 set -uf -o pipefail
 
 PATH=/usr/local/bin:/sbin:/usr/local/sbin:$PATH
--- hack/jenkins/windows_cleanup_and_reboot.bat
+++ hack/jenkins/windows_cleanup_and_reboot.bat
@@ -12,7 +12,7 @@
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
 
-:: Periodically cleanip and reboot if no Jenkins subprocesses are running.
+:: Periodically cleanup and reboot if no Jenkins subprocesses are running.
 
 @echo off
 call :jenkins
