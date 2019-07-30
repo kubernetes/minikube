e865c93fc9b5c2fe2b47000a5c076f71b4b179c6
Medya Gh <medya@google.com>
2019-07-29 17:57:36 -0700

    move all starts to new start func

---
+++ test/integration/a_download_only_test.go
@@ -0,0 +1,42 @@
+// +build integration
+
+/*
+Copyright 2019 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package integration
+
+import (
+	"testing"
+	"time"
+)
+
+// TestDownloadOnly downloads ISOs also tests the --download-only option
+// Note this test runs before all tests (because of file name) and caches images for them
+func TestDownloadOnly(t *testing.T) {
+	p := t.Name()
+	if isTestNoneDriver() {
+		t.Skip()
+
+	}
+	mk := NewMinikubeRunner(t, p)
+	stdout, stderr, err := mk.StartWithStds(15*time.Minute, "--download-only")
+	if err != nil {
+		t.Fatalf("%s minikube start failed : %v\nstdout: %s\nstderr: %s", p, err, stdout, stderr)
+	}
+	// TODO: add test to check if files are downloaded
+
+	mk.RunCommand("delete", true)
+}
