067c0683d607a6571cd1311ffc72205c4e352bdf
josedonizetti <jdbjunior@gmail.com>
2019-07-30 14:16:51 -0300

    Improve UnsetTest

--- cmd/minikube/cmd/config/unset_test.go
+++ cmd/minikube/cmd/config/unset_test.go
@@ -16,7 +16,11 @@ limitations under the License.
 
 package config
 
-import "testing"
+import (
+	"testing"
+
+	"k8s.io/minikube/pkg/minikube/config"
+)
 
 func TestUnsetConfig(t *testing.T) {
 	propName := "cpus"
@@ -25,8 +29,23 @@ func TestUnsetConfig(t *testing.T) {
 	if err != nil {
 		t.Errorf("Failed to set the property %q", propName)
 	}
+
+	cpus, err := config.Get("cpus")
+	if err != nil {
+		t.Errorf("Failed to read config %q", err)
+	}
+
+	if cpus != propValue {
+		t.Errorf("Expected cpus to be %s but got %s", propValue, cpus)
+	}
+
 	err = Unset(propName)
 	if err != nil {
 		t.Errorf("Failed to unset property %q", err)
+	}
+
+	_, err = config.Get("cpus")
+	if err != config.ErrKeyNotFound {
+		t.Errorf("Expected error %q but got %q", config.ErrKeyNotFound, err)
 	}
 }
