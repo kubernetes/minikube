5295d26e4f0c9c8e40239d487b53e5f9f402b41a
Medya Gh <medya@google.com>
2019-07-30 21:21:33 -0700

    Trying 4

--- hack/jenkins/osx_integration_tests_virtualbox.sh
+++ hack/jenkins/osx_integration_tests_virtualbox.sh
@@ -29,7 +29,7 @@ OS_ARCH="darwin-amd64"
 VM_DRIVER="virtualbox"
 JOB_NAME="OSX-Virtualbox"
 EXTRA_ARGS="--bootstrapper=kubeadm"
-PARALLEL_COUNT=6
+PARALLEL_COUNT=4
 
 # Download files and set permissions
 source common.sh
