f4b6b96f345da7fdf41050188b34bd362e1a6f10
Medya Gh <medya@google.com>
2019-08-07 12:01:13 -0700

    chanfges

--- pkg/minikube/kubeconfig/kubeconfig.go
+++ pkg/minikube/kubeconfig/kubeconfig.go
@@ -18,20 +18,14 @@ package kubeconfig
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
 
-	"github.com/golang/glog"
 	"github.com/pkg/errors"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/tools/clientcmd/api"
-	"k8s.io/client-go/tools/clientcmd/api/latest"
 	"k8s.io/minikube/pkg/minikube/constants"
-	pkgutil "k8s.io/minikube/pkg/util"
 )
 
 // VeryifyMachineIP verifies the ip stored in kubeconfig.
@@ -73,111 +67,11 @@ func Port(filename, machineName string) (int, error) {
 	return port, err
 }
 
-// GetKubeConfigPath gets the path to the first kubeconfig
+// Path() gets the path to the first kubeconfig
 func Path() string {
 	kubeConfigEnv := os.Getenv(constants.KubeconfigEnvVar)
 	if kubeConfigEnv == "" {
 		return constants.KubeconfigPath
 	}
 	return filepath.SplitList(kubeConfigEnv)[0]
-}
-
-// readOrNew retrieves Kubernetes client configuration from a file.
-// If no files exists, an empty configuration is returned.
-func readOrNew(filename string) (*api.Config, error) {
-	data, err := ioutil.ReadFile(filename)
-	if os.IsNotExist(err) {
-		return api.NewConfig(), nil
-	} else if err != nil {
-		return nil, errors.Wrapf(err, "Error reading file %q", filename)
-	}
-
-	// decode config, empty if no bytes
-	config, err := decode(data)
-	if err != nil {
-		return nil, errors.Errorf("could not read config: %v", err)
-	}
-
-	// initialize nil maps
-	if config.AuthInfos == nil {
-		config.AuthInfos = map[string]*api.AuthInfo{}
-	}
-	if config.Clusters == nil {
-		config.Clusters = map[string]*api.Cluster{}
-	}
-	if config.Contexts == nil {
-		config.Contexts = map[string]*api.Context{}
-	}
-
-	return config, nil
-}
-
-// writeToFile encodes the configuration and writes it to the given file.
-// If the file exists, it's contents will be overwritten.
-func writeToFile(config runtime.Object, filename string) error {
-	if config == nil {
-		glog.Errorf("could not write to '%s': config can't be nil", filename)
-	}
-
-	// encode config to YAML
-	data, err := runtime.Encode(latest.Codec, config)
-	if err != nil {
-		return errors.Errorf("could not write to '%s': failed to encode config: %v", filename, err)
-	}
-
-	// create parent dir if doesn't exist
-	dir := filepath.Dir(filename)
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		if err = os.MkdirAll(dir, 0755); err != nil {
-			return errors.Wrapf(err, "Error creating directory: %s", dir)
-		}
-	}
-
-	// write with restricted permissions
-	if err := ioutil.WriteFile(filename, data, 0600); err != nil {
-		return errors.Wrapf(err, "Error writing file %s", filename)
-	}
-	if err := pkgutil.MaybeChownDirRecursiveToMinikubeUser(dir); err != nil {
-		return errors.Wrapf(err, "Error recursively changing ownership for dir: %s", dir)
-	}
-
-	return nil
-}
-
-// extractIP returns the IP address stored for minikube in the kubeconfig specified
-func extractIP(filename, machineName string) (net.IP, error) {
-	con, err := readOrNew(filename)
-	if err != nil {
-		return nil, errors.Wrap(err, "Error getting kubeconfig status")
-	}
-	cluster, ok := con.Clusters[machineName]
-	if !ok {
-		return nil, errors.Errorf("Kubeconfig does not have a record of the machine cluster")
-	}
-	kurl, err := url.Parse(cluster.Server)
-	if err != nil {
-		return net.ParseIP(cluster.Server), nil
-	}
-	kip, _, err := net.SplitHostPort(kurl.Host)
-	if err != nil {
-		return net.ParseIP(kurl.Host), nil
-	}
-	ip := net.ParseIP(kip)
-	return ip, nil
-}
-
-// decode reads a Config object from bytes.
-// Returns empty config if no bytes.
-func decode(data []byte) (*api.Config, error) {
-	// if no data, return empty config
-	if len(data) == 0 {
-		return api.NewConfig(), nil
-	}
-
-	config, _, err := latest.Codec.Decode(data, nil, nil)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Error decoding config from data: %s", string(data))
-	}
-
-	return config.(*api.Config), nil
 }
--- pkg/minikube/kubeconfig/update.go
+++ pkg/minikube/kubeconfig/update.go
@@ -18,7 +18,11 @@ package kubeconfig
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
+	"net/url"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -26,9 +30,13 @@ import (
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/clientcmd/api"
+	"k8s.io/kubernetes/pkg/scheduler/api/latest"
 	cfg "k8s.io/minikube/pkg/minikube/config"
 	"k8s.io/minikube/pkg/minikube/constants"
 	"k8s.io/minikube/pkg/minikube/exit"
+	pkgutil "k8s.io/minikube/pkg/util"
 )
 
 // Update sets up kubeconfig to be used by kubectl
@@ -111,4 +119,104 @@ func UpdateIP(ip net.IP, filename string, machineName string) (bool, error) {
 	}
 	// Kubeconfig IP reconfigured
 	return true, nil
+}
+
+// writeToFile encodes the configuration and writes it to the given file.
+// If the file exists, it's contents will be overwritten.
+func writeToFile(config runtime.Object, filename string) error {
+	if config == nil {
+		glog.Errorf("could not write to '%s': config can't be nil", filename)
+	}
+
+	// encode config to YAML
+	data, err := runtime.Encode(latest.Codec, config)
+	if err != nil {
+		return errors.Errorf("could not write to '%s': failed to encode config: %v", filename, err)
+	}
+
+	// create parent dir if doesn't exist
+	dir := filepath.Dir(filename)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err = os.MkdirAll(dir, 0755); err != nil {
+			return errors.Wrapf(err, "Error creating directory: %s", dir)
+		}
+	}
+
+	// write with restricted permissions
+	if err := ioutil.WriteFile(filename, data, 0600); err != nil {
+		return errors.Wrapf(err, "Error writing file %s", filename)
+	}
+	if err := pkgutil.MaybeChownDirRecursiveToMinikubeUser(dir); err != nil {
+		return errors.Wrapf(err, "Error recursively changing ownership for dir: %s", dir)
+	}
+
+	return nil
+}
+
+// readOrNew retrieves Kubernetes client configuration from a file.
+// If no files exists, an empty configuration is returned.
+func readOrNew(filename string) (*api.Config, error) {
+	data, err := ioutil.ReadFile(filename)
+	if os.IsNotExist(err) {
+		return api.NewConfig(), nil
+	} else if err != nil {
+		return nil, errors.Wrapf(err, "Error reading file %q", filename)
+	}
+
+	// decode config, empty if no bytes
+	config, err := decode(data)
+	if err != nil {
+		return nil, errors.Errorf("could not read config: %v", err)
+	}
+
+	// initialize nil maps
+	if config.AuthInfos == nil {
+		config.AuthInfos = map[string]*api.AuthInfo{}
+	}
+	if config.Clusters == nil {
+		config.Clusters = map[string]*api.Cluster{}
+	}
+	if config.Contexts == nil {
+		config.Contexts = map[string]*api.Context{}
+	}
+
+	return config, nil
+}
+
+// extractIP returns the IP address stored for minikube in the kubeconfig specified
+func extractIP(filename, machineName string) (net.IP, error) {
+	con, err := readOrNew(filename)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error getting kubeconfig status")
+	}
+	cluster, ok := con.Clusters[machineName]
+	if !ok {
+		return nil, errors.Errorf("Kubeconfig does not have a record of the machine cluster")
+	}
+	kurl, err := url.Parse(cluster.Server)
+	if err != nil {
+		return net.ParseIP(cluster.Server), nil
+	}
+	kip, _, err := net.SplitHostPort(kurl.Host)
+	if err != nil {
+		return net.ParseIP(kurl.Host), nil
+	}
+	ip := net.ParseIP(kip)
+	return ip, nil
+}
+
+// decode reads a Config object from bytes.
+// Returns empty config if no bytes.
+func decode(data []byte) (*api.Config, error) {
+	// if no data, return empty config
+	if len(data) == 0 {
+		return api.NewConfig(), nil
+	}
+
+	config, _, err := latest.Codec.Decode(data, nil, nil)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Error decoding config from data: %s", string(data))
+	}
+
+	return config.(*api.Config), nil
 }
