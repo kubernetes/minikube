c5fb9505cb64845354b6911b54b43a60d3214e04
Sharif Elgamal <selgamal@google.com>
2019-08-06 17:23:08 -0700

    try goproxy for travis

--- test.sh
+++ test.sh
@@ -19,7 +19,7 @@ set -eu -o pipefail
 exitcode=0
 
 echo "= go mod ================================================================"
-export GOPROXY=http://proxy.golang.org
+export GOPROXY=https://proxy.golang.org
 go mod download 2>&1 | grep -v "go: finding" || true
 go mod tidy -v && echo ok || ((exitcode += 2))
 
