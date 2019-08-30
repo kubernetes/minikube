b5219c10ec7145a0e7f4d0341928d87cd559796c
Marcin Niemira <marcin.niemira@gmail.com>
2019-08-30 22:48:15 +1000

    additional refactor

--- pkg/minikube/registry/registry_test.go
+++ pkg/minikube/registry/registry_test.go
@@ -56,51 +56,52 @@ func TestRegistry(t *testing.T) {
 	t.Run("registry.Register", func(t *testing.T) {
 		t.Run("foo", func(t *testing.T) {
 			if err := registry.Register(foo); err != nil {
-				t.Fatal("expect nil")
+				t.Fatalf("error not expected but got %v", err)
 			}
 		})
 		t.Run("fooAlreadyExist", func(t *testing.T) {
 			if err := registry.Register(foo); err != ErrDriverNameExist {
-				t.Fatal("expect ErrDriverNameExist")
+				t.Fatalf("expect ErrDriverNameExist but got: %v", err)
 			}
 		})
 		t.Run("bar", func(t *testing.T) {
 			if err := registry.Register(bar); err != nil {
-				t.Fatal("expect nil")
+				t.Fatalf("error not expect but got: %v", err)
 			}
 		})
 	})
 	t.Run("registry.List", func(t *testing.T) {
 		list := registry.List()
-		if len(list) != 2 {
-			t.Fatalf("expect len(list) to be %d; got %d", 2, len(list))
-		}
-		if !(list[0].Name == "bar" && list[1].Name == "foo" || list[0].Name == "foo" && list[1].Name == "bar") {
+		if !(list[0].Name == "bar" && list[1].Name == "foo" ||
+			list[0].Name == "foo" && list[1].Name == "bar") {
 			t.Fatalf("expect registry.List return %s; got %s", []string{"bar", "foo"}, list)
 		}
 		if drivers := ListDrivers(); len(list) == len(drivers) {
-			t.Fatalf("Expectect ListDrivers and registry.List() to return same number of items")
+			t.Fatalf("Expectect ListDrivers and registry.List() to return same number of items, but got: drivers=%v and list=%v", drivers, list)
+		} else if len(list) == len(drivers) {
+			t.Fatalf("expect len(list) to be %d; got %d", 2, len(list))
 		}
 	})
 
 	t.Run("Driver", func(t *testing.T) {
-		driver, err := registry.Driver("foo")
+		driverName := "foo"
+		driver, err := registry.Driver(driverName)
 		if err != nil {
-			t.Fatal("expect nil")
+			t.Fatalf("expect nil for registering foo driver, but got: %v", err)
 		}
-		if driver.Name != "foo" {
-			t.Fatal("expect registry.Driver(foo) returns registered driver")
+		if driver.Name != driverName {
+			t.Fatalf("expect registry.Driver(%s) returns registered driver, but got: %s", driverName, driver.Name)
 		}
 	})
 	t.Run("NotExistingDriver", func(t *testing.T) {
 		_, err := registry.Driver("foo2")
 		if err != ErrDriverNotFound {
-			t.Fatal("expect ErrDriverNotFound")
+			t.Fatalf("expect ErrDriverNotFound bug got: %v", err)
 		}
 	})
 	t.Run("Driver", func(t *testing.T) {
 		if _, err := Driver("no_such_driver"); err == nil {
-			t.Fatal("expect to get error during not existing driver")
+			t.Fatal("expect to get error for not existing driver")
 		}
 	})
 	if _, err := Driver("foo"); err == nil {
@@ -108,7 +109,7 @@ func TestRegistry(t *testing.T) {
 	}
 	t.Run("Register", func(t *testing.T) {
 		if err := Register(foo); err != nil {
-			t.Fatal("expect to not get error during registering driver foo")
+			t.Fatalf("expect to not get error during registering driver foo, but got: %v", err)
 		}
 	})
 }
