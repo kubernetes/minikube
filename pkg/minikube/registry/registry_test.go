aa79d3962545def90107d002d2fd8bc1618a33b6
Marcin Niemira <marcin.niemira@gmail.com>
2019-08-30 22:04:39 +1000

    improve test coverage for registry package

--- pkg/minikube/registry/registry_test.go
+++ pkg/minikube/registry/registry_test.go
@@ -22,6 +22,20 @@ import (
 	"k8s.io/minikube/pkg/minikube/config"
 )
 
+func TestDriverString(t *testing.T) {
+	bar := DriverDef{
+		Name:    "bar",
+		Builtin: true,
+		ConfigCreator: func(_ config.MachineConfig) interface{} {
+			return nil
+		},
+	}
+	s := bar.String()
+	if s != "{name: bar, builtin: true}" {
+		t.Fatalf("Driver bar.String() returned unexpected: %v", s)
+	}
+}
+
 func TestRegistry(t *testing.T) {
 	foo := DriverDef{
 		Name:    "foo",
@@ -40,18 +54,15 @@ func TestRegistry(t *testing.T) {
 
 	registry := createRegistry()
 
-	err := registry.Register(foo)
-	if err != nil {
+	if err := registry.Register(foo); err != nil {
 		t.Fatal("expect nil")
 	}
 
-	err = registry.Register(foo)
-	if err != ErrDriverNameExist {
+	if err := registry.Register(foo); err != ErrDriverNameExist {
 		t.Fatal("expect ErrDriverNameExist")
 	}
 
-	err = registry.Register(bar)
-	if err != nil {
+	if err := registry.Register(bar); err != nil {
 		t.Fatal("expect nil")
 	}
 
@@ -63,6 +74,9 @@ func TestRegistry(t *testing.T) {
 	if !(list[0].Name == "bar" && list[1].Name == "foo" || list[0].Name == "foo" && list[1].Name == "bar") {
 		t.Fatalf("expect registry.List return %s; got %s", []string{"bar", "foo"}, list)
 	}
+	if drivers := ListDrivers(); len(list) == len(drivers) {
+		t.Fatalf("Expectect ListDrivers and registry.List() to return same number of items")
+	}
 
 	driver, err := registry.Driver("foo")
 	if err != nil {
@@ -72,8 +86,20 @@ func TestRegistry(t *testing.T) {
 		t.Fatal("expect registry.Driver(foo) returns registered driver")
 	}
 
-	driver, err = registry.Driver("foo2")
+	_, err = registry.Driver("foo2")
 	if err != ErrDriverNotFound {
 		t.Fatal("expect ErrDriverNotFound")
+	}
+
+	if _, err := Driver("no_such_driver"); err == nil {
+		t.Fatal("expect to get error during not existing driver")
+	}
+
+	if _, err := Driver("foo"); err == nil {
+		t.Fatal("expect to not get error during existing driver foo")
+	}
+
+	if err := Register(foo); err != nil {
+		t.Fatal("expect to not get error during registering driver foo")
 	}
 }
