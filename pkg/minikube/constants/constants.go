35ef2f391a2a68ea7941d57b798ab69a3b2411af
Thomas Stromberg <tstromberg@google.com>
2019-09-18 21:45:08 -0700

    Update image cache to addon-manager 9.0.2

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -348,7 +348,7 @@ func GetKubeadmCachedImages(imageRepository string, kubernetesVersionStr string)
 
 	images = append(images, []string{
 		imageRepository + "kubernetes-dashboard" + ArchTag(true) + "v1.10.1",
-		imageRepository + "kube-addon-manager" + ArchTag(false) + "v9.0",
+		imageRepository + "kube-addon-manager" + ArchTag(false) + "v9.0.2",
 		minikubeRepository + "storage-provisioner" + ArchTag(false) + "v1.8.1",
 	}...)
 
