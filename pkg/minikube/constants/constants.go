937e0cbeac858792a1e11a92ca55dc62b7983bd8
Sharif Elgamal <selgamal@google.com>
2019-10-10 13:01:02 -0700

    Update default Kubernetes version to 1.16.1

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -92,10 +92,10 @@ var DefaultISOURL = fmt.Sprintf("https://storage.googleapis.com/%s/minikube-%s.i
 var DefaultISOSHAURL = DefaultISOURL + SHASuffix
 
 // DefaultKubernetesVersion is the default kubernetes version
-var DefaultKubernetesVersion = "v1.16.0"
+var DefaultKubernetesVersion = "v1.16.1"
 
 // NewestKubernetesVersion is the newest Kubernetes version to test against
-var NewestKubernetesVersion = "v1.16.0"
+var NewestKubernetesVersion = "v1.16.1"
 
 // OldestKubernetesVersion is the oldest Kubernetes version to test against
 var OldestKubernetesVersion = "v1.11.10"
