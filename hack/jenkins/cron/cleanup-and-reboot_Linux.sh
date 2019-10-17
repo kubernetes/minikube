0ce8ab0524cf8fbfbc785063845cb351ff6ecbb6
Thomas Stromberg <tstromberg@google.com>
2019-10-17 13:11:31 -0700

    Add boilerplate

--- hack/jenkins/cron/cleanup-and-reboot_Linux.sh
+++ hack/jenkins/cron/cleanup-and-reboot_Linux.sh
@@ -14,25 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright 2016 The Kubernetes Authors All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Periodically cleanup and reboot if no Jenkins subprocesses are running.
-#
-# Installation:
-#   install cleanup-and-reboot.linux /etc/cron.hourly/cleanup-and-reboot
-
 function check_jenkins() {
   jenkins_pid="$(pidof java)"
   if [[ "${jenkins_pid}" = "" ]]; then
