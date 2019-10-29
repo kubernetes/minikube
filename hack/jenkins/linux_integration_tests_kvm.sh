4e32d2b915d61e4776558897c2ad392772a1dbd8
tstromberg <tstromberg@google.com>
2019-10-29 16:10:13 -0700

    Fix issues with crontab installation

--- hack/jenkins/linux_integration_tests_kvm.sh
+++ hack/jenkins/linux_integration_tests_kvm.sh
@@ -31,7 +31,7 @@ JOB_NAME="KVM_Linux"
 PARALLEL_COUNT=4
 
 mkdir cron && gsutil -qm rsync "gs://minikube-builds/${MINIKUBE_LOCATION}/cron" cron
-install cron/cleanup-and-reboot.linux /etc/cron.hourly/cleanup-and-reboot
+install cron/cleanup-and-reboot_Linux.sh /etc/cron.hourly/cleanup-and-reboot
 
 # Download files and set permissions
 source ./common.sh
