6c08fedf970cd851ef7f0951f5f80e1dc9436ce4
Medya Gh <medya@google.com>
2019-11-26 17:48:37 -0800

    remove cyclic depdenecy and make cluster pkg smaller

--- cmd/minikube/cmd/delete_test.go
+++ cmd/minikube/cmd/delete_test.go
@@ -26,9 +26,9 @@ import (
 	"github.com/otiai10/copy"
 	"github.com/spf13/viper"
 
-	"k8s.io/minikube/pkg/minikube/cluster"
 	"k8s.io/minikube/pkg/minikube/config"
 	"k8s.io/minikube/pkg/minikube/localpath"
+	"k8s.io/minikube/pkg/minikube/machine"
 )
 
 // except returns a list of strings, minus the excluded ones
@@ -117,7 +117,7 @@ func TestDeleteProfile(t *testing.T) {
 				t.Errorf("Profile folder of profile \"%s\" was not deleted", profile.Name)
 			}
 
-			pathToMachine := cluster.MachinePath(profile.Name, localpath.MiniPath())
+			pathToMachine := machine.MachinePath(profile.Name, localpath.MiniPath())
 			if _, err := os.Stat(pathToMachine); !os.IsNotExist(err) {
 				t.Errorf("Profile folder of profile \"%s\" was not deleted", profile.Name)
 			}
