89e9ef99d6849885ede1bb994de34e36556f9740
Zhou Hao <zhouhao@cn.fujitsu.com>
2019-12-30 15:46:19 +0800

    pkg/drivers/kic: add judgment of stop status for Stop
    Signed-off-by: Zhou Hao <zhouhao@cn.fujitsu.com>

--- pkg/drivers/kic/kic.go
+++ pkg/drivers/kic/kic.go
@@ -229,6 +229,13 @@ func (d *Driver) Start() error {
 
 // Stop a host gracefully, including any containers that we are managing.
 func (d *Driver) Stop() error {
+	s, err := d.GetState()
+	if err != nil {
+		return errors.Wrap(err, "get kic state")
+	}
+	if s == state.Stopped {
+		return fmt.Errorf("kic node %s is already stopped", d.MachineName)
+	}
 	cmd := exec.Command(d.NodeConfig.OCIBinary, "stop", d.MachineName)
 	if err := cmd.Run(); err != nil {
 		return errors.Wrapf(err, "stopping %s", d.MachineName)
