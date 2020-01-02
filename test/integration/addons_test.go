3f3635c3c2a4e7cad37ed8f78410fa14535b36d9
Priya Wadhwa <priyawadhwa@google.com>
2020-01-02 10:50:11 -0800

    Don't run TestAddons in parallel with other tests, virtualbox on linux complains

--- test/integration/addons_test.go
+++ test/integration/addons_test.go
@@ -36,7 +36,6 @@ import (
 
 // TestAddons tests addons that require no special environment -- in parallel
 func TestAddons(t *testing.T) {
-	MaybeParallel(t)
 	WaitForStartSlot(t)
 	profile := UniqueProfileName("addons")
 	ctx, cancel := context.WithTimeout(context.Background(), 40*time.Minute)
