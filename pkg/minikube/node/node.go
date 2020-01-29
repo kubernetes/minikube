07d066e7f16a1f86bda8fd191e4c8017f34745ea
Sharif Elgamal <selgamal@google.com>
2020-01-28 17:09:00 -0800

    moving packages around

--- pkg/minikube/node/node.go
+++ pkg/minikube/node/node.go
@@ -19,6 +19,8 @@ package node
 import (
 	"errors"
 
+	"github.com/golang/glog"
+	"github.com/spf13/viper"
 	"k8s.io/minikube/pkg/minikube/config"
 )
 
@@ -43,10 +45,12 @@ func Add(cc *config.MachineConfig, name string, controlPlane bool, k8sVersion st
 
 // Delete stops and deletes the given node from the given cluster
 func Delete(cc *config.MachineConfig, name string) error {
-	nd := nil
-	for _, n := range cc.Nodes {
+	var nd *config.Node
+	index := 0
+	for i, n := range cc.Nodes {
 		if n.Name == name {
-			nd = n
+			nd = &n
+			index = i
 			break
 		}
 	}
@@ -54,8 +58,25 @@ func Delete(cc *config.MachineConfig, name string) error {
 	if nd == nil {
 		return errors.New("Could not find node " + name)
 	}
+
+	err := Stop(cc, name)
+	if err != nil {
+		glog.Warningf("Failed to stop node %s. Will still try to delete.", name)
+	}
+
+	// Spin down the machine, eventually.
+
+	cc.Nodes = append(cc.Nodes[:index], cc.Nodes[index+1:]...)
+	return config.SaveProfile(viper.GetString(config.MachineProfile), cc)
 }
 
+// Stop stops the node in the given cluster
 func Stop(cc *config.MachineConfig, name string) error {
+	return nil
+}
+
+// Start spins up a guest and starts the kubernetes node.
+func Start(cc *config.MachineConfig, name string) error {
+	// Throw all the slop from cmd.start in here
 	return nil
 }
