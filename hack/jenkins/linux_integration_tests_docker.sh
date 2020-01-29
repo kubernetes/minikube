0760859e62ed2d1c1096b9c56ea3e99cba3bc44f
Medya Gh <medya@google.com>
2020-01-28 18:27:35 -0800

    remove not needed check

--- hack/jenkins/linux_integration_tests_docker.sh
+++ hack/jenkins/linux_integration_tests_docker.sh
@@ -28,7 +28,6 @@ set -e
 OS_ARCH="linux-amd64"
 VM_DRIVER="docker"
 JOB_NAME="Docker_Linux"
-EXPECTED_DEFAULT_DRIVER="kvm2"
 
 mkdir -p cron && gsutil -qm rsync "gs://minikube-builds/${MINIKUBE_LOCATION}/cron" cron || echo "FAILED TO GET CRON FILES"
 sudo install cron/cleanup_and_reboot_Linux.sh /etc/cron.hourly/cleanup_and_reboot || echo "FAILED TO INSTALL CLEANUP"
