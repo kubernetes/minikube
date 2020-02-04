90fcfeaa350a1a84e0edf992826e521f98f1f472
Thomas Stromberg <tstromberg@google.com>
2020-02-04 07:45:09 -0800

    Minor refactor fixes

--- pkg/minikube/cluster/fix.go
+++ pkg/minikube/cluster/fix.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cluster
 
 import (
@@ -8,6 +24,7 @@ import (
 	"time"
 
 	"github.com/docker/machine/libmachine"
+	"github.com/docker/machine/libmachine/host"
 	"github.com/docker/machine/libmachine/provision"
 	"github.com/docker/machine/libmachine/state"
 	"github.com/golang/glog"
@@ -17,72 +34,71 @@ import (
 	"k8s.io/minikube/pkg/util/retry"
 )
 
-var (
+// hostRunner is a minimal host.Host based interface for running commands
+type hostRunner interface {
+	RunSSHCommand(string) (string, error)
+}
+
+const (
 	// The maximum the guest VM clock is allowed to be ahead and behind. This value is intentionally
 	// large to allow for inaccurate methodology, but still small enough so that certificates are likely valid.
 	maxClockDesyncSeconds = 2.1
 )
 
 // fixHost fixes up a previously configured VM so that it is ready to run Kubernetes
-func fixHost(api libmachine.API, mc config.MachineConfig) error {
-	h, err := api.Load(cfg.Name)
-	if err != nil {
-		return nil, errors.Wrap(err, "Error loading existing host. Please try running [minikube delete], then run [minikube start] again.")
-	}
-
-	s, err := h.Driver.GetState()
-	if err != nil {
-		return nil, errors.Wrap(err, "Error getting state for host")
-	}
-
-	if s == state.Running {
-		out.T(out.Running, `Using the running {{.driver_name}} "{{.profile_name}}" VM ...`, out.V{"driver_name": cfg.VMDriver, "profile_name": cfg.Name})
-	} else {
-		out.T(out.Restarting, `Starting existing {{.driver_name}} VM for "{{.profile_name}}" ...`, out.V{"driver_name": cfg.VMDriver, "profile_name": cfg.Name})
-		if err := h.Driver.Start(); err != nil {
-			return nil, errors.Wrap(err, "start")
-		}
-		if err := api.Save(h); err != nil {
-			return nil, errors.Wrap(err, "save")
-		}
-	}
-
-	e := engineOptions(cfg)
-	out.T(out.Waiting, "Waiting for the host to be provisioned ...")
-	err = fixHost(h, e, cfg)
-	if err != nil {
-		return nil, err
-	}
-	return h, nil
+func fixHost(api libmachine.API, mc config.MachineConfig) (*host.Host, error) {
+	out.T(out.Waiting, "Reconfiguring existing host ...")
 
 	start := time.Now()
-	glog.Infof("fixHost: %+v", h.Driver)
+	glog.Infof("fixHost starting: %s", mc.Name)
 	defer func() {
 		glog.Infof("fixHost completed within %s", time.Since(start))
 	}()
 
+	h, err := api.Load(mc.Name)
+	if err != nil {
+		return h, errors.Wrap(err, "Error loading existing host. Please try running [minikube delete], then run [minikube start] again.")
+	}
+
+	s, err := h.Driver.GetState()
+	if err != nil {
+		return h, errors.Wrap(err, "Error getting state for host")
+	}
+
+	if s == state.Running {
+		out.T(out.Running, `Using the running {{.driver_name}} "{{.profile_name}}" VM ...`, out.V{"driver_name": mc.VMDriver, "profile_name": mc.Name})
+	} else {
+		out.T(out.Restarting, `Starting existing {{.driver_name}} VM for "{{.profile_name}}" ...`, out.V{"driver_name": mc.VMDriver, "profile_name": mc.Name})
+		if err := h.Driver.Start(); err != nil {
+			return h, errors.Wrap(err, "driver start")
+		}
+		if err := api.Save(h); err != nil {
+			return h, errors.Wrap(err, "save")
+		}
+	}
+
+	e := engineOptions(mc)
 	if len(e.Env) > 0 {
 		h.HostOptions.EngineOptions.Env = e.Env
 		glog.Infof("Detecting provisioner ...")
 		provisioner, err := provision.DetectProvisioner(h.Driver)
 		if err != nil {
-			return errors.Wrap(err, "detecting provisioner")
+			return h, errors.Wrap(err, "detecting provisioner")
 		}
-		glog.Infof("Provisioning with %s: %+v", provisioner.String(), *h.HostOptions)
 		if err := provisioner.Provision(*h.HostOptions.SwarmOptions, *h.HostOptions.AuthOptions, *h.HostOptions.EngineOptions); err != nil {
-			return errors.Wrap(err, "provision")
+			return h, errors.Wrap(err, "provision")
 		}
 	}
 
 	if err := postStartSetup(h, mc); err != nil {
-		return errors.Wrap(err, "post-start")
+		return h, errors.Wrap(err, "post-start")
 	}
 
 	glog.Infof("Configuring auth for driver %s ...", h.Driver.DriverName())
 	if err := h.ConfigureAuth(); err != nil {
-		return &retry.RetriableError{Err: errors.Wrap(err, "Error configuring auth on host")}
+		return h, &retry.RetriableError{Err: errors.Wrap(err, "Error configuring auth on host")}
 	}
-	return ensureSyncedGuestClock(h)
+	return h, ensureSyncedGuestClock(h)
 }
 
 // ensureGuestClockSync ensures that the guest system clock is relatively in-sync
