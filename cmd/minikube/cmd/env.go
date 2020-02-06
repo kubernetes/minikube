32bc0ff76aed38fbf35090c55e9cfd8d67ad0eec
Thomas Stromberg <tstromberg@google.com>
2020-02-06 15:32:00 -0800

    Refactor docker-env for testability

--- cmd/minikube/cmd/env.go
+++ cmd/minikube/cmd/env.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 The Kubernetes Authors All rights reserved.
+Copyright 2020 The Kubernetes Authors All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,28 +21,35 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"net"
 	"os"
+	"strconv"
 	"strings"
 	"text/template"
 
-	"github.com/docker/machine/libmachine"
 	"github.com/docker/machine/libmachine/drivers"
-	"github.com/docker/machine/libmachine/host"
-	"github.com/docker/machine/libmachine/log"
 	"github.com/docker/machine/libmachine/shell"
 	"github.com/docker/machine/libmachine/state"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"k8s.io/minikube/pkg/drivers/kic"
+	"k8s.io/minikube/pkg/drivers/kic/oci"
 	"k8s.io/minikube/pkg/minikube/cluster"
 	"k8s.io/minikube/pkg/minikube/config"
 	"k8s.io/minikube/pkg/minikube/constants"
 	"k8s.io/minikube/pkg/minikube/driver"
 	"k8s.io/minikube/pkg/minikube/exit"
+	"k8s.io/minikube/pkg/minikube/localpath"
 	"k8s.io/minikube/pkg/minikube/machine"
+	"k8s.io/minikube/pkg/minikube/out"
 )
 
-var envTmpl = fmt.Sprintf("{{ .Prefix }}%s{{ .Delimiter }}{{ .DockerTLSVerify }}{{ .Suffix }}{{ .Prefix }}%s{{ .Delimiter }}{{ .DockerHost }}{{ .Suffix }}{{ .Prefix }}%s{{ .Delimiter }}{{ .DockerCertPath }}{{ .Suffix }}{{ .Prefix }}%s{{ .Delimiter }}{{ .MinikubeDockerdProfile }}{{ .Suffix }}{{ if .NoProxyVar }}{{ .Prefix }}{{ .NoProxyVar }}{{ .Delimiter }}{{ .NoProxyValue }}{{ .Suffix }}{{end}}{{ .UsageHint }}", constants.DockerTLSVerifyEnv, constants.DockerHostEnv, constants.DockerCertPathEnv, constants.MinikubeActiveDockerdEnv)
+var (
+	forceShell string
+	envTmpl    = fmt.Sprintf("{{ .Prefix }}%s{{ .Delimiter }}{{ .DockerTLSVerify }}{{ .Suffix }}{{ .Prefix }}%s{{ .Delimiter }}{{ .DockerHost }}{{ .Suffix }}{{ .Prefix }}%s{{ .Delimiter }}{{ .DockerCertPath }}{{ .Suffix }}{{ .Prefix }}%s{{ .Delimiter }}{{ .MinikubeDockerdProfile }}{{ .Suffix }}{{ if .NoProxyVar }}{{ .Prefix }}{{ .NoProxyVar }}{{ .Delimiter }}{{ .NoProxyValue }}{{ .Suffix }}{{end}}{{ .UsageHint }}", constants.DockerTLSVerifyEnv, constants.DockerHostEnv, constants.DockerCertPathEnv, constants.MinikubeActiveDockerdEnv)
+)
 
 const (
 	fishSetPfx   = "set -gx "
@@ -105,20 +112,8 @@ type ShellConfig struct {
 }
 
 var (
-	noProxy              bool
-	forceShell           string
-	unset                bool
-	defaultShellDetector ShellDetector
 	defaultNoProxyGetter NoProxyGetter
 )
-
-// ShellDetector detects shell
-type ShellDetector interface {
-	GetShell(string) (string, error)
-}
-
-// LibmachineShellDetector detects shell, using libmachine
-type LibmachineShellDetector struct{}
 
 // NoProxyGetter gets the no_proxy variable
 type NoProxyGetter interface {
@@ -128,169 +123,134 @@ type NoProxyGetter interface {
 // EnvNoProxyGetter gets the no_proxy variable, using environment
 type EnvNoProxyGetter struct{}
 
-func generateUsageHint(profile string, userShell string) string {
-	const usgPlz = "To point your shell to minikube's docker-daemon:"
+func generateUsageHint(profile, sh string) string {
+	const usgPlz = "Please run command bellow to point your shell to minikube's docker-daemon :"
 	var usgCmd = fmt.Sprintf("minikube -p %s docker-env", profile)
 	var usageHintMap = map[string]string{
-		"bash": fmt.Sprintf(`# %s
+		"bash": fmt.Sprintf(`
+# %s
 # eval $(%s)
 `, usgPlz, usgCmd),
-		"fish": fmt.Sprintf(`# %s
+		"fish": fmt.Sprintf(`
+# %s
 # eval (%s)
 `, usgPlz, usgCmd),
-		"powershell": fmt.Sprintf(`# %s
+		"powershell": fmt.Sprintf(`
+# %s
 # & %s | Invoke-Expression
 	`, usgPlz, usgCmd),
-		"cmd": fmt.Sprintf(`REM %s
+		"cmd": fmt.Sprintf(`
+REM %s
 REM @FOR /f "tokens=*" %%i IN ('%s') DO @%%i
 	`, usgPlz, usgCmd),
-		"emacs": fmt.Sprintf(`;; %s
+		"emacs": fmt.Sprintf(`
+;; %s
 ;; (with-temp-buffer (shell-command "%s" (current-buffer)) (eval-buffer))
 	`, usgPlz, usgCmd),
 	}
 
-	hint, ok := usageHintMap[userShell]
+	hint, ok := usageHintMap[sh]
 	if !ok {
 		return usageHintMap["bash"]
 	}
 	return hint
 }
 
-func shellCfgSet(api libmachine.API) (*ShellConfig, error) {
-
-	envMap, err := cluster.GetNodeDockerEnv(api)
-	if err != nil {
-		return nil, err
-	}
-
-	userShell, err := defaultShellDetector.GetShell(forceShell)
-	if err != nil {
-		return nil, err
-	}
-
-	shellCfg := &ShellConfig{
+// shellCfgSet generates context variables for "docker-env"
+func shellCfgSet(ec EnvConfig, envMap map[string]string) *ShellConfig {
+	s := &ShellConfig{
 		DockerCertPath:         envMap[constants.DockerCertPathEnv],
 		DockerHost:             envMap[constants.DockerHostEnv],
 		DockerTLSVerify:        envMap[constants.DockerTLSVerifyEnv],
 		MinikubeDockerdProfile: envMap[constants.MinikubeActiveDockerdEnv],
-		UsageHint:              generateUsageHint(viper.GetString(config.MachineProfile), userShell),
+		UsageHint:              generateUsageHint(ec.profile, ec.shell),
 	}
 
-	if noProxy {
-		host, err := api.Load(viper.GetString(config.MachineProfile))
-		if err != nil {
-			return nil, errors.Wrap(err, "Error getting IP")
-		}
-
-		ip, err := host.Driver.GetIP()
-		if err != nil {
-			return nil, errors.Wrap(err, "Error getting host IP")
-		}
-
+	if ec.noProxy {
 		noProxyVar, noProxyValue := defaultNoProxyGetter.GetNoProxyVar()
 
 		// add the docker host to the no_proxy list idempotently
 		switch {
 		case noProxyValue == "":
-			noProxyValue = ip
-		case strings.Contains(noProxyValue, ip):
+			noProxyValue = ec.hostIP
+		case strings.Contains(noProxyValue, ec.hostIP):
 		// ip already in no_proxy list, nothing to do
 		default:
-			noProxyValue = fmt.Sprintf("%s,%s", noProxyValue, ip)
+			noProxyValue = fmt.Sprintf("%s,%s", noProxyValue, ec.hostIP)
 		}
 
-		shellCfg.NoProxyVar = noProxyVar
-		shellCfg.NoProxyValue = noProxyValue
+		s.NoProxyVar = noProxyVar
+		s.NoProxyValue = noProxyValue
 	}
 
-	switch userShell {
+	switch ec.shell {
 	case "fish":
-		shellCfg.Prefix = fishSetPfx
-		shellCfg.Suffix = fishSetSfx
-		shellCfg.Delimiter = fishSetDelim
+		s.Prefix = fishSetPfx
+		s.Suffix = fishSetSfx
+		s.Delimiter = fishSetDelim
 	case "powershell":
-		shellCfg.Prefix = psSetPfx
-		shellCfg.Suffix = psSetSfx
-		shellCfg.Delimiter = psSetDelim
+		s.Prefix = psSetPfx
+		s.Suffix = psSetSfx
+		s.Delimiter = psSetDelim
 	case "cmd":
-		shellCfg.Prefix = cmdSetPfx
-		shellCfg.Suffix = cmdSetSfx
-		shellCfg.Delimiter = cmdSetDelim
+		s.Prefix = cmdSetPfx
+		s.Suffix = cmdSetSfx
+		s.Delimiter = cmdSetDelim
 	case "emacs":
-		shellCfg.Prefix = emacsSetPfx
-		shellCfg.Suffix = emacsSetSfx
-		shellCfg.Delimiter = emacsSetDelim
+		s.Prefix = emacsSetPfx
+		s.Suffix = emacsSetSfx
+		s.Delimiter = emacsSetDelim
 	case "none":
-		shellCfg.Prefix = nonePfx
-		shellCfg.Suffix = noneSfx
-		shellCfg.Delimiter = noneDelim
-		shellCfg.UsageHint = ""
+		s.Prefix = nonePfx
+		s.Suffix = noneSfx
+		s.Delimiter = noneDelim
+		s.UsageHint = ""
 	default:
-		shellCfg.Prefix = bashSetPfx
-		shellCfg.Suffix = bashSetSfx
-		shellCfg.Delimiter = bashSetDelim
+		s.Prefix = bashSetPfx
+		s.Suffix = bashSetSfx
+		s.Delimiter = bashSetDelim
 	}
-
-	return shellCfg, nil
+	return s
 }
 
-func shellCfgUnset() (*ShellConfig, error) {
-
-	userShell, err := defaultShellDetector.GetShell(forceShell)
-	if err != nil {
-		return nil, err
+// shellCfgUnset generates context variables for "docker-env -u"
+func shellCfgUnset(ec EnvConfig) *ShellConfig {
+	s := &ShellConfig{
+		UsageHint: generateUsageHint(ec.profile, ec.shell),
 	}
 
-	shellCfg := &ShellConfig{
-		UsageHint: generateUsageHint(viper.GetString(config.MachineProfile), userShell),
+	if ec.noProxy {
+		s.NoProxyVar, s.NoProxyValue = defaultNoProxyGetter.GetNoProxyVar()
 	}
 
-	if noProxy {
-		shellCfg.NoProxyVar, shellCfg.NoProxyValue = defaultNoProxyGetter.GetNoProxyVar()
-	}
-
-	switch userShell {
+	switch ec.shell {
 	case "fish":
-		shellCfg.Prefix = fishUnsetPfx
-		shellCfg.Suffix = fishUnsetSfx
-		shellCfg.Delimiter = fishUnsetDelim
+		s.Prefix = fishUnsetPfx
+		s.Suffix = fishUnsetSfx
+		s.Delimiter = fishUnsetDelim
 	case "powershell":
-		shellCfg.Prefix = psUnsetPfx
-		shellCfg.Suffix = psUnsetSfx
-		shellCfg.Delimiter = psUnsetDelim
+		s.Prefix = psUnsetPfx
+		s.Suffix = psUnsetSfx
+		s.Delimiter = psUnsetDelim
 	case "cmd":
-		shellCfg.Prefix = cmdUnsetPfx
-		shellCfg.Suffix = cmdUnsetSfx
-		shellCfg.Delimiter = cmdUnsetDelim
+		s.Prefix = cmdUnsetPfx
+		s.Suffix = cmdUnsetSfx
+		s.Delimiter = cmdUnsetDelim
 	case "emacs":
-		shellCfg.Prefix = emacsUnsetPfx
-		shellCfg.Suffix = emacsUnsetSfx
-		shellCfg.Delimiter = emacsUnsetDelim
+		s.Prefix = emacsUnsetPfx
+		s.Suffix = emacsUnsetSfx
+		s.Delimiter = emacsUnsetDelim
 	case "none":
-		shellCfg.Prefix = nonePfx
-		shellCfg.Suffix = noneSfx
-		shellCfg.Delimiter = noneDelim
-		shellCfg.UsageHint = ""
+		s.Prefix = nonePfx
+		s.Suffix = noneSfx
+		s.Delimiter = noneDelim
+		s.UsageHint = ""
 	default:
-		shellCfg.Prefix = bashUnsetPfx
-		shellCfg.Suffix = bashUnsetSfx
-		shellCfg.Delimiter = bashUnsetDelim
+		s.Prefix = bashUnsetPfx
+		s.Suffix = bashUnsetSfx
+		s.Delimiter = bashUnsetDelim
 	}
-
-	return shellCfg, nil
-}
-
-func executeTemplateStdout(shellCfg *ShellConfig) error {
-	tmpl := template.Must(template.New("envConfig").Parse(envTmpl))
-	return tmpl.Execute(os.Stdout, shellCfg)
-}
-
-// GetShell detects the shell
-func (LibmachineShellDetector) GetShell(userShell string) (string, error) {
-	if userShell != "" {
-		return userShell, nil
-	}
-	return shell.Detect()
+	return s
 }
 
 // GetNoProxyVar gets the no_proxy var
@@ -307,30 +267,18 @@ func (EnvNoProxyGetter) GetNoProxyVar() (string, string) {
 	return noProxyVar, noProxyValue
 }
 
-// same as drivers.RunSSHCommandFromDriver, but allows errors
-func runSSHCommandFromDriver(d drivers.Driver, command string) (string, error) {
+// isDockerActive checks if Docker is active
+func isDockerActive(d drivers.Driver) (bool, error) {
 	client, err := drivers.GetSSHClientFromDriver(d)
 	if err != nil {
-		return "", err
+		return false, err
 	}
-
-	log.Debugf("About to run SSH command:\n%s", command)
-	output, err := client.Output(command)
-	log.Debugf("SSH cmd err, output: %v: %s", err, output)
-	return output, err
-}
-
-// same as host.RunSSHCommand, but allows errors
-func runSSHCommand(h *host.Host, command string) (string, error) {
-	return runSSHCommandFromDriver(h.Driver, command)
-}
-
-// GetDockerActive checks if Docker is active
-func GetDockerActive(host *host.Host) (bool, error) {
-	statusCmd := `sudo systemctl is-active docker`
-	status, err := runSSHCommand(host, statusCmd)
+	output, err := client.Output("sudo systemctl is-active docker")
+	if err != nil {
+		return false, err
+	}
 	// systemd returns error code on inactive
-	s := strings.TrimSpace(status)
+	s := strings.TrimSpace(output)
 	return err == nil && s == "active", nil
 }
 
@@ -345,7 +293,9 @@ var dockerEnvCmd = &cobra.Command{
 			exit.WithError("Error getting client", err)
 		}
 		defer api.Close()
-		cc, err := config.Load(viper.GetString(config.MachineProfile))
+
+		profile := viper.GetString(config.MachineProfile)
+		cc, err := config.Load(profile)
 		if err != nil {
 			exit.WithError("Error getting config", err)
 		}
@@ -356,45 +306,113 @@ var dockerEnvCmd = &cobra.Command{
 		if host.Driver.DriverName() == driver.None {
 			exit.UsageT(`'none' driver does not support 'minikube docker-env' command`)
 		}
+
 		hostSt, err := cluster.GetHostStatus(api, cc.Name)
 		if err != nil {
 			exit.WithError("Error getting host status", err)
 		}
 		if hostSt != state.Running.String() {
-			exit.WithCodeT(exit.Unavailable, `The docker host is currently not running`)
+			exit.WithCodeT(exit.Unavailable, `'{{.profile}}' is not running`, out.V{"profile": profile})
 		}
-		docker, err := GetDockerActive(host)
+		ok, err := isDockerActive(host.Driver)
 		if err != nil {
 			exit.WithError("Error getting service status", err)
 		}
-		if !docker {
-			exit.WithCodeT(exit.Unavailable, `The docker service is currently not active`)
+
+		if !ok {
+			exit.WithCodeT(exit.Unavailable, `The docker service within '{{.profile}}' is not active`, out.V{"profile": profile})
 		}
 
-		var shellCfg *ShellConfig
-
-		if unset {
-			shellCfg, err = shellCfgUnset()
-			if err != nil {
-				exit.WithError("Error unsetting shell variables", err)
-			}
-		} else {
-			shellCfg, err = shellCfgSet(api)
-			if err != nil {
-				exit.WithError("Error setting shell variables", err)
-			}
+		hostIP, err := host.Driver.GetIP()
+		if err != nil {
+			exit.WithError("Error getting host IP", err)
 		}
 
-		if err := executeTemplateStdout(shellCfg); err != nil {
-			exit.WithError("Error executing template", err)
+		ec := EnvConfig{
+			profile:  profile,
+			driver:   host.DriverName,
+			hostIP:   hostIP,
+			certsDir: localpath.MakeMiniPath("certs"),
+			noProxy:  viper.GetBool("no-proxy"),
+		}
+
+		sh := viper.GetString("shell")
+		fmt.Printf("user shell=%s\n", sh)
+		if sh == "" {
+			sh, err = shell.Detect()
+			if err != nil {
+				exit.WithError("Error detecting shell", err)
+			}
+		}
+		ec.shell = sh
+		fmt.Printf("shell=%s, ec: %+v\n", viper.GetString("shell"), ec)
+
+		if viper.GetBool("unset") {
+			if err := generateUnsetScript(ec, os.Stdout); err != nil {
+				exit.WithError("Error generating unset output", err)
+			}
+			return
+		}
+
+		if err := generateSetScript(ec, os.Stdout); err != nil {
+			exit.WithError("Error generating set output", err)
 		}
 	},
 }
 
+// EnvConfig encapsulates all external inputs into shell generation
+type EnvConfig struct {
+	profile  string
+	shell    string
+	driver   string
+	hostIP   string
+	certsDir string
+	noProxy  bool
+}
+
+// generateSetScript writes out a shell-compatible 'docker-env' script
+func generateSetScript(ec EnvConfig, w io.Writer) error {
+	tmpl := template.Must(template.New("envConfig").Parse(envTmpl))
+	envVars, err := dockerEnvVars(ec)
+	if err != nil {
+		return err
+	}
+	return tmpl.Execute(w, shellCfgSet(ec, envVars))
+}
+
+// generateSetScript writes out a shell-compatible 'docker-env unset' script
+func generateUnsetScript(ec EnvConfig, w io.Writer) error {
+	tmpl := template.Must(template.New("envConfig").Parse(envTmpl))
+	return tmpl.Execute(w, shellCfgUnset(ec))
+}
+
+// dockerURL returns a the docker endpoint URL for an ip/port pair.
+func dockerURL(ip string, port int) string {
+	return fmt.Sprintf("tcp://%s", net.JoinHostPort(ip, strconv.Itoa(port)))
+}
+
+// dockerEnvVars gets the necessary docker env variables to allow the use of docker through minikube's vm
+func dockerEnvVars(ec EnvConfig) (map[string]string, error) {
+	env := map[string]string{
+		constants.DockerTLSVerifyEnv:       "1",
+		constants.DockerHostEnv:            dockerURL(ec.hostIP, constants.DockerDaemonPort),
+		constants.DockerCertPathEnv:        ec.certsDir,
+		constants.MinikubeActiveDockerdEnv: ec.profile,
+	}
+
+	if driver.IsKIC(ec.driver) { // for kic we need to find out what port docker allocated during creation
+		port, err := oci.HostPortBinding(ec.driver, ec.profile, constants.DockerDaemonPort)
+		if err != nil {
+			return nil, errors.Wrapf(err, "get hostbind port for %d", constants.DockerDaemonPort)
+		}
+		env[constants.DockerCertPathEnv] = dockerURL(kic.DefaultBindIPV4, port)
+	}
+	return env, nil
+}
+
 func init() {
-	defaultShellDetector = &LibmachineShellDetector{}
 	defaultNoProxyGetter = &EnvNoProxyGetter{}
-	dockerEnvCmd.Flags().BoolVar(&noProxy, "no-proxy", false, "Add machine IP to NO_PROXY environment variable")
-	dockerEnvCmd.Flags().StringVar(&forceShell, "shell", "", "Force environment to be configured for a specified shell: [fish, cmd, powershell, tcsh, bash, zsh], default is auto-detect")
-	dockerEnvCmd.Flags().BoolVarP(&unset, "unset", "u", false, "Unset variables instead of setting them")
+	dockerEnvCmd.Flags().Bool("no-proxy", false, "Add machine IP to NO_PROXY environment variable")
+	dockerEnvCmd.Flags().StringVar(&forceShell, "shell", "auto", "Force environment to be configured for a specified shell: [fish, cmd, powershell, tcsh, bash, zsh], default is auto-detect")
+	dockerEnvCmd.Flags().BoolP("unset", "u", false, "Unset variables instead of setting them")
 }
