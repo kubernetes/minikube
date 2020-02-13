a34649b761d5faeefe372272d91001738013bcb9
Medya Gh <medya@google.com>
2020-02-12 19:32:28 -0800

    add per commit logs for windows too

--- hack/jenkins/minikube_set_pending.sh
+++ hack/jenkins/minikube_set_pending.sh
@@ -78,8 +78,9 @@ function retry_github_status() {
   done
 }
 
-for j in ${jobs[@]}; do
-  retry_github_status "${ghprbActualCommit}" "${j}" "pending" "${access_token}" \
-    "https://storage.googleapis.com/minikube-builds/logs/${ghprbPullId}/${j}.pending"
-done
+SHORT_COMMIT=${COMMIT:0:7}
+for j in ${jobs[@]}; do	for j in ${jobs[@]}; do
+  retry_github_status "${ghprbActualCommit}" "${j}" "pending" "${access_token}" \	  retry_github_status "${ghprbActualCommit}" "${j}" "pending" "${access_token}" \
+    "https://storage.googleapis.com/minikube-builds/logs/${ghprbPullId}/${j}.pending"	    "https://storage.googleapis.com/minikube-builds/logs/${ghprbPullId}/${SHORT_COMMIT}/${j}.pending"
+done	done
 
