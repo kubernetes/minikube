308d9163402e82604ffe41f41aad8f2d7d470c20
Medya Gh <medya@google.com>
2020-02-12 20:50:49 -0800

    linting

--- test/integration/functional_test.go
+++ test/integration/functional_test.go
@@ -118,11 +118,11 @@ func TestFunctional(t *testing.T) {
 
 // check functionality of minikube after evaling docker-env
 func validateDockerEnv(ctx context.Context, t *testing.T, profile string) {
-	mctx, cancel = context.WithTimeout(ctx, 13*time.Second)
+	mctx, cancel := context.WithTimeout(ctx, 13*time.Second)
 	defer cancel()
 	// we should be able to get minikube status with a bash which evaled docker-env
-	c = exec.CommandContext(mctx, "/bin/bash", "-c", "eval $("+Target()+" -p "+profile+" docker-env) && "+Target()+" status -p "+profile)
-	rr, err = Run(t, c)
+	c := exec.CommandContext(mctx, "/bin/bash", "-c", "eval $("+Target()+" -p "+profile+" docker-env) && "+Target()+" status -p "+profile)
+	rr, err := Run(t, c)
 	if err != nil {
 		t.Fatalf("Failed to do minikube status after eval-ing docker-env %s", err)
 	}
@@ -130,17 +130,17 @@ func validateDockerEnv(ctx context.Context, t *testing.T, profile string) {
 		t.Fatalf("Expected status output to include 'Running' after eval docker-env but got \n%s", rr.Output())
 	}
 
-	mctx, cancel := context.WithTimeout(ctx, 13*time.Second)
+	mctx, cancel = context.WithTimeout(ctx, 13*time.Second)
 	defer cancel()
 	// do a eval $(minikube -p profile docker-env) and check if we are point to docker inside minikube
-	c := exec.CommandContext(mctx, "/bin/bash", "-c", "eval $("+Target()+" -p "+profile+" docker-env) && docker images")
-	rr, err := Run(t, c)
+	c = exec.CommandContext(mctx, "/bin/bash", "-c", "eval $("+Target()+" -p "+profile+" docker-env) && docker images")
+	rr, err = Run(t, c)
 	if err != nil {
 		t.Fatalf("Failed to test eval docker-evn %s", err)
 	}
 
 	expectedImgInside := "gcr.io/k8s-minikube/storage-provisioner"
-	if !strings.Contains(rr.Output(), expectedContInside) {
+	if !strings.Contains(rr.Output(), expectedImgInside) {
 		t.Fatalf("Expected 'docker ps' to have %q from docker-daemon inside minikube. the docker ps output is:\n%q\n", expectedImgInside, rr.Output())
 	}
 
