3bccafb66bb644e2a77aaa0da93f20df1cc557d3
Ruben Baez <rubenbaez1995@gmail.com>
2020-02-19 20:23:06 -0500

    Update hyperv.go

--- pkg/minikube/registry/drvs/hyperv/hyperv.go
+++ pkg/minikube/registry/drvs/hyperv/hyperv.go
@@ -88,7 +88,7 @@ func status() registry.State {
 	// Allow no more than 2 seconds for querying state
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	
+
 	cmd := exec.CommandContext(ctx, path, "Get-WindowsOptionalFeature", "-FeatureName", "Microsoft-Hyper-V-All", "-Online")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
