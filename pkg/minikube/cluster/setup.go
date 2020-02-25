05bcb0c0bffabf1c5592acb64cb4ae20ec755cba
Sharif Elgamal <selgamal@google.com>
2020-02-24 17:39:41 -0800

    prepareNone was in the wrong spot

--- pkg/minikube/cluster/setup.go
+++ pkg/minikube/cluster/setup.go
@@ -43,6 +43,7 @@ import (
 	"k8s.io/minikube/pkg/minikube/machine"
 	"k8s.io/minikube/pkg/minikube/out"
 	"k8s.io/minikube/pkg/minikube/proxy"
+	"k8s.io/minikube/pkg/util"
 	"k8s.io/minikube/pkg/util/retry"
 )
 
@@ -81,6 +82,12 @@ func InitialSetup(cc config.ClusterConfig, n config.Node, existingAddons map[str
 	// enable addons, both old and new!
 	if existingAddons != nil {
 		addons.Start(viper.GetString(config.MachineProfile), existingAddons, config.AddonList)
+	}
+
+	// special ops for none , like change minikube directory.
+	// multinode super doesn't work on the none driver
+	if cc.Driver == driver.None && len(cc.Nodes) == 1 {
+		prepareNone()
 	}
 
 	// Skip pre-existing, because we already waited for health
@@ -285,5 +292,34 @@ func tryRegistry(r command.Runner) {
 	if rr, err := r.RunCmd(exec.Command("curl", opts...)); err != nil {
 		glog.Warningf("%s failed: %v", rr.Args, err)
 		out.WarningT("VM is unable to access {{.repository}}, you may need to configure a proxy or set --image-repository", out.V{"repository": repo})
+	}
+}
+
+// prepareNone prepares the user and host for the joy of the "none" driver
+func prepareNone() {
+	out.T(out.StartingNone, "Configuring local host environment ...")
+	if viper.GetBool(config.WantNoneDriverWarning) {
+		out.T(out.Empty, "")
+		out.WarningT("The 'none' driver provides limited isolation and may reduce system security and reliability.")
+		out.WarningT("For more information, see:")
+		out.T(out.URL, "https://minikube.sigs.k8s.io/docs/reference/drivers/none/")
+		out.T(out.Empty, "")
+	}
+
+	if os.Getenv("CHANGE_MINIKUBE_NONE_USER") == "" {
+		home := os.Getenv("HOME")
+		out.WarningT("kubectl and minikube configuration will be stored in {{.home_folder}}", out.V{"home_folder": home})
+		out.WarningT("To use kubectl or minikube commands as your own user, you may need to relocate them. For example, to overwrite your own settings, run:")
+
+		out.T(out.Empty, "")
+		out.T(out.Command, "sudo mv {{.home_folder}}/.kube {{.home_folder}}/.minikube $HOME", out.V{"home_folder": home})
+		out.T(out.Command, "sudo chown -R $USER $HOME/.kube $HOME/.minikube")
+		out.T(out.Empty, "")
+
+		out.T(out.Tip, "This can also be done automatically by setting the env var CHANGE_MINIKUBE_NONE_USER=true")
+	}
+
+	if err := util.MaybeChownDirRecursiveToMinikubeUser(localpath.MiniPath()); err != nil {
+		exit.WithCodeT(exit.Permissions, "Failed to change permissions for {{.minikube_dir_path}}: {{.error}}", out.V{"minikube_dir_path": localpath.MiniPath(), "error": err})
 	}
 }
