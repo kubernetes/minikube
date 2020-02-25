39f03bc925a0d7057c4bc2d1a8d93cc92a3fb32d
Sharif Elgamal <selgamal@google.com>
2020-02-24 17:39:41 -0800

    prepareNone was in the wrong spot

--- pkg/minikube/node/start.go
+++ pkg/minikube/node/start.go
@@ -17,17 +17,13 @@ limitations under the License.
 package node
 
 import (
-	"os"
-
 	"github.com/spf13/viper"
 	"golang.org/x/sync/errgroup"
 	cmdcfg "k8s.io/minikube/cmd/minikube/cmd/config"
 	"k8s.io/minikube/pkg/addons"
 	"k8s.io/minikube/pkg/minikube/cluster"
 	"k8s.io/minikube/pkg/minikube/config"
-	"k8s.io/minikube/pkg/minikube/driver"
 	"k8s.io/minikube/pkg/minikube/exit"
-	"k8s.io/minikube/pkg/minikube/localpath"
 	"k8s.io/minikube/pkg/minikube/out"
 	"k8s.io/minikube/pkg/util"
 )
@@ -75,12 +71,6 @@ func Start(cc config.ClusterConfig, n config.Node, existingAddons map[string]boo
 		out.T(out.FailureType, "Unable to load cached images from config file.")
 	}
 
-	// special ops for none , like change minikube directory.
-	// multinode super doesn't work on the none driver
-	if driverName == driver.None && len(cc.Nodes) == 1 {
-		prepareNone()
-	}
-
 	// Skip pre-existing, because we already waited for health
 	if viper.GetBool(waitUntilHealthy) && !preExists {
 		if err := bs.WaitForCluster(cc, viper.GetDuration(waitTimeout)); err != nil {
@@ -106,33 +96,4 @@ func Start(cc config.ClusterConfig, n config.Node, existingAddons map[string]boo
 		exit.WithError("generating join token", err)
 	}
 	return bs.JoinCluster(cc, n, joinCmd)
-}
-
-// prepareNone prepares the user and host for the joy of the "none" driver
-func prepareNone() {
-	out.T(out.StartingNone, "Configuring local host environment ...")
-	if viper.GetBool(config.WantNoneDriverWarning) {
-		out.T(out.Empty, "")
-		out.WarningT("The 'none' driver provides limited isolation and may reduce system security and reliability.")
-		out.WarningT("For more information, see:")
-		out.T(out.URL, "https://minikube.sigs.k8s.io/docs/reference/drivers/none/")
-		out.T(out.Empty, "")
-	}
-
-	if os.Getenv("CHANGE_MINIKUBE_NONE_USER") == "" {
-		home := os.Getenv("HOME")
-		out.WarningT("kubectl and minikube configuration will be stored in {{.home_folder}}", out.V{"home_folder": home})
-		out.WarningT("To use kubectl or minikube commands as your own user, you may need to relocate them. For example, to overwrite your own settings, run:")
-
-		out.T(out.Empty, "")
-		out.T(out.Command, "sudo mv {{.home_folder}}/.kube {{.home_folder}}/.minikube $HOME", out.V{"home_folder": home})
-		out.T(out.Command, "sudo chown -R $USER $HOME/.kube $HOME/.minikube")
-		out.T(out.Empty, "")
-
-		out.T(out.Tip, "This can also be done automatically by setting the env var CHANGE_MINIKUBE_NONE_USER=true")
-	}
-
-	if err := util.MaybeChownDirRecursiveToMinikubeUser(localpath.MiniPath()); err != nil {
-		exit.WithCodeT(exit.Permissions, "Failed to change permissions for {{.minikube_dir_path}}: {{.error}}", out.V{"minikube_dir_path": localpath.MiniPath(), "error": err})
-	}
 }
