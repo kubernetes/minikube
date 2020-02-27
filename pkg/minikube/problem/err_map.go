dc5cd606aebbeb726abd3c7a0c4b0a6ef8800b64
Csongor Halmai <csongor@halmai.hu>
2020-02-27 21:04:03 +1100

    give hint if 'minikube delete' fails deleting the machine directory

--- pkg/minikube/problem/err_map.go
+++ pkg/minikube/problem/err_map.go
@@ -109,6 +109,12 @@ var vmProblems = map[string]match{
 		Issues: []int{4511},
 		GOOS:   []string{"windows"},
 	},
+	"HYPERV_FILE_DELETE_FAILURE": {
+		Regexp: re(`Unable to remove machine directory`),
+		Advice: "Stop the  Hyper-V Manager and and run 'minikube delete'.",
+		Issues: []int{6804},
+		GOOS:   []string{"windows"},
+	},
 
 	// KVM
 	"KVM2_NOT_FOUND": {
