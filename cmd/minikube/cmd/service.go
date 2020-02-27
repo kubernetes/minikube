2cdb9056685b88895f33306fea7310085133ecdb
Jose Donizetti <jdbjunior@gmail.com>
2020-02-26 22:16:50 -0300

    kic on mac: add service warn

--- cmd/minikube/cmd/service.go
+++ cmd/minikube/cmd/service.go
@@ -175,6 +175,8 @@ func openURLs(svc string, urls []string) {
 		if err := browser.OpenURL(u); err != nil {
 			exit.WithError(fmt.Sprintf("open url failed: %s", u), err)
 		}
+
+		out.T(out.WarningType, "Because you are using docker driver on Mac, the terminal needs to be open to run it.")
 	}
 
 }
