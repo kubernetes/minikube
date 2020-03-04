e2ce45c6f0ca9fe51bd37b2fd478ec4ec69dd181
Sharif Elgamal <selgamal@google.com>
2020-03-04 13:32:43 -0800

    Switch --vm-driver paramter to --driver

--- cmd/minikube/cmd/config/util_test.go
+++ cmd/minikube/cmd/config/util_test.go
@@ -25,7 +25,7 @@ import (
 )
 
 var minikubeConfig = pkgConfig.MinikubeConfig{
-	"vm-driver":            driver.KVM2,
+	"driver":               driver.KVM2,
 	"cpus":                 12,
 	"show-libmachine-logs": true,
 }
@@ -38,17 +38,17 @@ func TestFindSettingNotFound(t *testing.T) {
 }
 
 func TestFindSetting(t *testing.T) {
-	s, err := findSetting("vm-driver")
+	s, err := findSetting("driver")
 	if err != nil {
-		t.Fatalf("Couldn't find setting, vm-driver: %v", err)
+		t.Fatalf("Couldn't find setting, driver: %v", err)
 	}
-	if s.name != "vm-driver" {
-		t.Fatalf("Found wrong setting, expected vm-driver, got %s", s.name)
+	if s.name != "driver" {
+		t.Fatalf("Found wrong setting, expected driver, got %s", s.name)
 	}
 }
 
 func TestSetString(t *testing.T) {
-	err := SetString(minikubeConfig, "vm-driver", driver.VirtualBox)
+	err := SetString(minikubeConfig, "driver", driver.VirtualBox)
 	if err != nil {
 		t.Fatalf("Couldn't set string: %v", err)
 	}
