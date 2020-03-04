3c55e95882950201cd7c7a1a67179da2d358eb61
Pierre Ugaz <ulm0@innersea.xyz>
2020-03-04 01:56:36 -0300

    add component configs for kubeadm v1beta2
    Signed-off-by: Pierre Ugaz <ulm0@innersea.xyz>

--- pkg/minikube/bootstrapper/bsutil/ktmpl/v1beta2.go
+++ pkg/minikube/bootstrapper/bsutil/ktmpl/v1beta2.go
@@ -69,4 +69,16 @@ networking:
   dnsDomain: {{if .DNSDomain}}{{.DNSDomain}}{{else}}cluster.local{{end}}
   podSubnet: "{{.PodSubnet }}"
   serviceSubnet: {{.ServiceCIDR}}
+---
+apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+imageGCHighThresholdPercent: 100
+evictionHard:
+  nodefs.available: "0%"
+  nodefs.inodesFree: "0%"
+  imagefs.available: "0%"
+---
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+kind: KubeProxyConfiguration
+metricsBindAddress: {{.AdvertiseAddress}}:10249
 `))
