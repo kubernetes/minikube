35bc36418bf7271fdd15f6653c04b820072fa2fd
Medya Gh <medya@google.com>
2020-03-10 11:39:55 -0700

    lint

--- test/integration/version_upgrade_test.go
+++ test/integration/version_upgrade_test.go
@@ -170,7 +170,7 @@ func TestVersionUpgradeV1(t *testing.T) {
 		}
 	}
 
-	args := append([]string{"start", "-p", profile, "--memory=2200", fmt.Sprintf("--iso-url=%s", oldISO), fmt.Sprintf("--kubernetes-version=%s", constants.OldestKubernetesVersion), "--alsologtostderr", "-v=1"}, StartArgs()...)
+	args := append([]string{"start", "-p", profile}, StartArgs()...)
 	rr := &RunResult{}
 	r := func() error {
 		rr, err = Run(t, exec.CommandContext(ctx, tf.Name(), args...))
@@ -181,7 +181,7 @@ func TestVersionUpgradeV1(t *testing.T) {
 	if err := retry.Expo(r, 13*time.Second, Minutes(30), 2); err != nil {
 		t.Fatalf("v1.0.0 start failed: %v", err)
 	}
-	rr, err := Run(t, exec.CommandContext(ctx, tf.Name(), "stop", "-p", profile))
+	rr, err = Run(t, exec.CommandContext(ctx, tf.Name(), "stop", "-p", profile))
 	if err != nil {
 		t.Fatalf("%s failed: %v", rr.Args, err)
 	}
