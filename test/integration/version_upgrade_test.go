6eb51e002d7707210e5d61253b65af9cce53090a
Medya Gh <medya@google.com>
2020-03-10 10:07:36 -0700

    add new test from upgrading from minikube v1.0.0

--- test/integration/version_upgrade_test.go
+++ test/integration/version_upgrade_test.go
@@ -33,16 +33,20 @@ import (
 	"k8s.io/minikube/pkg/minikube/constants"
 	"k8s.io/minikube/pkg/util/retry"
 
+	"github.com/blang/semver"
 	"github.com/hashicorp/go-getter"
 	pkgutil "k8s.io/minikube/pkg/util"
 )
 
-// TestVersionUpgrade downloads the latest version of minikube and runs with
-// the oldest supported k8s version and then runs the current head minikube
-// and tries to upgrade from the oldest supported k8s to newest supported k8s
-func TestVersionUpgrade(t *testing.T) {
+// TestVersionUpgradeLatest is a comprehensive combination of tests
+// downloads the current latest release binary from https://github.com/kubernetes/minikube/releases/latest
+// and starts it with:  a custom ISO-URL and oldest supported kuberentes version
+// and then stops it
+// and then starts with head minikube binary with oldest supported minikube
+// and then starts with head minikube binary with oldest newest minikube
+func TestVersionUpgradeLatest(t *testing.T) {
 	MaybeParallel(t)
-	profile := UniqueProfileName("vupgrade")
+	profile := UniqueProfileName("vupgrade-latest")
 	ctx, cancel := context.WithTimeout(context.Background(), Minutes(55))
 
 	defer CleanupWithLogs(t, profile, cancel)
@@ -134,5 +138,74 @@ func TestVersionUpgrade(t *testing.T) {
 	rr, err = Run(t, exec.CommandContext(ctx, Target(), args...))
 	if err != nil {
 		t.Errorf("%s failed: %v", rr.Args, err)
+	}
+}
+
+// TestVersionUpgradeV1
+// downloads the minikube v1.0.0 binary and starts it
+// stops it
+// and then starts with head minikube binary
+func TestVersionUpgradeV1(t *testing.T) {
+	MaybeParallel(t)
+	profile := UniqueProfileName("vupgrade-v1")
+	ctx, cancel := context.WithTimeout(context.Background(), Minutes(55))
+
+	defer CleanupWithLogs(t, profile, cancel)
+
+	tf, err := ioutil.TempFile("", "minikube-v1-release.*.exe")
+	if err != nil {
+		t.Fatalf("tempfile: %v", err)
+	}
+	defer os.Remove(tf.Name())
+	tf.Close()
+
+	url := pkgutil.GetBinaryDownloadURL("v1.0.0", runtime.GOOS)
+	if err := retry.Expo(func() error { return getter.GetFile(tf.Name(), url) }, 3*time.Second, Minutes(3)); err != nil {
+		t.Fatalf("get failed: %v", err)
+	}
+
+	if runtime.GOOS != "windows" {
+		if err := os.Chmod(tf.Name(), 0700); err != nil {
+			t.Errorf("chmod: %v", err)
+		}
+	}
+
+	args := append([]string{"start", "-p", profile, "--alsologtostderr", "-v=1"}, StartArgs()...)
+	rr, err := Run(t, exec.CommandContext(ctx, tf.Name(), "stop", "-p", profile))
+	if err != nil {
+		t.Fatalf("%s failed: %v", rr.Args, err)
+	}
+
+	rr, err = Run(t, exec.CommandContext(ctx, tf.Name(), "-p", profile, "status", "--format={{.Host}}"))
+	if err != nil {
+		t.Logf("status error: %v (may be ok)", err)
+	}
+	got := strings.TrimSpace(rr.Stdout.String())
+	if got != state.Stopped.String() {
+		t.Errorf("status = %q; want = %q", got, state.Stopped.String())
+	}
+
+	args = append([]string{"start", "-p", profile, "--alsologtostderr", "-v=1"}, StartArgs()...)
+	rr, err = Run(t, exec.CommandContext(ctx, Target(), args...))
+	if err != nil {
+		t.Errorf("%s failed: %v", rr.Args, err)
+	}
+
+	s, err := Run(t, exec.CommandContext(ctx, "kubectl", "--context", profile, "version", "--output=json"))
+	if err != nil {
+		t.Fatalf("error running kubectl: %v", err)
+	}
+	cv := struct {
+		ServerVersion struct {
+			GitVersion string `json:"gitVersion"`
+		} `json:"serverVersion"`
+	}{}
+	err = json.Unmarshal(s.Stdout.Bytes(), &cv)
+
+	if err != nil {
+		t.Fatalf("error traversing json output: %v", err)
+	}
+	if _, err = semver.Parse(cv.ServerVersion.GitVersion); err != nil {
+		t.Fatalf("err parsing the kubernets version %v", err)
 	}
 }
