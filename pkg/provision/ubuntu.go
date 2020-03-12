613e51fea423cb3cb514dcf325877c891936259d
Medya Gh <medya@google.com>
2020-03-12 14:28:37 -0700

    try

--- pkg/provision/ubuntu.go
+++ pkg/provision/ubuntu.go
@@ -155,11 +155,11 @@ WantedBy=multi-user.target
 		return nil, err
 	}
 
-	if err := p.Service("docker", serviceaction.DaemonReload); err != nil {
+	if err := p.Service("", serviceaction.DaemonReload); err != nil {
 		return nil, err
 	}
 
-	if err := p.Service("docker", serviceaction.Enable); err != nil {
+	if err := p.Service("", serviceaction.Enable); err != nil {
 		return nil, err
 	}
 
