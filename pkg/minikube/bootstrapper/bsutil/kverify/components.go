b7087c4cb253c2b07ddbc766f83d2fbc84ca4b1a
Medya Gh <medya@google.com>
2020-03-24 08:19:29 -0700

    fix import

--- pkg/minikube/bootstrapper/bsutil/kverify/components.go
+++ pkg/minikube/bootstrapper/bsutil/kverify/components.go
@@ -23,11 +23,11 @@ import (
 	"time"
 
 	"github.com/golang/glog"
+	core "k8s.io/api/core/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	kconst "k8s.io/kubernetes/cmd/kubeadm/app/constants"
-	"k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/minikube/pkg/minikube/bootstrapper"
 	"k8s.io/minikube/pkg/minikube/command"
 	"k8s.io/minikube/pkg/minikube/config"
