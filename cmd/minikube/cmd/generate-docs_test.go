c409c4c91c3683c3e25a9e259b78dd25612a8363
Priya Wadhwa <priyawadhwa@google.com>
2020-04-01 16:13:54 -0700

    Add unit tests to make sure docs are up to date

---
+++ cmd/minikube/cmd/generate-docs_test.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2020 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"k8s.io/minikube/pkg/generate"
+)
+
+func TestCommandsDocs(t *testing.T) {
+	dir := "../../../site/content/en/docs/Reference/Commands/"
+
+	for _, sc := range RootCmd.Commands() {
+		t.Run(sc.Name(), func(t *testing.T) {
+			fp := filepath.Join(dir, fmt.Sprintf("%s.md", sc.Name()))
+			expectedContents, err := ioutil.ReadFile(fp)
+			if err != nil {
+				t.Fatalf("Docs are not updated. Please run `make generate-docs` to update commands documentation: %v", err)
+			}
+			actualContents, err := generate.DocForCommand(sc)
+			if err != nil {
+				t.Fatalf("error getting contents: %v", err)
+			}
+			if string(actualContents) != string(expectedContents) {
+				t.Fatalf("Docs are not updated. Please run `make generate-docs` to update commands documentation.")
+			}
+		})
+	}
+}
