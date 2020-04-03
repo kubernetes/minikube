f87350344d0846860a0e063a0fda7b805df307a6
Medya Gh <medya@google.com>
2020-04-02 17:19:45 -0700

    improve messge

--- pkg/addons/addons.go
+++ pkg/addons/addons.go
@@ -126,7 +126,9 @@ func enableOrDisableAddon(cc *config.ClusterConfig, name string, val string) err
 	// to match both ingress and ingress-dns adons
 	if strings.HasPrefix(name, "ingress") && enable && driver.IsKIC(cc.Driver) && runtime.GOOS != "linux" {
 		exit.UsageT(`Due to {{.driver_name}} networking limitations on {{.os_name}}, {{.addon_name}} addon is not supported for this driver.
-you can use a different driver such as hyperkit or virtualbox or hypev.
+Alternatively to use this addon you can use a vm-based driver:
+
+		'minikube start --vm=true'
 
 To track the update on this work in progress feature please check:
 https://github.com/kubernetes/minikube/issues/7332`, out.V{"driver_name": cc.Driver, "os_name": runtime.GOOS, "addon_name": name})
