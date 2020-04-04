3a32ce67106a58dc393285280bed310e18d58014
Thomas Stromberg <tstromberg@google.com>
2020-04-04 13:24:31 -0700

    Improve test

--- test/integration/no_error_shown_test.go
+++ test/integration/no_error_shown_test.go
@@ -25,38 +25,45 @@ import (
 	"testing"
 )
 
-// TestNoErrorShown asserts that there are no errors displayed
+// TestMultiNodeNoErrorShown asserts that there are no errors displayed
 func TestNoErrorShown(t *testing.T) {
 	if NoneDriver() {
 		t.Skip("none driver always shows a warning")
 	}
 	MaybeParallel(t)
 
-	if NeedsPortForward() {
-		t.Skip("Docker for Desktop can be noisy")
-	}
-
-	profile := UniqueProfileName("no-error")
-	ctx, cancel := context.WithTimeout(context.Background(), Minutes(20))
+	profile := UniqueProfileName("nospam")
+	ctx, cancel := context.WithTimeout(context.Background(), Minutes(25))
 	defer CleanupWithLogs(t, profile, cancel)
 
 	// TODO: make this multi-node once it's stable
-	args := append([]string{"start", "-p", profile, "--memory=1900", "--wait=false"}, StartArgs()...)
+	args := append([]string{"start", "-p", profile, "-n=1", "--memory=2250", "--wait=false"}, StartArgs()...)
 
 	rr, err := Run(t, exec.CommandContext(ctx, Target(), args...))
 	if err != nil {
 		t.Errorf("failed to start minikube with args: %q : %v", rr.Command(), err)
 	}
 
-	if rr.Stderr.Len() > 0 {
-		t.Errorf("unexpected stderr: %v", rr.Stderr.String())
+	for _, line := range strings.Split(rr.Stderr.String(), "\n") {
+		if strings.HasPrefix(line, "E") {
+			t.Errorf("unexpected error log in stderr: %q", line)
+			continue
+		}
+
+		if strings.Contains(line, "kubectl") || strings.Contains(line, "slow") || strings.Contains(line, "long time") {
+			continue
+		}
+		if len(strings.TrimSpace(line)) > 0 {
+			t.Errorf("unexpected stderr line: %q", line)
+		}
 	}
 
-	stdout := rr.Stdout.String()
-	keywords := []string{"error", "fail", "warning", "conflict"}
-	for _, keyword := range keywords {
-		if strings.Contains(stdout, keyword) {
-			t.Errorf("unexpected %q in stdout: %s", keyword, stdout)
+	for _, line := range strings.Split(rr.Stdout.String(), "\n") {
+		keywords := []string{"error", "fail", "warning", "conflict"}
+		for _, keyword := range keywords {
+			if strings.Contains(line, keyword) {
+				t.Errorf("unexpected %q in stdout line: %q", keyword, line)
+			}
 		}
 	}
 }
