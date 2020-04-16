0687e2adc0f71524f2a2e042653d9f8ea9afa39c
Medya Gh <medya@google.com>
2020-04-16 13:39:59 -0700

    defer print addon

--- pkg/addons/addons.go
+++ pkg/addons/addons.go
@@ -332,7 +332,7 @@ func Start(wg *sync.WaitGroup, cc *config.ClusterConfig, toEnable map[string]boo
 
 	var awg sync.WaitGroup
 
-	defer func() { // making it show after verifications
+	defer func() { // making it show after verifications( not perfect till #7613 is closed)
 		out.T(out.AddonEnable, "Enabling addons: {{.addons}}", out.V{"addons": strings.Join(toEnableList, ", ")})
 	}()
 	for _, a := range toEnableList {
