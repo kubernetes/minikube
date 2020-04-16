5ceb7352630b18efbf7a483175c81f8f0f06f9a2
Priya Wadhwa <priyawadhwa@google.com>
2020-04-16 12:19:13 -0700

    reintroduce unit test

--- pkg/minikube/perf/start_test.go
+++ pkg/minikube/perf/start_test.go
@@ -17,9 +17,66 @@ limitations under the License.
 package perf
 
 import (
+	"bytes"
+	"context"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
+func mockCollectTimes(times [][]float64) func(ctx context.Context, binaries []*Binary) ([][]float64, error) {
+	return func(ctx context.Context, binaries []*Binary) ([][]float64, error) {
+		return times, nil
+	}
+}
+
+func TestCompareMinikubeStartOutput(t *testing.T) {
+	binaries := []*Binary{
+		{
+			path: "minikube1",
+		}, {
+			path: "minikube2",
+		},
+	}
+	tests := []struct {
+		description string
+		times       [][]float64
+		expected    string
+	}{
+		{
+			description: "standard run",
+			times:       [][]float64{{4.5, 6}, {1, 2}},
+			expected: `Results for minikube1:
+Times: [4.5 6]
+Average Time: 5.250000
+
+Results for minikube2:
+Times: [1 2]
+Average Time: 1.500000
+
+`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			originalCollectTimes := collectTimes
+			collectTimeMinikubeStart = mockCollectTimes(test.times)
+			defer func() { collectTimeMinikubeStart = originalCollectTimes }()
+
+			buf := bytes.NewBuffer([]byte{})
+			err := CompareMinikubeStart(context.Background(), buf, binaries)
+			if err != nil {
+				t.Fatalf("error comparing minikube start: %v", err)
+			}
+
+			actual := buf.String()
+			if diff := cmp.Diff(test.expected, actual); diff != "" {
+				t.Errorf("machines mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
 func TestAverage(t *testing.T) {
 	tests := []struct {
 		description string
