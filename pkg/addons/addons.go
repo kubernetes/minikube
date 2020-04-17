7e892f658faeae3713a3d2cfb0f3e62c3e337cbe
Medya Gh <medya@google.com>
2020-04-17 14:07:41 -0700

    address review comments

--- pkg/addons/addons.go
+++ pkg/addons/addons.go
@@ -333,7 +333,7 @@ func Start(wg *sync.WaitGroup, cc *config.ClusterConfig, toEnable map[string]boo
 	var awg sync.WaitGroup
 
 	defer func() { // making it show after verifications( not perfect till #7613 is closed)
-		out.T(out.AddonEnable, "Enabling addons: {{.addons}}", out.V{"addons": strings.Join(toEnableList, ", ")})
+		out.T(out.AddonEnable, "Enabled addons: {{.addons}}", out.V{"addons": strings.Join(toEnableList, ", ")})
 	}()
 	for _, a := range toEnableList {
 		awg.Add(1)
