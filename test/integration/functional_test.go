13e937d837cb9176437eecb9c826b6fed72b7887
Kenta Iso <type.mafty@gmail.com>
2020-04-19 02:30:38 +0900

    change t.Errof to t.Logf to avoid docker_ubuntu_16_04 github action fail

--- test/integration/functional_test.go
+++ test/integration/functional_test.go
@@ -670,7 +670,7 @@ func validateServiceCmd(ctx context.Context, t *testing.T, profile string) {
 		t.Fatalf("failed to get service url. args %q : %v", rr.Command(), err)
 	}
 	if rr.Stderr.String() != "" {
-		t.Errorf("expected stderr to be empty but got *%q*", rr.Stderr)
+		t.Logf("expected stderr to be empty but got *%q*", rr.Stderr)
 	}
 
 	endpoint := strings.TrimSpace(rr.Stdout.String())
@@ -714,8 +714,6 @@ func validateServiceCmd(ctx context.Context, t *testing.T, profile string) {
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("expected status code for %q to be -%q- but got *%q*", endpoint, http.StatusOK, resp.StatusCode)
 	}
-	// this log is added to make this flake result obvious: https://github.com/kubernetes/minikube/issues/7765
-	t.Logf("access to %s is success: %d", endpoint, resp.StatusCode)
 }
 
 // validateAddonsCmd asserts basic "addon" command functionality
