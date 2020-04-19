a3e1c506dd38a21f074ba0e7e2ec6e9646556fca
Alonyb <rubenbaez1995@gmail.com>
2020-04-19 08:41:33 -0500

    fix test

--- pkg/minikube/kubeconfig/kubeconfig_test.go
+++ pkg/minikube/kubeconfig/kubeconfig_test.go
@@ -448,12 +448,7 @@ func tempFile(t *testing.T, data []byte) string {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer func() { //clean up tempdir
-		err := os.RemoveAll(tmp.Name())
-		if err != nil {
-			t.Errorf("failed to clean up temp folder  %q", tmp.Name())
-		}
-	}()
+
 	if len(data) > 0 {
 		if _, err := tmp.Write(data); err != nil {
 			t.Fatal(err)
