546540cbbb03b056b99dd1fccf0eb74fabfea558
Radoslaw Smigielski <radoslaw.smigielski@nokia.com>
2020-04-22 05:10:18 +0100

    Fix the broken line

--- cmd/minikube/cmd/start.go
+++ cmd/minikube/cmd/start.go
@@ -997,9 +997,7 @@ func getKubernetesVersion(old *config.ClusterConfig) string {
 
 	}
 	if defaultVersion.GT(nvs) {
-        out.T(out.New,
-              "Kubernetes {{.new}} is now available. If you would like to upgrade, specify: --kubernetes-version={{.new}}",
-              out.V{"new": defaultVersion})
+        out.T(out.New, "Kubernetes {{.new}} is now available. If you would like to upgrade, specify: --kubernetes-version={{.new}}", out.V{"new": defaultVersion})
 	}
 	return nv
 }
