2d49c3b6111c038cf7dcd5f3d60220074b03956b
Alonyb <rubenbaez1995@gmail.com>
2020-04-23 23:35:57 -0500

    restore types.go

--- pkg/drivers/kic/types.go
+++ pkg/drivers/kic/types.go
@@ -18,7 +18,6 @@ package kic
 
 import (
 	"fmt"
-
 	"k8s.io/minikube/pkg/drivers/kic/oci"
 )
 
@@ -40,8 +39,7 @@ const (
 
 var (
 	// BaseImage is the base image is used to spin up kic containers. it uses same base-image as kind.
-	//BaseImage = fmt.Sprintf("gcr.io/k8s-minikube/kicbase:%s@sha256:%s", Version, baseImageSHA)
-	BaseImage = fmt.Sprintf("kicbase-ruben:v3.3.3")
+	BaseImage = fmt.Sprintf("gcr.io/k8s-minikube/kicbase:%s@sha256:%s", Version, baseImageSHA)
 )
 
 // Config is configuration for the kic driver used by registry
