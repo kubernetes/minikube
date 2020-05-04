d4303687d2621fcca2c69fe151a2545744bbcbb6
Sharif Elgamal <selgamal@google.com>
2020-05-04 16:50:31 -0700

    force systemd for start-stop test

--- test/integration/start_stop_delete_test.go
+++ test/integration/start_stop_delete_test.go
@@ -97,6 +97,7 @@ func TestStartStop(t *testing.T) {
 				startArgs := append([]string{"start", "-p", profile, "--memory=2200", "--alsologtostderr", waitFlag}, tc.args...)
 				startArgs = append(startArgs, StartArgs()...)
 				startArgs = append(startArgs, fmt.Sprintf("--kubernetes-version=%s", tc.version))
+				startArgs = append(startArgs, "--force-systemd")
 
 				rr, err := Run(t, exec.CommandContext(ctx, Target(), startArgs...))
 				if err != nil {
