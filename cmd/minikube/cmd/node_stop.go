16590bcbee00b40cec353da387a4ea8f74c9f68d
Sharif Elgamal <selgamal@google.com>
2020-05-11 21:59:03 -0700

    improve node stop output

--- cmd/minikube/cmd/node_stop.go
+++ cmd/minikube/cmd/node_stop.go
@@ -49,6 +49,8 @@ var nodeStopCmd = &cobra.Command{
 		if err != nil {
 			out.FatalT("Failed to stop node {{.name}}", out.V{"name": name})
 		}
+
+		out.T(out.Stopped, "Successfully stopped node {{.name}}", out.V{"name": name})
 	},
 }
 
