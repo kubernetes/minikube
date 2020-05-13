d885da29fc5cd277bd840a67852a3ee77b7ae9c8
Medya Gh <medya@google.com>
2020-05-12 19:33:16 -0700

    dont change oldest

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -31,7 +31,7 @@ const (
 	// NewestKubernetesVersion is the newest Kubernetes version to test against
 	NewestKubernetesVersion = "v1.18.3-beta.0"
 	// OldestKubernetesVersion is the oldest Kubernetes version to test against
-	OldestKubernetesVersion = "v1.12.10"
+	OldestKubernetesVersion = "v1.12.0"
 	// DefaultClusterName is the default nane for the k8s cluster
 	DefaultClusterName = "minikube"
 	// DockerDaemonPort is the port Docker daemon listening inside a minikube node (vm or container).
