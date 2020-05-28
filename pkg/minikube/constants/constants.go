4b280ab362b5d557dbd4ba30884d8d048e3aaf57
Medya Gh <medya@google.com>
2020-05-28 13:44:57 -0700

    bump default kubernetes version

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -27,9 +27,9 @@ import (
 
 const (
 	// DefaultKubernetesVersion is the default Kubernetes version
-	DefaultKubernetesVersion = "v1.18.2"
+	DefaultKubernetesVersion = "v1.18.3"
 	// NewestKubernetesVersion is the newest Kubernetes version to test against
-	NewestKubernetesVersion = "v1.18.3-beta.0"
+	NewestKubernetesVersion = "v1.18.4-rc.0"
 	// OldestKubernetesVersion is the oldest Kubernetes version to test against
 	OldestKubernetesVersion = "v1.12.0"
 	// DefaultClusterName is the default nane for the k8s cluster
