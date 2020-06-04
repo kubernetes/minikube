15088526cd6b145a64716aab73937718911a960e
Priya Wadhwa <priyawadhwa@google.com>
2020-06-04 11:05:52 -0700

    clean up comments

--- pkg/drivers/kic/types.go
+++ pkg/drivers/kic/types.go
@@ -38,11 +38,11 @@ var (
 
 	// FallbackImages are backup base images in case gcr isn't available
 	FallbackImages = []string{
-		// BaseImageFallBack1 the fall back of BaseImage in case gcr.io is not available. stored in docker hub
+		// the fallback of BaseImage in case gcr.io is not available. stored in docker hub
 		// same image is push to https://github.com/kicbase/stable
 		fmt.Sprintf("kicbase/stable:%s@sha256:%s", Version, baseImageSHA),
 
-		// BaseImageFallBack2 the fall back of BaseImage in case gcr.io is not available. stored in github packages https://github.com/kubernetes/minikube/packages/206071
+		// the fallback of BaseImage in case gcr.io is not available. stored in github packages https://github.com/kubernetes/minikube/packages/206071
 		// github packages docker does _NOT_ support pulling by sha as mentioned in the docs:
 		// https://help.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-docker-for-use-with-github-packages
 		fmt.Sprintf("docker.pkg.github.com/kubernetes/minikube/kicbase:%s", Version),
