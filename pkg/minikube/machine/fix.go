99ab246ba399a9611502c8741c1aaab14d44fa2c
Marcin Niemira <marcin.niemira@gmail.com>
2020-06-10 20:51:33 +1000

    handle missing error

--- pkg/minikube/machine/fix.go
+++ pkg/minikube/machine/fix.go
@@ -172,8 +172,10 @@ func maybeWarnAboutEvalEnv(drver string, name string) {
 
 	if os.Getenv(constants.MinikubeActiveDockerdEnv) != "" {
 		out.T(out.Notice, "Noticed you have an activated docker-env on {{.driver_name}} driver in this terminal:", out.V{"driver_name": drver})
-		out.WarningT(`Please re-eval your docker-env`)
-		daemonenv.SetScript(ec, os.Stdout)
+		out.WarningT(`Please re-eval your docker-env using given snipset`)
+		if err := daemonenv.SetScript(ec, os.Stdout); err != nil {
+			out.WarningT("got unexpected error: {{.error}}", out.V{"error": err})
+		}
 	}
 	if os.Getenv(constants.MinikubeActivePodmanEnv) != "" {
 		out.T(out.Notice, "Noticed you have an activated podman-env on {{.driver_name}} driver in this terminal:", out.V{"driver_name": drver})
