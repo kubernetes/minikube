69387a3937947bf57eb24b0cecedc3ffd3fd1818
Priya Wadhwa <priyawadhwa@google.com>
2020-06-10 10:58:37 -0700

    Add in extra args for v1beta1

--- pkg/minikube/bootstrapper/bsutil/ktmpl/v1beta1.go
+++ pkg/minikube/bootstrapper/bsutil/ktmpl/v1beta1.go
@@ -65,6 +65,12 @@ etcd:
     dataDir: {{.EtcdDataDir}}
     extraArgs:
       listen-metrics-urls: http://127.0.0.1:2381,http://{{.AdvertiseAddress}}:2381
+controllerManager:
+  extraArgs:
+    "leader-elect": "false"
+scheduler:
+  extraArgs:
+    "leader-elect": "false"
 kubernetesVersion: {{.KubernetesVersion}}
 networking:
   dnsDomain: {{if .DNSDomain}}{{.DNSDomain}}{{else}}cluster.local{{end}}
