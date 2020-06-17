131362d14885090538d2387ae9aa3d61eae9d705
Medya Gh <medya@google.com>
2020-06-16 17:07:36 -0700

    add error type for check running

--- pkg/drivers/kic/oci/oci.go
+++ pkg/drivers/kic/oci/oci.go
@@ -218,7 +218,7 @@ func CreateContainerNode(p CreateParams) error {
 
 	// retry up to up 13 seconds to make sure the created container status is running.
 	if err := retry.Expo(checkRunning, 13*time.Millisecond, time.Second*13); err != nil {
-		return errors.Wrapf(err, "check container %q running", p.Name)
+		return errors.Wrapf(ErrNotRunningAfterCreate, "container name %q", p.Name)
 	}
 
 	return nil
