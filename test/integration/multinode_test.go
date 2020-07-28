916b795d4208ba5fd51f64989a0553863fa939fe
Sharif Elgamal <selgamal@google.com>
2020-07-28 15:51:04 -0700

    add verbosity to restart multinode test too

--- test/integration/multinode_test.go
+++ test/integration/multinode_test.go
@@ -219,7 +219,7 @@ func validateRestartMultiNodeCluster(ctx context.Context, t *testing.T, profile 
 		}
 	}
 	// Restart a full cluster with minikube start
-	startArgs := append([]string{"start", "-p", profile, "--wait=true"}, StartArgs()...)
+	startArgs := append([]string{"start", "-p", profile, "--wait=true", "-v=8", "--alsologtostderr"}, StartArgs()...)
 	rr, err := Run(t, exec.CommandContext(ctx, Target(), startArgs...))
 	if err != nil {
 		t.Fatalf("failed to start cluster. args %q : %v", rr.Command(), err)
