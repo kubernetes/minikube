a8998b177e62cb9c7a0562d0a0eb75570299508f
Dean Coakley <dean.s.coakley@gmail.com>
2020-08-05 02:06:06 +0100

    Add prune docker images instructions

--- pkg/minikube/machine/advice.go
+++ pkg/minikube/machine/advice.go
@@ -40,7 +40,8 @@ func MaybeDisplayAdvice(err error, driver string) {
 	if errors.Is(err, oci.ErrExitedUnexpectedly) || errors.Is(err, oci.ErrDaemonInfo) {
 		out.T(style.Tip, "If you are still interested to make {{.driver_name}} driver work. The following suggestions might help you get passed this issue:", out.V{"driver_name": driver})
 		out.T(style.Empty, `
-	- Prune unused {{.driver_name}} images, volumes and abandoned containers.`, out.V{"driver_name": driver})
+	- Prune unused {{.driver_name}} images, volumes, networks and abandoned containers.
+		docker system prune --volumes`, out.V{"driver_name": driver})
 		out.T(style.Empty, `
 	- Restart your {{.driver_name}} service`, out.V{"driver_name": driver})
 		if runtime.GOOS != "linux" {
