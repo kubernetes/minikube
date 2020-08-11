abc282dc71bbaf2baa3208948dc9d5a41270ef2e
yogendra0sharma <eh.yogendra@gmail.com>
2020-08-11 16:11:52 +0530

    fix linting build break

--- cmd/minikube/cmd/start.go
+++ cmd/minikube/cmd/start.go
@@ -524,7 +524,7 @@ func selectDriver(existing *config.ClusterConfig) (registry.DriverState, []regis
 		ds := driver.Status(d)
 		if ds.Name == "" {
 			if d == "kvm" {
-				viper.Set("driver","kvm2")
+				viper.Set("driver", "kvm2")
 				d = "kvm2"
 			}
 			exit.WithCodeT(exit.Unavailable, "The driver '{{.driver}}' is not supported on {{.os}}", out.V{"driver": d, "os": runtime.GOOS})
@@ -538,7 +538,7 @@ func selectDriver(existing *config.ClusterConfig) (registry.DriverState, []regis
 		ds := driver.Status(viper.GetString("vm-driver"))
 		if ds.Name == "" {
 			if d == "kvm" {
-				viper.Set("driver","kvm2")
+				viper.Set("driver", "kvm2")
 				d = "kvm2"
 			}
 			exit.WithCodeT(exit.Unavailable, "The driver '{{.driver}}' is not supported on {{.os}}", out.V{"driver": d, "os": runtime.GOOS})
