0bf4c263cc43a8d0c2e9ffff21ef35214571511a
Pablo Caderno <kaderno@gmail.com>
2020-08-11 19:05:12 +1000

    Removed extra flag add.
    Set default value if not  specified by the user.
    
    Signed-off-by: Pablo Caderno <kaderno@gmail.com>

--- pkg/minikube/machine/cluster_test.go
+++ pkg/minikube/machine/cluster_test.go
@@ -68,7 +68,6 @@ var defaultClusterConfig = config.ClusterConfig{
 	Driver:    driver.Mock,
 	DockerEnv: []string{"MOCK_MAKE_IT_PROVISION=true"},
 	Nodes:     []config.Node{{Name: "minikube"}},
-	StartHostTimeout: 4 * time.Minute,
 }
 
 func TestCreateHost(t *testing.T) {
@@ -282,7 +281,6 @@ func TestStartHostConfig(t *testing.T) {
 		Driver:    driver.Mock,
 		DockerEnv: []string{"FOO=BAR"},
 		DockerOpt: []string{"param=value"},
-		StartHostTimeout: 4 * time.Minute,
 	}
 
 	h, _, err := StartHost(api, &cfg, &config.Node{Name: "minikube"})
