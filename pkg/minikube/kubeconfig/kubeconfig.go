065b7e021a077324e91133011f55194f77a6446c
Kazuki Suda <kazuki.suda@gmail.com>
2020-08-16 09:36:09 +0900

    Revert support for adding context if no clusters in the kubeconfig

--- pkg/minikube/kubeconfig/kubeconfig.go
+++ pkg/minikube/kubeconfig/kubeconfig.go
@@ -21,6 +21,7 @@ import (
 	"io/ioutil"
 	"net/url"
 	"os"
+	"path"
 	"path/filepath"
 	"strconv"
 
@@ -30,6 +31,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/client-go/tools/clientcmd/api/latest"
 	"k8s.io/minikube/pkg/minikube/constants"
+	"k8s.io/minikube/pkg/minikube/localpath"
 	pkgutil "k8s.io/minikube/pkg/util"
 	"k8s.io/minikube/pkg/util/lock"
 )
@@ -119,11 +121,28 @@ func UpdateEndpoint(contextName string, hostname string, port int, confpath stri
 		return false, errors.Wrap(err, "read")
 	}
 
+	address := "https://" + hostname + ":" + strconv.Itoa(port)
+
+	// if the cluster setting is missed in the kubeconfig, create new one
 	if _, ok := cfg.Clusters[contextName]; !ok {
-		return false, errors.Errorf("%q does not appear in %s", contextName, confpath)
+		lp := localpath.Profile(contextName)
+		gp := localpath.MiniPath()
+		kcs := &Settings{
+			ClusterName:          contextName,
+			ClusterServerAddress: address,
+			ClientCertificate:    path.Join(lp, "client.crt"),
+			ClientKey:            path.Join(lp, "client.key"),
+			CertificateAuthority: path.Join(gp, "ca.crt"),
+			KeepContext:          false,
+		}
+		err = PopulateFromSettings(kcs, cfg)
+		if err != nil {
+			return false, errors.Wrap(err, "populating kubeconfig")
+		}
+	} else {
+		cfg.Clusters[contextName].Server = address
 	}
 
-	cfg.Clusters[contextName].Server = "https://" + hostname + ":" + strconv.Itoa(port)
 	err = writeToFile(cfg, confpath)
 	if err != nil {
 		return false, errors.Wrap(err, "write")
