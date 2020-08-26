06b9a35a2e9254bc41000a546380638cca1ff72b
Medya Gh <medya.gh@gmail.com>
2020-08-26 00:04:02 -0700

    remove debugging info

--- cmd/minikube/cmd/mount.go
+++ cmd/minikube/cmd/mount.go
@@ -117,7 +117,6 @@ var mountCmd = &cobra.Command{
 			}
 		}
 		port, err := getPort()
-		fmt.Println("port is ", ip)
 		if err != nil {
 			exit.WithError("Error finding port for mount", err)
 		}
@@ -148,7 +147,6 @@ var mountCmd = &cobra.Command{
 		}
 
 		bindIP := ip.String() // the ip to listen on the user's host machine
-		fmt.Println("bind ip is", bindIP)
 		if driver.IsKIC(co.CP.Host.Driver.DriverName()) && runtime.GOOS != "linux" {
 			bindIP = "127.0.0.1"
 		}
