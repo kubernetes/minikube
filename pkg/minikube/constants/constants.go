942eb539e600b4e17aaa4d4afcc5ec85f0c31b6e
Thomas Stromberg <tstromberg@google.com>
2020-09-16 17:44:15 -0700

    Update DefaultKubernetesVersion to v1.19.2

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -27,10 +27,10 @@ import (
 
 const (
 	// DefaultKubernetesVersion is the default Kubernetes version
-	DefaultKubernetesVersion = "v1.19.1"
+	DefaultKubernetesVersion = "v1.19.2"
 	// NewestKubernetesVersion is the newest Kubernetes version to test against
 	// NOTE: You may need to update coreDNS & etcd versions in pkg/minikube/bootstrapper/images/images.go
-	NewestKubernetesVersion = "v1.19.1"
+	NewestKubernetesVersion = "v1.19.2"
 	// OldestKubernetesVersion is the oldest Kubernetes version to test against
 	OldestKubernetesVersion = "v1.13.0"
 	// DefaultClusterName is the default nane for the k8s cluster
