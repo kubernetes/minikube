042bc75e2c1a6c6484dea19a756f6461274168ba
Priya Wadhwa <priyawadhwa@google.com>
2020-09-22 13:54:23 -0400

    Add performance monitor code

--- cmd/performance/pr-bot/bot.go
+++ cmd/performance/pr-bot/bot.go
@@ -18,12 +18,17 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 func main() {
 	for {
+		log.Print("~~~~~~~~~ Starting performance analysis ~~~~~~~~~~~~~~")
 		if err := analyzePerformance(context.Background()); err != nil {
 			log.Printf("error executing performance analysis: %v", err)
 		}
@@ -36,5 +41,38 @@ func main() {
 //   2. running mkcmp against those PRs
 //   3. commenting results on those PRs
 func analyzePerformance(ctx context.Context) error {
+	logsFile := "/home/performance-monitor/logs.txt"
+	if _, err := os.Stat(logsFile); err != nil {
+		return err
+	}
+	client := monitor.NewClient(context.Background(), "kubernetes", "minikube")
+	prs, err := client.ListOpenPRsWithLabel("")
+	if err != nil {
+		return errors.Wrap(err, "listing open prs")
+	}
+	log.Print("got prs:", prs)
+	// TODO: priyawadhwa@ for each PR we should comment the error if we get one?
+	for _, pr := range prs {
+		log.Printf("~~~ Analyzing PR %d ~~~", pr)
+		newCommitsExist, err := client.NewCommitsExist(pr, "minikube-pr-bot")
+		if err != nil {
+			return err
+		}
+		if !newCommitsExist {
+			log.Println("New commits don't exist, skipping rerun...")
+			continue
+		}
+		// TODO: priyawadhwa@ we should download mkcmp for each run?
+		var message string
+		message, err = monitor.RunMkcmp(ctx, pr)
+		if err != nil {
+			message = fmt.Sprintf("Error: %v\n%s", err, message)
+		}
+		log.Printf("got message for pr %d:\n%s\n", pr, message)
+		if err := client.CommentOnPR(pr, message); err != nil {
+			return err
+		}
+		log.Print("successfully commented on PR")
+	}
 	return nil
 }
