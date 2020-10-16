d397e4ca3e3f446ab87bbdb4a591b0d89a55d6d0
Sharif Elgamal <selgamal@google.com>
2020-10-16 13:13:37 -0700

    hyperkit driver perms

--- hack/jenkins/macos_presubmit.sh
+++ hack/jenkins/macos_presubmit.sh
@@ -50,6 +50,10 @@ gsutil -qm cp \
 gsutil -qm cp -r "gs://minikube-builds/${MINIKUBE_LOCATION}/testdata"/* testdata/
 
 gsutil -qm cp "gs://minikube-builds/${MINIKUBE_LOCATION}/gvisor-addon" testdata/
+
+sudo chown root:wheel /tmpfs/src/github/minikube/hack/jenkins/out/docker-machine-driver-hyperkit 
+sudo chmod u+s /tmpfs/src/github/minikube/hack/jenkins/out/docker-machine-driver-hyperkit
+
 export MINIKUBE_BIN="out/minikube-${OS_ARCH}"
 export E2E_BIN="out/e2e-${OS_ARCH}"
 chmod +x "${MINIKUBE_BIN}" "${E2E_BIN}" out/docker-machine-driver-*
