615830d411adb5a3f839ea0933250bcd4946e36d
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-10-23 20:16:41 +0200

    Upgrade podman to version 2.1.1 and remove varlink
    Add the podman group, to remove "sudo varlink bridge"
    
    Use the podman socket /run/podman/podman.sock instead

--- deploy/iso/minikube-iso/package/podman/podman.mk
+++ deploy/iso/minikube-iso/package/podman/podman.mk
@@ -1,12 +1,15 @@
-PODMAN_DUMMY = DUMMY
-PODMAN_VERSION = v1.9.3
-PODMAN_COMMIT = 5d44534fff6877b1cb15b760242279ae6293154c
+PODMAN_VERSION = v2.1.1
+PODMAN_COMMIT = 9f6d6ba0b314d86521b66183c9ce48eaa2da1de2
 PODMAN_SITE = https://github.com/containers/podman/archive
 PODMAN_SOURCE = $(PODMAN_VERSION).tar.gz
 PODMAN_LICENSE = Apache-2.0
 PODMAN_LICENSE_FILES = LICENSE
 
 PODMAN_DEPENDENCIES = host-go
+ifeq ($(BR2_INIT_SYSTEMD),y)
+# need libsystemd for journal
+PODMAN_DEPENDENCIES += systemd
+endif
 
 PODMAN_GOPATH = $(@D)/_output
 PODMAN_BIN_ENV = \
@@ -17,11 +20,24 @@ PODMAN_BIN_ENV = \
 	PATH=$(PODMAN_GOPATH)/bin:$(BR_PATH)
 
 
+define PODMAN_USERS
+	- -1 podman -1 - - - - -
+endef
+
+define PODMAN_MOD_VENDOR_MAKEFILE
+	# "build flag -mod=vendor only valid when using modules"
+	sed -e 's|-mod=vendor ||' -i $(@D)/Makefile
+endef
+
+PODMAN_POST_EXTRACT_HOOKS += PODMAN_MOD_VENDOR_MAKEFILE
+
 define PODMAN_CONFIGURE_CMDS
+	mkdir -p $(PODMAN_GOPATH) && mv $(@D)/vendor $(PODMAN_GOPATH)/src
+
 	mkdir -p $(PODMAN_GOPATH)/src/github.com/containers
-	ln -sf $(@D) $(PODMAN_GOPATH)/src/github.com/containers/libpod
-	mkdir -p $(PODMAN_GOPATH)/src/github.com/varlink
-	ln -sf $(@D)/vendor/github.com/varlink/go $(PODMAN_GOPATH)/src/github.com/varlink/go
+	ln -sf $(@D) $(PODMAN_GOPATH)/src/github.com/containers/podman
+
+	ln -sf $(@D) $(PODMAN_GOPATH)/src/github.com/containers/podman/v2
 endef
 
 define PODMAN_BUILD_CMDS
@@ -33,6 +49,23 @@ define PODMAN_INSTALL_TARGET_CMDS
 	$(INSTALL) -Dm755 $(@D)/bin/podman $(TARGET_DIR)/usr/bin/podman
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/cni/net.d/
 	$(INSTALL) -m 644 $(@D)/cni/87-podman-bridge.conflist $(TARGET_DIR)/etc/cni/net.d/87-podman-bridge.conflist
+endef
+
+define PODMAN_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 \
+			$(@D)/contrib/systemd/system/podman.service \
+			$(TARGET_DIR)/usr/lib/systemd/system/podman.service
+	$(INSTALL) -D -m 644 \
+			$(@D)/contrib/systemd/system/podman.socket \
+			$(TARGET_DIR)/usr/lib/systemd/system/podman.socket
+
+	# Allow running podman-remote as a user in the group "podman"
+	$(INSTALL) -D -m 644 \
+			$(PODMAN_PKGDIR)/override.conf \
+			$(TARGET_DIR)/usr/lib/systemd/system/podman.socket.d/override.conf
+	$(INSTALL) -D -m 644 \
+			$(PODMAN_PKGDIR)/podman.conf \
+			$(TARGET_DIR)/usr/lib/tmpfiles.d/podman.conf
 endef
 
 $(eval $(generic-package))
