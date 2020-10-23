64d38c813ad16f72dd6b2cb9679e597d134c3915
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-10-23 20:16:41 +0200

    Upgrade podman to version 2.1.1 and remove varlink
    Add the podman group, to remove "sudo varlink bridge"
    
    Use the podman socket /run/podman/podman.sock instead

--- deploy/iso/minikube-iso/package/varlink/varlink.mk
+++
@@ -1,9 +0,0 @@
-VARLINK_VERSION = 19
-VARLINK_SITE = https://github.com/varlink/libvarlink/archive
-VARLINK_SOURCE = $(VARLINK_VERSION).tar.gz
-VARLINK_LICENSE = Apache-2.0
-VARLINK_LICENSE_FILES = LICENSE
-
-VARLINK_NEEDS_HOST_PYTHON = python3
-
-$(eval $(meson-package))
