d9ffb927cf187e0260100cdffc6e0fa561ba5f13
Sharif Elgamal <selgamal@google.com>
2020-11-18 17:33:50 -0800

    install kubectl

--- hack/jenkins/macos_presubmit.sh
+++ hack/jenkins/macos_presubmit.sh
@@ -35,4 +35,8 @@ docker version --format {{.Server.Os}}-{{.Server.Version}}
 # Force python3.7
 export CLOUDSDK_PYTHON=/usr/bin/python3
 
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 make integration-functional-only
