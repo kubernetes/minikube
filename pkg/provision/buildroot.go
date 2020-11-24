1033ab2d21997d23a85fac4c8345fa7f66217746
Medya Gh <medya@google.com>
2020-11-24 15:13:56 -0800

    use docker recommendation

--- pkg/provision/buildroot.go
+++ pkg/provision/buildroot.go
@@ -76,13 +76,12 @@ Description=Docker Application Container Engine
 Documentation=https://docs.docker.com
 After=network.target  minikube-automount.service docker.socket
 Requires= minikube-automount.service docker.socket 
-StartLimitBurst=5
-StartLimitIntervalSec=60
 
 [Service]
-Restart=on-failure
-RestartSec=1
 Type=notify
+Restart=on-failure
+StartLimitBurst=3
+StartLimitIntervalSec=60
 `
 	if noPivot {
 		klog.Warning("Using fundamentally insecure --no-pivot option")
--- pkg/provision/ubuntu.go
+++ pkg/provision/ubuntu.go
@@ -79,13 +79,12 @@ BindsTo=containerd.service
 After=network-online.target firewalld.service containerd.service
 Wants=network-online.target
 Requires=docker.socket
-StartLimitBurst=5
-StartLimitIntervalSec=60
 
 [Service]
-Restart=on-failure
-RestartSec=1
 Type=notify
+Restart=on-failure
+StartLimitBurst=3
+StartLimitIntervalSec=60
 `
 	if noPivot {
 		klog.Warning("Using fundamentally insecure --no-pivot option")
