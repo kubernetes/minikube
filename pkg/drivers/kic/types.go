7bc81b1cbdae09aa2cdae46e377e4a8cc333f343
Medya Gh <medya@google.com>
2020-11-25 14:22:29 -0800

    add imagesha

--- pkg/drivers/kic/types.go
+++ pkg/drivers/kic/types.go
@@ -26,7 +26,7 @@ const (
 	// Version is the current version of kic
 	Version = "v0.0.15-snapshot2"
 	// SHA of the kic base image
-	baseImageSHA = ""
+	baseImageSHA = "0973e4bcdfef0dc8c5a581ecfcca5e36fa6a1cc8773e832ecfd31de3d2b6bf46"
 )
 
 var (
