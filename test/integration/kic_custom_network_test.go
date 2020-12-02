a6a6671175052f6345380250a8103d352a796949
Priya Wadhwa <priyawadhwa@google.com>
2020-12-02 13:19:04 -0800

    make integration tests more robust

--- test/integration/kic_custom_network_test.go
+++ test/integration/kic_custom_network_test.go
@@ -20,8 +20,12 @@ package integration
 
 import (
 	"context"
+	"fmt"
 	"os/exec"
+	"strings"
 	"testing"
+
+	"k8s.io/minikube/pkg/drivers/kic/oci"
 )
 
 func TestKicCustomNetwork(t *testing.T) {
@@ -31,13 +35,13 @@ func TestKicCustomNetwork(t *testing.T) {
 
 	tests := []struct {
 		description string
-		flag        string
+		networkName string
 	}{
 		{
 			description: "create custom network",
 		}, {
 			description: "use default bridge network",
-			flag:        "--docker-network=bridge",
+			networkName: "bridge",
 		},
 	}
 
@@ -47,12 +51,53 @@ func TestKicCustomNetwork(t *testing.T) {
 			ctx, cancel := context.WithTimeout(context.Background(), Minutes(5))
 			defer Cleanup(t, profile, cancel)
 
-			startArgs := []string{"start", "-p", profile, test.flag}
+			startArgs := []string{"start", "-p", profile, fmt.Sprintf("--network=%s", test.networkName)}
 			c := exec.CommandContext(ctx, Target(), startArgs...)
 			rr, err := Run(t, c)
 			if err != nil {
 				t.Fatalf("%v failed: %v\n%v", rr.Command(), err, rr.Output())
 			}
+			nn := test.networkName
+			if nn == "" {
+				nn = profile
+			}
+			verifyNetworkExists(ctx, t, nn)
 		})
+	}
+}
+
+func TestKicExistingNetwork(t *testing.T) {
+	// create custom network
+	networkName := "existing-network"
+	if _, err := oci.CreateNetwork(oci.Docker, networkName); err != nil {
+		t.Fatalf("error creating network: %v", err)
+	}
+	defer func() {
+		if err := oci.DeleteKICNetworks(); err != nil {
+			t.Logf("error deleting kic network, may need to delete manually: %v", err)
+		}
+	}()
+	profile := UniqueProfileName("existing-network")
+	ctx, cancel := context.WithTimeout(context.Background(), Minutes(5))
+	defer Cleanup(t, profile, cancel)
+
+	verifyNetworkExists(ctx, t, networkName)
+
+	startArgs := []string{"start", "-p", profile, fmt.Sprintf("--network=%s", networkName)}
+	c := exec.CommandContext(ctx, Target(), startArgs...)
+	rr, err := Run(t, c)
+	if err != nil {
+		t.Fatalf("%v failed: %v\n%v", rr.Command(), err, rr.Output())
+	}
+}
+
+func verifyNetworkExists(ctx context.Context, t *testing.T, networkName string) {
+	c := exec.CommandContext(ctx, "docker", "network", "ls", "--format", "{{.Name}}")
+	rr, err := Run(t, c)
+	if err != nil {
+		t.Fatalf("%v failed: %v\n%v", rr.Command(), err, rr.Output())
+	}
+	if output := rr.Output(); !strings.Contains(output, networkName) {
+		t.Fatalf("%s network is not listed by [%v]: %v", networkName, c.Args, output)
 	}
 }
