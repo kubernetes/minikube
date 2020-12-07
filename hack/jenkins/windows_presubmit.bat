e36009d53c0b12937c51522f2d58886da87a91b8
Sharif Elgamal <selgamal@google.com>
2020-12-07 12:59:59 -0800

    call executables better

--- hack/jenkins/windows_presubmit.bat
+++ hack/jenkins/windows_presubmit.bat
@@ -21,8 +21,8 @@ call gsutil -m cp gs://minikube-builds/%MINIKUBE_LOCATION%/minikube-windows-amd6
 call gsutil -m cp gs://minikube-builds/%MINIKUBE_LOCATION%/e2e-windows-amd64.exe out/
 call gsutil -m cp -r gs://minikube-builds/%MINIKUBE_LOCATION%/testdata .
 
-call out/minikube-windows-amd64.exe delete --all
+start /w .\out\minikube-windows-amd64.exe delete --all
 
-call out/e2e-windows-amd64.exe -minikube-start-args="--driver=docker" -binary=out/minikube-windows-amd64.exe -test.v -test.timeout=65m -test.run=TestFunctional
+call /w .\out\e2e-windows-amd64.exe -minikube-start-args="--driver=docker" -binary=out/minikube-windows-amd64.exe -test.v -test.timeout=65m -test.run=TestFunctional
 
 if not "%ERRORLEVEL%" == "0" exit /B 1
