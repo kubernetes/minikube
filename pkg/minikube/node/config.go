f7abca67f762fc3e84d4fec94517ce887fb8a9ac
alonyb <rubenbaez1995@gmail.com>
2020-12-06 21:55:41 -0500

    remove comment line

--- pkg/minikube/node/config.go
+++ pkg/minikube/node/config.go
@@ -41,7 +41,6 @@ func showVersionInfo(k8sVersion string, cr cruntime.Manager) {
 	version, _ := cr.Version()
 	register.Reg.SetStep(register.PreparingKubernetes)
 	out.Step(cr.Style(), "Preparing Kubernetes {{.k8sVersion}} on {{.runtime}} {{.runtimeVersion}} ...", true, out.V{"k8sVersion": k8sVersion, "runtime": cr.Name(), "runtimeVersion": version})
-	//out.SpinnerStep(cr.Style(), "Preparing Kubernetes {{.k8sVersion}} on {{.runtime}} {{.runtimeVersion}} ...", out.V{"k8sVersion": k8sVersion, "runtime": cr.Name(), "runtimeVersion": version})
 	for _, v := range config.DockerOpt {
 		out.Infof("opt {{.docker_option}}", out.V{"docker_option": v})
 	}
