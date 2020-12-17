4e3632ce986bfb56e95ad2fb7e0978a45cca27c9
Yanshu Zhao <azhao155@gmail.com>
2020-12-17 17:50:51 +0000

    fix lint

--- cmd/minikube/cmd/docker-env.go
+++ cmd/minikube/cmd/docker-env.go
@@ -201,7 +201,9 @@ func mustRestartDockerd(name string, runner command.Runner) {
 		// verifying apisever using kverify would add code complexity for a rare case.
 		klog.Warningf("waiting to ensure apisever container is up...")
 		startTime := time.Now()
-		WaitForAPIServerProcess(runner, startTime, time.Second*5)
+		if err = WaitForAPIServerProcess(runner, startTime, time.Second*5); err != nil {
+			exit.Message(reason.RuntimeRestart, `The api server within '{{.name}}' is not up`, out.V{"name": name})
+		}
 	}
 }
 
