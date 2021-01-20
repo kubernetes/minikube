68e6ee36b2c7cb762fad1e56c4f16432720a4dff
Sharif Elgamal <selgamal@google.com>
2021-01-19 18:18:53 -0800

    run docker rm after starting it

--- hack/jenkins/windows_integration_test_docker.ps1
+++ hack/jenkins/windows_integration_test_docker.ps1
@@ -11,8 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker ps -aq | ForEach -Process {docker rm -fv $_}
-
 mkdir -p out
 gsutil.cmd -m cp gs://minikube-builds/$env:MINIKUBE_LOCATION/minikube-windows-amd64.exe out/
 gsutil.cmd -m cp gs://minikube-builds/$env:MINIKUBE_LOCATION/e2e-windows-amd64.exe out/
@@ -20,6 +18,8 @@ gsutil.cmd -m cp -r gs://minikube-builds/$env:MINIKUBE_LOCATION/testdata .
 gsutil.cmd -m cp -r gs://minikube-builds/$env:MINIKUBE_LOCATION/setup_docker_desktop_windows.ps1 out/
 
 ./out/setup_docker_desktop_windows.ps1
+
+docker ps -aq | ForEach -Process {docker rm -fv $_}
 
 ./out/minikube-windows-amd64.exe delete --all
 
