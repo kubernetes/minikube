d6350c28d3f3e9648a64b5ad71d73057c4ce7a35
Priya Wadhwa <priyawadhwa@google.com>
2021-01-21 13:15:00 -0800

    add debugging

--- test/integration/start_stop_delete_test.go
+++ test/integration/start_stop_delete_test.go
@@ -44,33 +44,33 @@ func TestStartStop(t *testing.T) {
 			version string
 			args    []string
 		}{
-			{"old-k8s-version", constants.OldestKubernetesVersion, []string{
-				// default is the network created by libvirt, if we change the name minikube won't boot
-				// because the given network doesn't exist
-				"--kvm-network=default",
-				"--kvm-qemu-uri=qemu:///system",
-				"--disable-driver-mounts",
-				"--keep-context=false",
-				"--container-runtime=docker",
-			}},
-			{"newest-cni", constants.NewestKubernetesVersion, []string{
-				"--feature-gates",
-				"ServerSideApply=true",
-				"--network-plugin=cni",
-				"--extra-config=kubelet.network-plugin=cni",
-				"--extra-config=kubeadm.pod-network-cidr=192.168.111.111/16",
-			}},
-			{"containerd", constants.DefaultKubernetesVersion, []string{
-				"--container-runtime=containerd",
-				"--docker-opt",
-				"containerd=/var/run/containerd/containerd.sock",
-				"--apiserver-port=8444",
-			}},
-			{"crio", "v1.15.7", []string{
-				"--container-runtime=crio",
-				"--disable-driver-mounts",
-				"--extra-config=kubeadm.ignore-preflight-errors=SystemVerification",
-			}},
+			// {"old-k8s-version", constants.OldestKubernetesVersion, []string{
+			// 	// default is the network created by libvirt, if we change the name minikube won't boot
+			// 	// because the given network doesn't exist
+			// 	"--kvm-network=default",
+			// 	"--kvm-qemu-uri=qemu:///system",
+			// 	"--disable-driver-mounts",
+			// 	"--keep-context=false",
+			// 	"--container-runtime=docker",
+			// }},
+			// {"newest-cni", constants.NewestKubernetesVersion, []string{
+			// 	"--feature-gates",
+			// 	"ServerSideApply=true",
+			// 	"--network-plugin=cni",
+			// 	"--extra-config=kubelet.network-plugin=cni",
+			// 	"--extra-config=kubeadm.pod-network-cidr=192.168.111.111/16",
+			// }},
+			// {"containerd", constants.DefaultKubernetesVersion, []string{
+			// 	"--container-runtime=containerd",
+			// 	"--docker-opt",
+			// 	"containerd=/var/run/containerd/containerd.sock",
+			// 	"--apiserver-port=8444",
+			// }},
+			// {"crio", "v1.15.7", []string{
+			// 	"--container-runtime=crio",
+			// 	"--disable-driver-mounts",
+			// 	"--extra-config=kubeadm.ignore-preflight-errors=SystemVerification",
+			// }},
 			{"embed-certs", constants.DefaultKubernetesVersion, []string{
 				"--embed-certs",
 			}},
@@ -103,14 +103,14 @@ func TestStartStop(t *testing.T) {
 						validator validateStartStopFunc
 					}{
 						{"FirstStart", validateFirstStart},
-						{"DeployApp", validateDeploying},
-						{"Stop", validateStop},
-						{"EnableAddonAfterStop", validateEnableAddonAfterStop},
-						{"SecondStart", validateSecondStart},
-						{"UserAppExistsAfterStop", validateAppExistsAfterStop},
-						{"AddonExistsAfterStop", validateAddonAfterStop},
+						// {"DeployApp", validateDeploying},
+						// {"Stop", validateStop},
+						// {"EnableAddonAfterStop", validateEnableAddonAfterStop},
+						// {"SecondStart", validateSecondStart},
+						// {"UserAppExistsAfterStop", validateAppExistsAfterStop},
+						// {"AddonExistsAfterStop", validateAddonAfterStop},
 						{"VerifyKubernetesImages", validateKubernetesImages},
-						{"Pause", validatePauseAfterStart},
+						// {"Pause", validatePauseAfterStart},
 					}
 					for _, stc := range serialTests {
 						if ctx.Err() == context.DeadlineExceeded {
