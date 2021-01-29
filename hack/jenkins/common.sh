8b858290e0ab71c3771107fb397a3f17b05ecc0d
Priya Wadhwa <priyawadhwa@google.com>
2021-01-29 13:52:05 -0800

    Add timeout to cleaning up tests

--- hack/jenkins/common.sh
+++ hack/jenkins/common.sh
@@ -308,7 +308,7 @@ fi
 
 ${SUDO_PREFIX}${E2E_BIN} \
   -minikube-start-args="--driver=${VM_DRIVER} ${EXTRA_START_ARGS}" \
-  -test.timeout=180m -test.v \
+  -test.timeout=120m -test.v \
   ${EXTRA_TEST_ARGS} \
   -binary="${MINIKUBE_BIN}" 2>&1 | tee "${TEST_OUT}"
 
