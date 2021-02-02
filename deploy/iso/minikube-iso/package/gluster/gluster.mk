d1f4aa7edc4ce6278afba22efc9c175ed206f14a
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-02-02 16:45:07 +0100

    Old glusterfs releases were moved to a subdir

--- deploy/iso/minikube-iso/package/gluster/gluster.mk
+++ deploy/iso/minikube-iso/package/gluster/gluster.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 GLUSTER_VERSION = 4.1.5
-GLUSTER_SITE = https://download.gluster.org/pub/gluster/glusterfs/4.1/$(GLUSTER_VERSION)
+GLUSTER_SITE = https://download.gluster.org/pub/gluster/glusterfs/01.old-releases/4.1/$(GLUSTER_VERSION)
 GLUSTER_SOURCE = glusterfs-$(GLUSTER_VERSION).tar.gz
 GLUSTER_CONF_OPTS = --disable-tiering --disable-ec-dynamic --disable-xmltest --disable-crypt-xlator --disable-georeplication --disable-ibverbs --disable-glupy --disable-gnfs --disable-cmocka --without-server
 GLUSTER_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) install
