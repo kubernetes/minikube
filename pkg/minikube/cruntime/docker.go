6bf615d3d53eb70d51b448136b09bb5c728d5133
Medya Gh <medya@google.com>
2021-02-05 12:20:29 -0800

    disable dockerd for container runtime

--- pkg/minikube/cruntime/docker.go
+++ pkg/minikube/cruntime/docker.go
@@ -137,6 +137,10 @@ func (r *Docker) Restart() error {
 
 // Disable idempotently disables Docker on a host
 func (r *Docker) Disable() error {
+	// because #10373
+	if err := r.Init.ForceStop("docker.socket"); err != nil {
+		return err
+	}
 	return r.Init.ForceStop("docker")
 }
 
