f13ae5cdc6bea443cc23918093e7f707e53b1e94
Steven Powell <spowellgrr@gmail.com>
2021-02-05 17:01:56 -0700

    Refactor tmp dir detection to improve testability

---
+++ pkg/minikube/command/kic_runner_test.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2019 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package command
+
+import (
+	"os"
+	"testing"
+)
+
+func TestKICRunner(t *testing.T) {
+	t.Parallel()
+
+	t.Run("TestTempDirectory", func(t *testing.T) {
+		t.Parallel()
+
+		home, err := os.UserHomeDir()
+		if err != nil {
+			t.Fatalf("failed to get user home directory: %v", err)
+		}
+
+		tests := []struct {
+			in   bool
+			want string
+		}{
+			{false, ""},
+			{true, home},
+		}
+
+		for _, tt := range tests {
+			got, err := tempDirectory(tt.in)
+			if err != nil {
+				t.Fatalf("failed to get temp directory: %v", err)
+			}
+
+			if got != tt.want {
+				t.Errorf("tempDirectory(%t) = %s; want %s", tt.in, got, tt.want)
+			}
+		}
+	})
+}
