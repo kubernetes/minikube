b22fab310620943fd516a2942c33f4587647adae
Michael Henkel <michael.henkel@gmail.com>
2021-02-05 17:20:01 -0800

    fixed lint problem

--- pkg/minikube/cni/custom.go
+++ pkg/minikube/cni/custom.go
@@ -17,7 +17,6 @@ limitations under the License.
 package cni
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"path"
@@ -73,11 +72,9 @@ func (c Custom) CIDR() string {
 
 func (c Custom) removeCNIConfig(r Runner) error {
 	klog.Infof("Removing existing cni configuration (/etc/cni/net.d/*)")
-	rm := fmt.Sprintf("rm -rf /etc/cni/net.d/*")
-	if _, err := r.RunCmd(exec.Command("sudo", "/bin/bash", "-c", rm)); err != nil {
+	if _, err := r.RunCmd(exec.Command("sudo", "/bin/bash", "-c", "rm -rf /etc/cni/net.d/*")); err != nil {
 		klog.Errorf("netconf update failed: %v", err)
 		return err
 	}
 	return sysinit.New(r).Restart(c.cc.KubernetesConfig.ContainerRuntime)
-
 }
