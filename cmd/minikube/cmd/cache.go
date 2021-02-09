e8945f293448b3a4c84875ee12e4c81b314c2daa
Priya Wadhwa <priyawadhwa@google.com>
2021-02-09 13:36:40 -0800

    Add warning for cache add

--- cmd/minikube/cmd/cache.go
+++ cmd/minikube/cmd/cache.go
@@ -50,6 +50,7 @@ var addCacheCmd = &cobra.Command{
 	Short: "Add an image to local cache.",
 	Long:  "Add an image to local cache.",
 	Run: func(cmd *cobra.Command, args []string) {
+		out.WarningT("\"minikube cache\" will be deprecated in upcoming versions, please switch to \"minikube image load\"")
 		// Cache and load images into docker daemon
 		if err := machine.CacheAndLoadImages(args, cacheAddProfiles()); err != nil {
 			exit.Error(reason.InternalCacheLoad, "Failed to cache and load images", err)
