5d642c5311e2d4469e245048de58004470c38fba
Sharif Elgamal <selgamal@google.com>
2021-02-16 17:39:46 -0800

    fix up escaping nonsense

--- hack/jenkins/kicbase_auto_build.sh
+++ hack/jenkins/kicbase_auto_build.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
+
 ./hack/jenkins/installers/check_install_docker.sh
 yes|gcloud auth configure-docker
 now=$(date +%s)
@@ -26,16 +28,7 @@ docker pull $KICBASE_IMAGE_REGISTRIES
 fullsha=$(docker inspect --format='{{index .RepoDigests 0}}' $KICBASE_IMAGE_REGISTRIES)
 sha=$(echo ${fullsha} | cut -d ":" -f 2)
 
-message="Hi ${ghprbPullAuthorLoginMention},
-
-A new kicbase image is available, please update your PR with the new tag and SHA.
-In pkg/drivers/kic/types.go:
-
-	// Version is the current version of kic
-	Version = \"${KICBASE_IMAGE_REGISTRIES}\"
-	// SHA of the kic base image
-	baseImageSHA = \"${sha}\"
-"
+message="Hi ${ghprbPullAuthorLoginMention},\\n\\nA new kicbase image is available, please update your PR with the new tag and SHA.\\nIn pkg/drivers/kic/types.go:\\n\\n\\t// Version is the current version of kic\\n\\tVersion = \\\"${KICBASE_IMAGE_REGISTRIES}\\\"\\n\\t// SHA of the kic base image\\n\\tbaseImageSHA = \\\"${sha}\\\"\\n"
 
 curl -s -H "Authorization: token ${access_token}" \
 	 -H "Accept: application/vnd.github.v3+json" \
