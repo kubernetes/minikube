5cf8dce9443c4dcff04e1f084d467316cd5cd730
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-03-14 16:16:25 +0100

    Add a red box around docker desktop registry port
    To make it harder to miss the notice, with the rest of the emojis
    
    Also remove some stray "on" text, but do add "driver" and "port"

--- pkg/addons/addons.go
+++ pkg/addons/addons.go
@@ -29,6 +29,8 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 
+	"github.com/Delta456/box-cli-maker/v2"
+
 	"k8s.io/klog/v2"
 	"k8s.io/minikube/pkg/drivers/kic/oci"
 	"k8s.io/minikube/pkg/kapi"
@@ -199,7 +201,13 @@ https://github.com/kubernetes/minikube/issues/7332`, out.V{"driver_name": cc.Dri
 				return errors.Wrap(err, "registry port")
 			}
 			if enable {
-				out.Styled(style.Tip, `Registry addon on with {{.driver}} uses {{.port}} please use that instead of default 5000`, out.V{"driver": cc.Driver, "port": port})
+				str := out.Sprintf(style.None, `Registry addon with {{.driver}} driver uses port {{.port}} please use that instead of default port 5000`, out.V{"driver": cc.Driver, "port": port})
+				box := box.New(box.Config{Px: 0, Py: 0, Type: "Round", Color: "Red"})
+				txt := strings.Split(box.String("", str), "\n")
+				out.Styled(style.Indent, txt[0])
+				out.Styled(style.Tip, txt[1])
+				out.Styled(style.Indent, txt[2])
+				out.Styled(style.Indent, txt[3])
 			}
 			out.Styled(style.Documentation, `For more information see: https://minikube.sigs.k8s.io/docs/drivers/{{.driver}}`, out.V{"driver": cc.Driver})
 		}
