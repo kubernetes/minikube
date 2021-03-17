e05a187faa4cee75766193b0dc02d74bce91155f
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-03-17 20:05:08 +0100

    Upgrade Docker, from 20.10.4 to 20.10.5

--- deploy/iso/minikube-iso/package/docker-bin/docker-bin.mk
+++ deploy/iso/minikube-iso/package/docker-bin/docker-bin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_BIN_VERSION = 20.10.4
+DOCKER_BIN_VERSION = 20.10.5
 DOCKER_BIN_SITE = https://download.docker.com/linux/static/stable/x86_64
 DOCKER_BIN_SOURCE = docker-$(DOCKER_BIN_VERSION).tgz
 
