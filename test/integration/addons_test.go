b311692f523b9b6bd88a88f29096f626fcd005a9
Tomas Kral <tkral@redhat.com>
2021-04-02 22:46:45 +0200

    remove skip from olm integration test

--- test/integration/addons_test.go
+++ test/integration/addons_test.go
@@ -462,7 +462,6 @@ func validateHelmTillerAddon(ctx context.Context, t *testing.T, profile string) 
 
 // validateOlmAddon tests the OLM addon
 func validateOlmAddon(ctx context.Context, t *testing.T, profile string) {
-	t.Skipf("Skipping olm test till this timeout issue is solved https://github.com/operator-framework/operator-lifecycle-manager/issues/1534#issuecomment-632342257")
 	defer PostMortemLogs(t, profile)
 
 	client, err := kapi.Client(profile)
