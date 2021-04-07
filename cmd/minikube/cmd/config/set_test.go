cc553bfb00cd3b74d3c53886c1b927372e492687
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-04-07 20:27:47 +0200

    Fix broken unit tests encoding wrong result

--- cmd/minikube/cmd/config/set_test.go
+++ cmd/minikube/cmd/config/set_test.go
@@ -39,7 +39,7 @@ func TestSetNotAllowed(t *testing.T) {
 		t.Fatalf("Set did not return error for unallowed value: %+v", err)
 	}
 	err = Set("memory", "10a")
-	if err == nil || err.Error() != "run validations for \"memory\" with value of \"10a\": [invalid disk size: invalid size: '10a']" {
+	if err == nil || err.Error() != "run validations for \"memory\" with value of \"10a\": [invalid memory size: invalid size: '10a']" {
 		t.Fatalf("Set did not return error for unallowed value: %+v", err)
 	}
 }
