8644c21002c244de80402999b587eb1f6a79ba9f
Steven Powell <spowellgrr@gmail.com>
2021-04-23 16:49:35 -0700

    lint comment

--- pkg/minikube/notify/notify_test.go
+++ pkg/minikube/notify/notify_test.go
@@ -108,7 +108,7 @@ func TestShouldCheckURLBetaVersion(t *testing.T) {
 	// test that update notifications get triggered if it has been longer than 24 hours
 	viper.Set(config.ReminderWaitPeriodInHours, 24)
 
-	//time.Time{} returns time -> January 1, year 1, 00:00:00.000000000 UTC.
+	// time.Time{} returns time -> January 1, year 1, 00:00:00.000000000 UTC.
 	if err := writeTimeToFile(lastUpdateCheckFilePath, time.Time{}); err != nil {
 		t.Errorf("write failed: %v", err)
 	}
