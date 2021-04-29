98f4d4706bb9da1fa4956f341f9ab736ed2bb3a2
Andriy Dzikh <andriydzikh@gmail.com>
2021-04-29 16:12:58 -0700

    Add --file flag to 'minikube logs' to automatically put logs into a file.

--- cmd/minikube/cmd/logs.go
+++ cmd/minikube/cmd/logs.go
@@ -44,6 +44,8 @@ var (
 	numberOfLines int
 	// showProblems only shows lines that match known issues
 	showProblems bool
+	// fileOutput is where to write logs to. If omitted, writes to stdout.
+	fileOutput string
 )
 
 // logsCmd represents the logs command
@@ -52,7 +54,18 @@ var logsCmd = &cobra.Command{
 	Short: "Returns logs to debug a local Kubernetes cluster",
 	Long:  `Gets the logs of the running instance, used for debugging minikube, not user code.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		logs.OutputOffline(numberOfLines)
+		var logOutput *os.File = os.Stdout
+		var err error
+
+		if fileOutput != "" {
+			logOutput, err = os.Create(fileOutput)
+			if err != nil {
+				exit.Error(reason.Usage, "Failed to create file", err)
+			}
+			defer logOutput.Close()
+		}
+
+		logs.OutputOffline(numberOfLines, logOutput)
 
 		co := mustload.Running(ClusterFlagValue())
 
@@ -65,8 +78,9 @@ var logsCmd = &cobra.Command{
 		if err != nil {
 			exit.Error(reason.InternalNewRuntime, "Unable to get runtime", err)
 		}
+
 		if followLogs {
-			err := logs.Follow(cr, bs, *co.Config, co.CP.Runner)
+			err := logs.Follow(cr, bs, *co.Config, co.CP.Runner, logOutput)
 			if err != nil {
 				exit.Error(reason.InternalLogFollow, "Follow", err)
 			}
@@ -74,10 +88,10 @@ var logsCmd = &cobra.Command{
 		}
 		if showProblems {
 			problems := logs.FindProblems(cr, bs, *co.Config, co.CP.Runner)
-			logs.OutputProblems(problems, numberOfProblems)
+			logs.OutputProblems(problems, numberOfProblems, logOutput)
 			return
 		}
-		err = logs.Output(cr, bs, *co.Config, co.CP.Runner, numberOfLines)
+		err = logs.Output(cr, bs, *co.Config, co.CP.Runner, numberOfLines, logOutput)
 		if err != nil {
 			out.Ln("")
 			// Avoid exit.Error, since it outputs the issue URL
@@ -92,4 +106,5 @@ func init() {
 	logsCmd.Flags().BoolVar(&showProblems, "problems", false, "Show only log entries which point to known problems")
 	logsCmd.Flags().IntVarP(&numberOfLines, "length", "n", 60, "Number of lines back to go within the log")
 	logsCmd.Flags().StringVar(&nodeName, "node", "", "The node to get logs from. Defaults to the primary control plane.")
+	logsCmd.Flags().StringVar(&fileOutput, "file", "", "If present, writes to the provided file instead of stdout.")
 }
