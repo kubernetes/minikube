733a35a0f4a9f32150addc67283854c07a221dab
Yanshu <azhao155@gmail.com>
2021-05-02 17:36:13 +0000

    Fix header file

--- deploy/prow/wrapper.sh
+++ deploy/prow/wrapper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Copyright 2019 The Kubernetes Authors.
+
+# Copyright 2021 The Kubernetes Authors All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
