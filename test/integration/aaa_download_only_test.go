459c42bf4847a22defb82dad17efd59e0f87aed5
Steven Powell <spowellgrr@gmail.com>
2021-05-04 16:12:19 -0700

    dont check exit code of minikube logs in logs duration test

--- test/integration/aaa_download_only_test.go
+++ test/integration/aaa_download_only_test.go
@@ -158,13 +158,12 @@ func TestDownloadOnly(t *testing.T) {
 				}
 			})
 
-			t.Run("minikube logs", func(t *testing.T) {
+			// checks if the duration of `minikube logs` takes longer than 5 seconds
+			t.Run("LogsDuration", func(t *testing.T) {
 				ctx, cancel := context.WithTimeout(context.Background(), Seconds(5))
 				defer cancel()
 				args := []string{"logs", "-p", profile}
-				if _, err := Run(t, exec.CommandContext(ctx, Target(), args...)); err != nil {
-					t.Errorf("minikube logs failed with error: %v", err)
-				}
+				Run(t, exec.CommandContext(ctx, Target(), args...))
 				if err := ctx.Err(); err == context.DeadlineExceeded {
 					t.Error("minikube logs expected to finish by 5 seconds, but took longer")
 				}
