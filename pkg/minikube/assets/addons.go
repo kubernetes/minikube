a84652f0d55db68d9de7f1944f19f8347a01695e
Yanshu <azhao155@gmail.com>
2021-05-05 00:49:44 +0000

    add sha256

--- pkg/minikube/assets/addons.go
+++ pkg/minikube/assets/addons.go
@@ -105,7 +105,7 @@ var Addons = map[string]*Addon{
 
 		// GuestPersistentDir
 	}, false, "auto-pause", map[string]string{
-		"AutoPauseHook": "k8s-minikube/auto-pause-hook:v0.0.2",
+		"AutoPauseHook": "k8s-minikube/auto-pause-hook:v0.0.2@sha256:c76be418df5ca9c66d0d11c2c68461acbf4072c1cdfc17e64729c5ef4d5a4128",
 	}, map[string]string{
 		"AutoPauseHook": "gcr.io",
 	}),
