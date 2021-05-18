0068f923272c25906ca7f07148396f6e3dd69db1
Medya Gh <medya@google.com>
2021-05-17 18:30:18 -0700

    add port to addons templates

--- pkg/addons/addons.go
+++ pkg/addons/addons.go
@@ -219,6 +219,9 @@ https://github.com/kubernetes/minikube/issues/7332`, out.V{"driver_name": cc.Dri
 	var networkInfo assets.NetworkInfo
 	if len(cc.Nodes) >= 1 {
 		networkInfo.ControlPlaneNodeIP = cc.Nodes[0].IP
+		networkInfo.ControlPlaneNodePort = cc.Nodes[0].Port
+		fmt.Printf("+%v", networkInfo)
+		fmt.Println(cc.Nodes[0].Port)
 	} else {
 		out.WarningT("At least needs control plane nodes to enable addon")
 	}
