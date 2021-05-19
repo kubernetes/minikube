c04f2d731f99dd3187c4ecd0d9114ee6d612167a
Andriy Dzikh <andriydzikh@gmail.com>
2021-05-19 15:14:24 -0700

    Add message to hack/release_notes.sh to tell maintainers to update contributions.

--- hack/release_notes.sh
+++ hack/release_notes.sh
@@ -64,3 +64,6 @@ echo "Thank you to our triage members for this release!"
 echo ""
 AWK_ISSUE_COMMENTS='NR>1{arr[$4] += $7}END{for (a in arr) printf "%d %s\n", arr[a], a}'
 "${DIR}/pullsheet" issue-comments --since "$recent_date" --repos kubernetes/minikube --token-path $DIR/gh_token.txt --logtostderr=false --stderrthreshold=2 | awk -F ',' "$AWK_ISSUE_COMMENTS" | sort -k1nr -k2d  | awk -F ' ' "$AWK_FORMAT_ITEM" | head -n 5
+
+echo ""
+echo "Don't forget to run \"hack/update_contributions.sh\"!"
