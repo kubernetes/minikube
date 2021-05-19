246a2e55d567c9f775edf5a9eebd937db20f3c19
Ilya Zuyev <izuyev@google.com>
2021-05-18 17:21:08 -0700

    fix boilerplate

--- hack/gh_actions/install_cleanup.sh
+++ hack/gh_actions/install_cleanup.sh
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 if [[ ! -f cleanup.sh ]]; then
   echo "cleanup.sh is missing"
   exit 1
