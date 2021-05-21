63f0f8774b7903f46d18b80f6b3348490fa2e109
Andriy Dzikh <andriydzikh@gmail.com>
2021-05-21 16:11:00 -0700

    Create script to collect log data.

---
+++ hack/test-flake-chart/collect_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create temp path for partial data (storing everything but the commit date.)
+PARTIAL_DATA_PATH=$(mktemp)
+# Write 
+echo "Partial path: $PARTIAL_DATA_PATH" 1>&2
+
+# Print header.
+printf "Commit Hash,Commit Date,Environment,Test,Status\n"
+
+# 1) "cat" together all summary files.
+# 2) Turn each test in each summary file to a CSV line containing its commit hash, environment, test, and status.
+# 3) Copy partial data to $PARTIAL_DATA_PATH to join with date later.
+# 4) Extract only commit hash for each row
+# 5) Make the commit hashes unique (we assume that gsutil cats files from the same hash next to each other).
+#   Also force buffering to occur per line so remainder of pipe can continue to process.
+# 6) Execute git log for each commit to get the date of each.
+# 7) Join dates with test data.
+gsutil cat gs://minikube-builds/logs/master/*/*_summary.json \
+| jq -r '({commit: .Detail.Details, environment: .Detail.Name, test: .PassedTests[]?, status: "Passed"},{commit: .Detail.Details, environment: .Detail.Name, test: .FailedTests[]?, status: "Failed"},{commit: .Detail.Details, environment: .Detail.Name, test: .SkippedTests[]?, status: "Skipped"}) | .commit + "," + .environment + "," + .test + "," + .status' \
+| tee $PARTIAL_DATA_PATH \
+| sed -r -n 's/^([^,]+),.*/\1/p' \
+| stdbuf -oL -eL uniq \
+| xargs -I {} git log -1 --pretty=format:"{},%as%n" {} \
+| join -t "," - $PARTIAL_DATA_PATH
