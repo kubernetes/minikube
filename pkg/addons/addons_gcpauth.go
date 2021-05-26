643cfb91f483c78394601e93e7971a9338f6da97
Sharif Elgamal <selgamal@google.com>
2021-05-26 09:33:33 -0700

    fix last bool

--- pkg/addons/addons_gcpauth.go
+++ pkg/addons/addons_gcpauth.go
@@ -307,7 +307,7 @@ func verifyGCPAuthAddon(cc *config.ClusterConfig, name string, val string) error
 	if enable && err == nil {
 		out.Styled(style.Notice, "Your GCP credentials will now be mounted into every pod created in the {{.name}} cluster.", out.V{"name": cc.Name})
 		out.Styled(style.Notice, "If you don't want your credentials mounted into a specific pod, add a label with the `gcp-auth-skip-secret` key to your pod configuration.")
-		if !Force {
+		if !Refresh {
 			out.Styled(style.Notice, "If you want existing pods to be mounted with credentials, either recreate them or rerun addons enable with --refresh.")
 		}
 	}
