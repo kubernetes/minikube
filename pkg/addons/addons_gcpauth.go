ad1fc0b8fe1528b35fd2f114a3f38e1296db6549
Sharif Elgamal <selgamal@google.com>
2021-05-27 10:22:42 -0700

    wrap pull secret error

--- pkg/addons/addons_gcpauth.go
+++ pkg/addons/addons_gcpauth.go
@@ -87,7 +87,7 @@ func enableAddonGCPAuth(cfg *config.ClusterConfig) error {
 	// Create a registry secret in every namespace we can find
 	err = createPullSecret(cfg, creds)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to create pull secret")
 	}
 
 	// First check if the project env var is explicitly set
