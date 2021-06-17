9609a91a8a35ac8c839e175817b5e19057c0a0c1
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-06-17 16:55:02 +0200

    Add packaging for crun

---
+++ deploy/iso/minikube-iso/package/crun/crun.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# crun
+#
+################################################################################
+
+CRUN_VERSION = 0.19
+CRUN_COMMIT = e67a75672412975916dac6b87f8346f870e4b99a
+# need the pre-generated release tarball with the git submodules and configure
+CRUN_SITE = https://github.com/containers/crun/releases/download/$(CRUN_VERSION)
+CRUN_LICENSE = GPL-2.0
+CRUN_LICENSE_FILES = COPYING
+
+CRUN_DEPENDENCIES += host-python3
+
+CRUN_MAKE_OPTS = crun
+
+ifeq ($(BR2_PACKAGE_LIBCAP),y)
+CRUN_DEPENDENCIES += libcap
+else
+CRUN_CONF_OPTS += --disable-caps
+endif
+
+ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
+CRUN_CONF_OPTS += --enable-seccomp
+CRUN_DEPENDENCIES += libseccomp host-pkgconf
+else
+CRUN_CONF_OPTS += --disable-seccomp
+endif
+
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+CRUN_CONF_OPTS += --enable-systemd
+CRUN_DEPENDENCIES += systemd host-pkgconf
+else
+CRUN_CONF_OPTS += --disable-systemd
+endif
+
+$(eval $(autotools-package))
