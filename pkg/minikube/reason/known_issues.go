0167da64358170ccce41cd2bdacb6df6b73985f2
Medya Gh <medya@google.com>
2021-08-16 15:50:33 -0700

    add solution message for cgroup related issues on cloud shell-like environments

--- pkg/minikube/reason/known_issues.go
+++ pkg/minikube/reason/known_issues.go
@@ -177,6 +177,37 @@ var hostIssues = []match{
 	},
 	{
 		Kind: Kind{
+			ID:       "HOST_CGROUP_NOT_SUPPORTED",
+			ExitCode: ExHostUnsupported,
+			Advice: `CGroup allocation is not available in your environment, You are possibly running minikube in a nested container, try run with these options:
+			
+	minikube start --extra-config=kubelet.cgroups-per-qos=false --extra-config=kubelet.enforce-node-allocatable=""
+
+			
+			`,
+			Issues: []int{12232},
+		},
+		Regexp: re(`Failed to start ContainerManager" err="Unit kubepods.slice already exists.`),
+		GOOS:   []string{"linux"},
+	},
+	{
+		Kind: Kind{
+			ID:       "HOST_ROOT_CGROUP",
+			ExitCode: ExHostUnsupported,
+			Advice: `CGroup allocation is not available in your environment, You are possibly running minikube in a nested container, try run with these options:
+			
+	minikube start --extra-config=kubelet.cgroups-per-qos=false --extra-config=kubelet.enforce-node-allocatable=""
+
+			
+			`,
+			Issues: []int{12232},
+		},
+		Regexp: re(`Failed to start ContainerManager" err="failed to initialize top level QOS containers: root container [kubepods] doesn't exist`),
+		GOOS:   []string{"linux"},
+	},
+
+	{
+		Kind: Kind{
 			ID:       "HOST_PIDS_CGROUP",
 			ExitCode: ExHostUnsupported,
 			Advice:   "Ensure that the required 'pids' cgroup is enabled on your host: grep pids /proc/cgroups",
