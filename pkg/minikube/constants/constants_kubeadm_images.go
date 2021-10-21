d0928b19131d78737cf7732e32d72b69e990a56c
srikrishnabh93@gmail.com <srikrishnabh93@gmail.com>
2021-10-21 23:01:32 +0530

    updated kubeadm-constants image version

--- pkg/minikube/constants/constants_kubeadm_images.go
+++ pkg/minikube/constants/constants_kubeadm_images.go
@@ -21,10 +21,10 @@ var (
 		"v1.22": {
 			"k8s.gcr.io/coredns/coredns":         "v1.8.4",
 			"k8s.gcr.io/etcd":                    "3.5.0-0",
-			"k8s.gcr.io/kube-apiserver":          "v1.22.1",
-			"k8s.gcr.io/kube-controller-manager": "v1.22.1",
-			"k8s.gcr.io/kube-proxy":              "v1.22.1",
-			"k8s.gcr.io/kube-scheduler":          "v1.22.1",
+			"k8s.gcr.io/kube-apiserver":          "v1.22.2",
+			"k8s.gcr.io/kube-controller-manager": "v1.22.2",
+			"k8s.gcr.io/kube-proxy":              "v1.22.2",
+			"k8s.gcr.io/kube-scheduler":          "v1.22.2",
 			"k8s.gcr.io/pause":                   "3.5",
 		},
 		"v1.21": {
