93e2934ab3b2dfd8c84a591a9072523b7fd63374
Medya Gh <medya@google.com>
2021-10-25 14:38:27 -0700

    address review comment

--- test/integration/functional_test_tunnel_test.go
+++ test/integration/functional_test_tunnel_test.go
@@ -94,7 +94,7 @@ func checkRoutePassword(t *testing.T) {
 func checkDNSForward(t *testing.T) {
 	// Not all platforms support DNS forwarding
 	if runtime.GOOS != "darwin" || KicDriver() {
-		t.Skip("DNS forwarding is only supported for hyprkit on darwin, skipping test DNS forwarding")
+		t.Skip("DNS forwarding is only supported for Hyperkit on Darwin, skipping test DNS forwarding")
 	}
 }
 
