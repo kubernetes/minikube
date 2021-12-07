4d706033bb50f17550177cb3dc2cc782a92dbcfe
klaases <bayintel@gmail.com>
2021-12-07 10:35:26 -0800

    Bump PreloadVersion const to v15

--- pkg/minikube/download/preload.go
+++ pkg/minikube/download/preload.go
@@ -44,7 +44,7 @@ const (
 	// PreloadVersion is the current version of the preloaded tarball
 	//
 	// NOTE: You may need to bump this version up when upgrading auxiliary docker images
-	PreloadVersion = "v14"
+	PreloadVersion = "v15"
 	// PreloadBucket is the name of the GCS bucket where preloaded volume tarballs exist
 	PreloadBucket = "minikube-preloaded-volume-tarballs"
 )
