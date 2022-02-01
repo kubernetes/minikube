3b4666f7fe03f2efe1c71ccb35c28dabef47646f
klaases <bayintel@gmail.com>
2022-02-01 13:45:09 -0800

    Attempt to use older version of genimage.cfg

--- deploy/iso/minikube-iso/board/iso/x86_64/post-image-efi.sh
+++ deploy/iso/minikube-iso/board/iso/x86_64/post-image-efi.sh
@@ -14,9 +14,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+#!/bin/sh
+
 set -e
 
-UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.ext2" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
-sed -i "s/UUID_TMP/$UUID/g" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
-sed "s/UUID_TMP/$UUID/g" board/pc/genimage-efi.cfg > "$BINARIES_DIR/genimage-efi.cfg"
-support/scripts/genimage.sh -c "$BINARIES_DIR/genimage-efi.cfg"
+GENIMAGE_CFG="$2"
+
+support/scripts/genimage.sh -c "$GENIMAGE_CFG"
+
+cd "$BINARIES_DIR"
+mkdir -p root/boot
+cp bzImage root/boot/vmlinuz
+cp rootfs.cpio.gz root/boot/initrd.img
+mkdir -p root/EFI/BOOT
+cp efi-part/EFI/BOOT/* root/EFI/BOOT/
+cp efiboot.img root/EFI/BOOT/
+
+mkisofs \
+   -o boot.iso \
+   -R -J -v -d -N \
+   -hide-rr-moved \
+   -no-emul-boot \
+   -eltorito-platform=efi \
+   -eltorito-boot EFI/BOOT/efiboot.img \
+   -V "EFIBOOTISO" \
+   -A "EFI Boot ISO" \
+   root
+cd -
+
+
+# set -e
+
+# UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.ext2" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
+# sed -i "s/UUID_TMP/$UUID/g" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
+# sed "s/UUID_TMP/$UUID/g" board/pc/genimage-efi.cfg > "$BINARIES_DIR/genimage-efi.cfg"
+# support/scripts/genimage.sh -c "$BINARIES_DIR/genimage-efi.cfg"
