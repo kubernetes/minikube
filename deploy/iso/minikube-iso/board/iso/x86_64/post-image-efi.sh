5957888f3e641cbb4f2ee1324c08467ab65da7f7
klaases <bayintel@gmail.com>
2022-02-11 16:23:38 -0800

    Attempt previous version of post-image

--- deploy/iso/minikube-iso/board/iso/x86_64/post-image-efi.sh
+++ deploy/iso/minikube-iso/board/iso/x86_64/post-image-efi.sh
@@ -17,10 +17,10 @@
 
 #!/bin/sh
 
-set -e
+# set -e
 
-echo "2*** I am inside post-image-efi.sh"
-pwd
+# echo "2*** I am inside post-image-efi.sh"
+# pwd
 
 # GENIMAGE_CFG="./genimage-efi.cfg"
 # GENIMAGE_CFG="$2"
@@ -30,33 +30,33 @@ pwd
 
 # support/scripts/genimage.sh -c "$GENIMAGE_CFG"
 
-cd "$BINARIES_DIR"
-echo "BINARIES_DIR: $BINARIES_DIR"
-ls -lah
-mkdir -p root/boot
-cp bzImage root/boot/vmlinuz
-cp rootfs.cpio.gz root/boot/initrd.img
-mkdir -p root/EFI/BOOT
-cp efi-part/EFI/BOOT/* root/EFI/BOOT/
+# cd "$BINARIES_DIR"
+# echo "BINARIES_DIR: $BINARIES_DIR"
+# ls -lah
+# mkdir -p root/boot
+# cp bzImage root/boot/vmlinuz
+# cp rootfs.cpio.gz root/boot/initrd.img
+# mkdir -p root/EFI/BOOT
+# cp efi-part/EFI/BOOT/* root/EFI/BOOT/
 
 # Create an empty boot image.
-dd if=/dev/zero of=EFI/BOOT/efiboot.img bs=512 count=2880
-cp efiboot.img root/EFI/BOOT/
+# dd if=/dev/zero of=EFI/BOOT/efiboot.img bs=512 count=2880
+# cp efiboot.img root/EFI/BOOT/
 
-echo "2c*** mkisofs"
-ls -lah
+# echo "2c*** mkisofs"
+# ls -lah
 
-mkisofs \
-   -o boot.iso \
-   -R -J -v -d -N \
-   -hide-rr-moved \
-   -no-emul-boot \
-   -eltorito-platform=efi \
-   -eltorito-boot root/EFI/BOOT/efiboot.img \
-   -V "EFIBOOTISO" \
-   -A "EFI Boot ISO" \
-   root
-cd -
+# mkisofs \
+#    -o boot.iso \
+#    -R -J -v -d -N \
+#    -hide-rr-moved \
+#    -no-emul-boot \
+#    -eltorito-platform=efi \
+#    -eltorito-boot root/EFI/BOOT/efiboot.img \
+#    -V "EFIBOOTISO" \
+#    -A "EFI Boot ISO" \
+#    root
+# cd -
 
 # -eltorito-boot EFI/BOOT/efiboot.img \
 
@@ -72,7 +72,7 @@ cd -
 
 # set -e
 
-# UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.ext2" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
-# sed -i "s/UUID_TMP/$UUID/g" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
-# sed "s/UUID_TMP/$UUID/g" board/pc/genimage-efi.cfg > "$BINARIES_DIR/genimage-efi.cfg"
-# support/scripts/genimage.sh -c "$BINARIES_DIR/genimage-efi.cfg"
+UUID=$(dumpe2fs "$BINARIES_DIR/rootfs.ext2" 2>/dev/null | sed -n 's/^Filesystem UUID: *\(.*\)/\1/p')
+sed -i "s/UUID_TMP/$UUID/g" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
+sed "s/UUID_TMP/$UUID/g" board/pc/genimage-efi.cfg > "$BINARIES_DIR/genimage-efi.cfg"
+support/scripts/genimage.sh -c "$BINARIES_DIR/genimage-efi.cfg"
