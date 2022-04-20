caf8e6402ed89ebd30a364227823de0e5eac2cd5
Steven Powell <spowellgrr@gmail.com>
2022-04-20 15:12:20 -0700

    fix having flag before command cause problems

--- cmd/minikube/main.go
+++ cmd/minikube/main.go
@@ -229,7 +229,7 @@ func setFlags(parse bool) {
 
 // setLastStartFlags sets the log_file flag to lastStart.txt if start command and user doesn't specify log_file or log_dir flags.
 func setLastStartFlags() {
-	if len(os.Args) < 2 || os.Args[1] != "start" {
+	if pflag.Arg(0) != "start" {
 		return
 	}
 	if pflag.CommandLine.Changed("log_file") || pflag.CommandLine.Changed("log_dir") {
