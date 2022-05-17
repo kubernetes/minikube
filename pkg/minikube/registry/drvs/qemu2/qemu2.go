823ea12bc25a99caa5fbcffccb402aad3ad404dc
Sharif Elgamal <selgamal@google.com>
2022-05-16 23:12:39 -0700

    let's try kvm accel again

--- pkg/minikube/registry/drvs/qemu2/qemu2.go
+++ pkg/minikube/registry/drvs/qemu2/qemu2.go
@@ -68,11 +68,16 @@ func qemuFirmwarePath() (string, error) {
 	arch := runtime.GOARCH
 	// For macOS, find the correct brew installation path for qemu firmware
 	if runtime.GOOS == "darwin" {
-		p := "/usr/local/Cellar/qemu"
-		fw := "share/qemu/edk2-x86_64-code.fd"
-		if arch == "arm64" {
+		var p, fw string
+		switch arch {
+		case "amd64":
+			p = "/usr/local/Cellar/qemu"
+			fw = "share/qemu/edk2-x86_64-code.fd"
+		case "arm64":
 			p = "/opt/homebrew/Cellar/qemu"
 			fw = "share/qemu/edk2-aarch64-code.fd"
+		default:
+			return "", fmt.Errorf("unknown arch: %s", arch)
 		}
 
 		v, err := ioutil.ReadDir(p)
@@ -109,8 +114,13 @@ func configure(cc config.ClusterConfig, n config.Node) (interface{}, error) {
 		qemuMachine = "" // default
 		qemuCPU = ""     // default
 	case "arm64":
-		qemuMachine = "virt"
-		qemuCPU = "cortex-a72"
+		if runtime.GOOS == "darwin" {
+			qemuMachine = "virt"
+			qemuCPU = "cortex-a72"
+		} else if runtime.GOOS == "linux" {
+			qemuMachine = "gic-version=3"
+			qemuCPU = "host"
+		}
 	default:
 		return nil, fmt.Errorf("unknown arch: %s", runtime.GOARCH)
 	}
