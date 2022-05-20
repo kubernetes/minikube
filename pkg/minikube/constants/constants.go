1033e05218ea2b1cc314c5e71675edfc25b3d165
Predrag Rogic <prezha@users.noreply.github.com>
2022-05-20 17:43:31 +0100

    keep OldestKubernetesVersion as-is

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -37,7 +37,7 @@ const (
 	// NOTE: You may need to update coreDNS & etcd versions in pkg/minikube/bootstrapper/images/images.go
 	NewestKubernetesVersion = "v1.23.6"
 	// OldestKubernetesVersion is the oldest Kubernetes version to test against
-	OldestKubernetesVersion = "v1.17.0"
+	OldestKubernetesVersion = "v1.16.0"
 	// NoKubernetesVersion is the version used when users does NOT want to install kubernetes
 	NoKubernetesVersion = "v0.0.0"
 
