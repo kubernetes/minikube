5c3d3be0a6180c8cbdf97540ce48f79d0dbca4d9
Sharif Elgamal <selgamal@google.com>
2022-06-07 13:06:22 -0700

    qemu: only set highmem=off for darwin if availble RAM is under 32GB

--- pkg/minikube/registry/drvs/qemu2/qemu2.go
+++ pkg/minikube/registry/drvs/qemu2/qemu2.go
@@ -122,9 +122,12 @@ func configure(cc config.ClusterConfig, n config.Node) (interface{}, error) {
 		qemuCPU = "cortex-a72"
 		// highmem=off needed, see https://patchwork.kernel.org/project/qemu-devel/patch/20201126215017.41156-9-agraf@csgraf.de/#23800615 for details
 		if runtime.GOOS == "darwin" {
-			qemuMachine = "virt,highmem=off"
+			if cc.Memory < 32000 {
+				qemuMachine += ",highmem=off"
+			}
+			qemuCPU = "host"
 		} else if _, err := os.Stat("/dev/kvm"); err == nil {
-			qemuMachine = "virt,gic-version=3"
+			qemuMachine += ",gic-version=3"
 			qemuCPU = "host"
 		}
 	default:
