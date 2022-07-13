5fac416884f2593987011bd7f2caabd5d52f489f
Steven Powell <spowellgrr@gmail.com>
2022-07-13 15:50:36 -0700

    update entrypoint and kindnetd

--- pkg/minikube/bootstrapper/images/images.go
+++ pkg/minikube/bootstrapper/images/images.go
@@ -182,7 +182,7 @@ func KindNet(repo string) string {
 	if repo == "" {
 		repo = "kindest"
 	}
-	return path.Join(repo, "kindnetd:v20220510-4929dd75")
+	return path.Join(repo, "kindnetd:v20220607-9a4d8d2a")
 }
 
 // all calico images are from https://docs.projectcalico.org/manifests/calico.yaml
