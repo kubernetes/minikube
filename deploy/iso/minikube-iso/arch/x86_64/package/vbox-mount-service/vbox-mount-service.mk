kubernetes/minikube

commit 770d41f21b42b037fbf4386b1817e3a3d3370aaf

Use kernel 5.10 for minikube.iso.
This commit jumps kernel version used in minikube.iso to 5.10.
To do so, the following kernel configuration options were added to
linux_*_defconfig:
- CONFIG_TMPFS to be able to use mount -t tmpfs in init.
- CONFIG_PCI to have network.
- CONFIG_BRIDGE_NETFILTER to have /proc/sys/net/bridge/bridge-nf-call-iptables
which is needed by kubeadm.

CONFIG_* relaed to vbox were added to linux_x86_64_defconfig and as consequence
vbox related packages were removed since vbox modules are available in
upstream kernel.

To compile falco module, CONFIG_FTRACE_* were added to linux_aarch64_defconfig.

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

deploy/iso/minikube-iso/arch/x86_64/package/Config.in
@@ -10,6 +10,5 @@ menu "System tools x86_64"
     source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/cni-plugins/Config.in"
     source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/containerd-bin/Config.in"
     source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/podman/Config.in"
-    source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/vbox-guest/Config.in"
     source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/hyperv-daemons/Config.in"
 endmenu

deploy/iso/minikube-iso/configs/minikube_x86_64_defconfig
@@ -31,13 +31,13 @@ BR2_TARGET_SYSLINUX=y
 BR2_TARGET_ROOTFS_CPIO=y
 BR2_TARGET_ROOTFS_CPIO_GZIP=y
 
-# Linux headers same as kernel, a 4.19 series
-BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_4_19=y
+# Linux headers same as kernel, a 5.10 series
+BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_5_10=y
 
 # Kernel
 BR2_LINUX_KERNEL=y
 BR2_LINUX_KERNEL_CUSTOM_VERSION=y
-BR2_LINUX_KERNEL_CUSTOM_VERSION_VALUE="4.19.235"
+BR2_LINUX_KERNEL_CUSTOM_VERSION_VALUE="5.10.57"
 BR2_LINUX_KERNEL_USE_ARCH_DEFAULT_CONFIG=y
 BR2_LINUX_KERNEL_NEEDS_HOST_OPENSSL=y
 BR2_LINUX_KERNEL_INSTALL_TARGET=y

deploy/iso/minikube-iso/arch/x86_64/package/vbox-guest/Config.in
@@ -1,5 +1,0 @@
-config BR2_PACKAGE_VBOX_GUEST
-	bool "vbox-guest"
-	depends on BR2_x86_64
-	depends on BR2_LINUX_KERNEL
-

deploy/iso/minikube-iso/arch/x86_64/package/vbox-guest/vboxservice.service
@@ -1,17 +1,0 @@
-[Unit]
-Description=VirtualBox Guest Service
-ConditionVirtualization=oracle
-
-[Service]
-ExecStartPre=-/usr/sbin/modprobe vboxguest
-
-# Broken and probably unused: Unknown symbol ttm_bo_del_sub_from_lru
-# ExecStartPre=-/usr/sbin/modprobe vboxvideo
-
-ExecStartPre=-/usr/sbin/modprobe vboxsf
-# Normally, VirtualBox only syncs every 20 minutes. This syncs on start, and
-# forces an immediate sync if VM time is over 5 seconds off.
-ExecStart=/usr/sbin/VBoxService -f --disable-automount --timesync-set-start --timesync-set-threshold 5000
-
-[Install]
-WantedBy=multi-user.target

Makefile
@@ -73,7 +73,7 @@ MINIKUBE_BUCKET ?= minikube/releases
 MINIKUBE_UPLOAD_LOCATION := gs://${MINIKUBE_BUCKET}
 MINIKUBE_RELEASES_URL=https://github.com/kubernetes/minikube/releases/download
 
-KERNEL_VERSION ?= 4.19.235
+KERNEL_VERSION ?= 5.10.57
 # latest from https://github.com/golangci/golangci-lint/releases 
 # update this only by running `make update-golint-version`
 GOLINT_VERSION ?= v1.46.2

deploy/iso/minikube-iso/arch/x86_64/package/hyperv-daemons/hyperv-daemons.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HYPERV_DAEMONS_VERSION = $(call qstrip,$(BR2_LINUX_KERNEL_VERSION))
-HYPERV_DAEMONS_SITE = https://www.kernel.org/pub/linux/kernel/v4.x
+HYPERV_DAEMONS_SITE = https://www.kernel.org/pub/linux/kernel/v5.x
 HYPERV_DAEMONS_SOURCE = linux-$(HYPERV_DAEMONS_VERSION).tar.xz
 
 define HYPERV_DAEMONS_BUILD_CMDS

deploy/iso/minikube-iso/arch/x86_64/package/vbox-guest/vbox-guest.hash
@@ -1,9 +1,0 @@
-# From http://download.virtualbox.org/virtualbox/5.1.30/SHA256SUMS
-sha256 96cab2296fb014ce0a16b7b9603b52208b9403c10c1524b44201d3c274e8a821  VirtualBox-5.1.38.tar.bz2
-sha256 0e7ee2c78ebf7cd0d3a933d51148bef04a64f64fb27ccf70d59cddf9ca1e517a  VBoxGuestAdditions_5.1.38.iso
-# From http://download.virtualbox.org/virtualbox/5.2.32/SHA256SUMS
-sha256 ff6390e50cb03718cd3f5779627910999c12279b465e340c80d7175778a33958  VirtualBox-5.2.32.tar.bz2
-sha256 4311c7408a3410e6a33264a9062347d9eec04f58339a49f0a60488c0cabc8996  VBoxGuestAdditions_5.2.32.iso
-# From http://download.virtualbox.org/virtualbox/5.2.42/SHA256SUMS
-sha256 e5bee2e34f349aac115ee93974febfe3213ad5e94045fa36b9f04b5f8caa3720  VirtualBox-5.2.42.tar.bz2
-sha256 ff784417295e48e3cee80a596faf05e3b0976e1b94d3b88427939912b0c1fc45  VBoxGuestAdditions_5.2.42.iso

deploy/iso/minikube-iso/arch/x86_64/package/vbox-guest/vbox-guest.mk
@@ -1,67 +1,0 @@
-################################################################################
-#
-# VirtualBox Linux Guest Drivers
-#
-################################################################################
-
-VBOX_GUEST_VERSION = 5.2.42
-VBOX_GUEST_SITE = http://download.virtualbox.org/virtualbox/$(VBOX_GUEST_VERSION)
-VBOX_GUEST_LICENSE = GPLv2
-VBOX_GUEST_LICENSE_FILES = COPYING
-VBOX_GUEST_SOURCE = VirtualBox-$(VBOX_GUEST_VERSION).tar.bz2
-VBOX_GUEST_EXTRA_DOWNLOADS = http://download.virtualbox.org/virtualbox/${VBOX_GUEST_VERSION}/VBoxGuestAdditions_${VBOX_GUEST_VERSION}.iso
-
-define VBOX_GUEST_EXPORT_MODULES
-	( cd $(@D)/src/VBox/Additions/linux; ./export_modules.sh modules.tar.gz )
-	mkdir -p $(@D)/vbox-modules
-	tar -C $(@D)/vbox-modules -xzf $(@D)/src/VBox/Additions/linux/modules.tar.gz
-endef
-
-VBOX_GUEST_POST_EXTRACT_HOOKS += VBOX_GUEST_EXPORT_MODULES
-
-VBOX_GUEST_MODULE_SUBDIRS = vbox-modules/
-VBOX_GUEST_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED) KERN_DIR=$(LINUX_DIR)
-
-define VBOX_GUEST_USERS
-	- -1 vboxsf -1 - - - - -
-endef
-
-define VBOX_GUEST_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 \
-		$(VBOX_GUEST_PKGDIR)/vboxservice.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/vboxservice.service
-
-	ln -fs /usr/lib/systemd/system/vboxservice.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/vboxservice.service
-endef
-
-define VBOX_GUEST_BUILD_CMDS
-	7z x -aoa $(BR2_DL_DIR)/vbox-guest/VBoxGuestAdditions_${VBOX_GUEST_VERSION}.iso -ir'!VBoxLinuxAdditions.run' -o"$(@D)"
-	sh $(@D)/VBoxLinuxAdditions.run --noexec --target $(@D)
-	tar --overwrite -C $(@D) -xjf $(@D)/VBoxGuestAdditions-amd64.tar.bz2 sbin/VBoxService
-	tar --overwrite -C $(@D) -xjf $(@D)/VBoxGuestAdditions-amd64.tar.bz2 bin/VBoxControl
-
-	$(TARGET_CC) -Wall -O2 -D_GNU_SOURCE -DIN_RING3 \
-		-I$(@D)/vbox-modules/vboxsf/include \
-		-I$(@D)/vbox-modules/vboxsf \
-		-o $(@D)/vbox-modules/mount.vboxsf \
-		$(@D)/src/VBox/Additions/linux/sharedfolders/vbsfmount.c \
-		$(@D)/src/VBox/Additions/linux/sharedfolders/mount.vboxsf.c
-endef
-
-define VBOX_GUEST_INSTALL_TARGET_CMDS
-	$(INSTALL) -Dm755 \
-		$(@D)/vbox-modules/mount.vboxsf \
-		$(TARGET_DIR)/sbin
-
-	$(INSTALL) -Dm755 \
-		$(@D)/sbin/VBoxService \
-		$(TARGET_DIR)/sbin
-
-	$(INSTALL) -Dm755 \
-		$(@D)/bin/VBoxControl \
-		$(TARGET_DIR)/bin
-endef
-
-$(eval $(kernel-module))
-$(eval $(generic-package))

commit aa63b4fc9431b5670381a514fd6ed62f3698a157

Add back service to mount VirtualBox host directory into the guest.
Fixes: 770d41f21b42 ("Use kernel 5.10 for minikube.iso.")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

deploy/iso/minikube-iso/arch/x86_64/package/Config.in
@@ -6,4 +6,5 @@ menu "System tools x86_64"
     source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/cni-plugins/Config.in"
     source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/containerd-bin/Config.in"
     source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/hyperv-daemons/Config.in"
+    source "$BR2_EXTERNAL_MINIKUBE_PATH/arch/x86_64/package/vbox-mount-service/Config.in"
 endmenu

deploy/iso/minikube-iso/arch/x86_64/package/vbox-mount-service/Config.in
@@ -1,0 +1,5 @@
+config BR2_PACKAGE_VBOX_MOUNT_SERVICE
+	bool "vbox-mount-service"
+	depends on BR2_x86_64
+	depends on BR2_LINUX_KERNEL
+	default y

deploy/iso/minikube-iso/arch/x86_64/package/vbox-mount-service/vbox-mount-service.hash
@@ -1,0 +1,3 @@
+# From http://download.virtualbox.org/virtualbox/5.2.42/SHA256SUMS
+sha256 e5bee2e34f349aac115ee93974febfe3213ad5e94045fa36b9f04b5f8caa3720  VirtualBox-5.2.42.tar.bz2
+sha256 ff784417295e48e3cee80a596faf05e3b0976e1b94d3b88427939912b0c1fc45  VBoxGuestAdditions_5.2.42.iso

deploy/iso/minikube-iso/arch/x86_64/package/vbox-mount-service/vbox-mount-service.mk
@@ -1,0 +1,42 @@
+################################################################################
+#
+# VirtualBox Mount Service
+#
+################################################################################
+VBOX_MOUNT_SERVICE_VERSION = 5.2.42
+VBOX_MOUNT_SERVICE_SITE = http://download.virtualbox.org/virtualbox/$(VBOX_MOUNT_SERVICE_VERSION)
+VBOX_MOUNT_SERVICE_LICENSE = GPLv2
+VBOX_MOUNT_SERVICE_LICENSE_FILES = COPYING
+VBOX_MOUNT_SERVICE_SOURCE = VirtualBox-$(VBOX_MOUNT_SERVICE_VERSION).tar.bz2
+VBOX_MOUNT_SERVICE_EXTRA_DOWNLOADS = http://download.virtualbox.org/virtualbox/${VBOX_MOUNT_SERVICE_VERSION}/VBoxGuestAdditions_${VBOX_MOUNT_SERVICE_VERSION}.iso
+
+define VBOX_MOUNT_SERVICE_USERS
+	- -1 vboxsf -1 - - - - -
+endef
+
+define VBOX_MOUNT_SERVICE_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 \
+		$(VBOX_MOUNT_SERVICE_PKGDIR)/vboxservice.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/vboxservice.service
+
+	ln -fs /usr/lib/systemd/system/vboxservice.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/vboxservice.service
+endef
+
+define VBOX_MOUNT_SERVICE_BUILD_CMDS
+	7z x -aoa $(BR2_DL_DIR)/vbox-mount-service/VBoxGuestAdditions_${VBOX_MOUNT_SERVICE_VERSION}.iso -ir'!VBoxLinuxAdditions.run' -o"$(@D)"
+	sh $(@D)/VBoxLinuxAdditions.run --noexec --target $(@D)
+	tar --overwrite -C $(@D) -xjf $(@D)/VBoxGuestAdditions-amd64.tar.bz2 sbin/VBoxService
+	tar --overwrite -C $(@D) -xjf $(@D)/VBoxGuestAdditions-amd64.tar.bz2 bin/VBoxControl
+endef
+
+define VBOX_MOUNT_SERVICE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m755 \
+		$(@D)/sbin/VBoxService \
+		$(TARGET_DIR)/sbin
+	$(INSTALL) -D -m755 \
+		$(@D)/bin/VBoxControl \
+		$(TARGET_DIR)/sbin
+endef
+
+$(eval $(generic-package))

deploy/iso/minikube-iso/arch/x86_64/package/vbox-mount-service/vboxservice.service
@@ -1,0 +1,14 @@
+[Unit]
+Description=VirtualBox Guest Service
+ConditionVirtualization=oracle
+
+[Service]
+ExecStartPre=-/usr/sbin/modprobe vboxguest
+ExecStartPre=-/usr/sbin/modprobe vboxsf
+
+# Normally, VirtualBox only syncs every 20 minutes. This syncs on start, and
+# forces an immediate sync if VM time is over 5 seconds off.
+ExecStart=/usr/sbin/VBoxService -f --disable-automount --timesync-set-start --timesync-set-threshold 5000
+
+[Install]
+WantedBy=multi-user.target

deploy/iso/minikube-iso/configs/minikube_x86_64_defconfig
@@ -49,8 +49,6 @@ BR2_PACKAGE_ACPID=y
 
 # Minikube
 
-config BR2_PACKAGE_VBOX_GUEST=y
-
 BR2_OPTIMIZE_2=y
 BR2_GLOBAL_PATCH_DIR="$(BR2_EXTERNAL_MINIKUBE_PATH)/board/minikube/x86_64/patches"
 BR2_TOOLCHAIN_BUILDROOT_VENDOR="minikube"

deploy/iso/minikube-iso/package/automount/minikube-automount
@@ -179,7 +179,7 @@ mkdir -p /var/lib/boot2docker/etc/
 # VirtualBox Host Mounting
 # - this will bail quickly and gracefully if we're not in VBox
 if modprobe vboxguest &> /dev/null && modprobe vboxsf &> /dev/null; then
-	mountOptions='defaults,iocharset=utf8'
+	mountOptions='defaults'
 	if grep -q '^docker:' /etc/passwd; then
 		mountOptions="${mountOptions},uid=$(id -u docker),gid=$(id -g docker)"
 	fi