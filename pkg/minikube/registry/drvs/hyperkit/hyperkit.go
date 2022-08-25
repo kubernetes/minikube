c7247be4e13f05dad16133286ed720b995fbdc46
Pete Nelson <petiepooo@gmail.com>
2022-08-25 09:05:37 -0400

    remove hyperkit version checks
    Minikube was using an undocumented API. The hyperkit developers
    never promised a stable format for their version string.  In fact,
    the version has changed from date-based to commit-based version
    strings.
    
    Since it has been several releases since the last issue caused by
    older versions of hyperkit, and removal of obsolete code is often
    something to be celebrated, this removes any checks for a minimum
    compatible version of hyperkit.  If hyperkit is installed, it is
    assumed to be a compatible version.
    
    Signed-off-by: Pete Nelson <petiepooo@gmail.com>

--- pkg/minikube/registry/drvs/hyperkit/hyperkit.go
+++ pkg/minikube/registry/drvs/hyperkit/hyperkit.go
@@ -42,11 +42,6 @@ const (
 	docURL = "https://minikube.sigs.k8s.io/docs/reference/drivers/hyperkit/"
 )
 
-var (
-	// minimumVersion is used by hyperkit versionCheck(whether user's hyperkit is older than this)
-	minimumVersion = "0.20190201"
-)
-
 func init() {
 	if err := registry.Register(registry.DriverDef{
 		Name:     driver.HyperKit,
@@ -101,63 +96,5 @@ func status() registry.State {
 		return registry.State{Installed: true, Running: false, Error: fmt.Errorf("%s failed:\n%s", strings.Join(cmd.Args, " "), out), Fix: "Run 'brew install hyperkit'", Doc: docURL}
 	}
 
-	// Split version from v0.YYYYMMDD-HH-xxxxxxx or 0.YYYYMMDD to YYYYMMDD
-	currentVersion := convertVersionToDate(string(out))
-	specificVersion := splitHyperKitVersion(minimumVersion)
-	// If current hyperkit is not newer than minimumVersion, suggest upgrade information
-	isNew, err := isNewerVersion(currentVersion, specificVersion)
-	if err != nil {
-		return registry.State{Installed: true, Running: true, Healthy: true, Error: fmt.Errorf("hyperkit version check failed:\n%v", err), Doc: docURL}
-	}
-	if !isNew {
-		return registry.State{Installed: true, Running: true, Healthy: true, Error: fmt.Errorf("the installed hyperkit version (0.%s) is older than the minimum recommended version (%s)", currentVersion, minimumVersion), Fix: "Run 'brew upgrade hyperkit'", Doc: docURL}
-	}
-
 	return registry.State{Installed: true, Running: true, Healthy: true}
-}
-
-// isNewerVersion checks whether current hyperkit is newer than specific version
-func isNewerVersion(currentVersion string, specificVersion string) (bool, error) {
-	// Convert hyperkit version to time.Date to compare whether hyperkit version is not old.
-	layout := "20060102"
-	currentVersionDate, err := time.Parse(layout, currentVersion)
-	if err != nil {
-		return false, errors.Wrap(err, "parse date")
-	}
-	specificVersionDate, err := time.Parse(layout, specificVersion)
-	if err != nil {
-		return false, errors.Wrap(err, "parse date")
-	}
-	// If currentVersionDate is equal to specificVersionDate, no need to upgrade hyperkit
-	if currentVersionDate.Equal(specificVersionDate) {
-		return true, nil
-	}
-	// If currentVersionDate is after specificVersionDate, return true
-	return currentVersionDate.After(specificVersionDate), nil
-}
-
-// convertVersionToDate returns current hyperkit version
-// hyperkit returns version info with two patterns(v0.YYYYMMDD-HH-xxxxxxx or 0.YYYYMMDD)
-// convertVersionToDate splits version to YYYYMMDD format
-func convertVersionToDate(hyperKitVersionOutput string) string {
-	// `hyperkit -v` returns version info with multi line.
-	// Cut off like "hyperkit: v0.20190201-11-gc0dd46" or "hyperkit: 0.20190201"
-	versionLine := strings.Split(hyperKitVersionOutput, "\n")[0]
-	// Cut off like "v0.20190201-11-gc0dd46" or "0.20190201"
-	version := strings.Split(versionLine, ": ")[1]
-	// Format to "YYYYMMDD"
-	return splitHyperKitVersion(version)
-}
-
-// splitHyperKitVersion splits version from v0.YYYYMMDD-HH-xxxxxxx or 0.YYYYMMDD to YYYYMMDD
-func splitHyperKitVersion(version string) string {
-	if strings.Contains(version, ".") {
-		// Cut off like "20190201-11-gc0dd46" or "20190201"
-		version = strings.Split(version, ".")[1]
-	}
-	if len(version) >= 8 {
-		// Format to "20190201"
-		version = version[:8]
-	}
-	return version
 }
