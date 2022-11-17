ffa45583fbe8819bc0cd8474721a4b9e91034bdc
NimishKashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-11-17 08:11:59 +0530

    chore: deleted generated license files

--- test/integration/licenses/github.com/juju/mutex/export_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-2017 Canonical Ltd.
-// Licensed under the LGPLv3, see LICENCE file for details.
-
-package mutex
-
-var (
-	// Export the Envars so we can test it via mocks
-	Envars = &envars
-)
