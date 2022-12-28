649b8ef5b1137a50dd4c0d24982284b96f965e61
Steven Powell <spowellgrr@gmail.com>
2022-12-28 08:42:01 -0800

    remove experimental markings from QEMU driver

--- pkg/minikube/registry/drvs/qemu2/qemu2.go
+++ pkg/minikube/registry/drvs/qemu2/qemu2.go
@@ -48,7 +48,7 @@ func init() {
 		Config:   configure,
 		Status:   status,
 		Default:  true,
-		Priority: registry.Experimental,
+		Priority: registry.Preferred,
 	}); err != nil {
 		panic(fmt.Sprintf("register failed: %v", err))
 	}
