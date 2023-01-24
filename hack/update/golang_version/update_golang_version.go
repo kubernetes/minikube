45c7854e5fa33a4b465bcc2dd245288eee95f36d
Steven Powell <spowellgrr@gmail.com>
2023-01-24 14:12:18 -0800

    improve automated golang version updating

--- hack/update/golang_version/update_golang_version.go
+++ hack/update/golang_version/update_golang_version.go
@@ -17,8 +17,10 @@ limitations under the License.
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strings"
 
 	"k8s.io/klog/v2"
@@ -138,6 +140,7 @@ var (
 				// searching for 1.* so it does NOT match "KVM_GO_VERSION ?= $(GO_VERSION:.0=)" in the Makefile
 				`GO_VERSION \?= 1.*`:             `GO_VERSION ?= {{.StableVersion}}`,
 				`GO_K8S_VERSION_PREFIX \?= v1.*`: `GO_K8S_VERSION_PREFIX ?= {{.K8SVersion}}`,
+				`GO_VERSION=[0-9.]+`:             `GO_VERSION={{.StableVersion}}`,
 			},
 		},
 		"hack/jenkins/installers/check_install_golang.sh": {
@@ -148,6 +151,16 @@ var (
 		"hack/jenkins/common.ps1": {
 			Replace: map[string]string{
 				`GoVersion = ".*"`: `GoVersion = "{{.StableVersion}}"`,
+			},
+		},
+		"deploy/kicbase/Dockerfile": {
+			Replace: map[string]string{
+				`golang:[0-9.]+`: `golang:{{.StableVersion}}`,
+			},
+		},
+		"deploy/addons/auto-pause/Dockerfile": {
+			Replace: map[string]string{
+				`golang:.*`: `golang:{{.StableVersion}}`,
 			},
 		},
 	}
@@ -176,6 +189,10 @@ func main() {
 	klog.Infof("Golang stable version: %s", data.StableVersion)
 
 	update.Apply(schema, data)
+
+	if err := updateGoHashFile(stable); err != nil {
+		klog.Fatalf("failed to update go hash file: %v", err)
+	}
 }
 
 // goVersions returns Golang stable version.
@@ -197,4 +214,25 @@ func goVersions() (stable, stableMM, k8sVersion string, err error) {
 	mmp := strings.SplitN(stable, ".", 3)
 	stableMM = strings.Join(mmp[0:2], ".") // <major>.<minor> version
 	return stable, stableMM, k8sVersion, nil
+}
+
+func updateGoHashFile(version string) error {
+	r, err := http.Get(fmt.Sprintf("https://dl.google.com/go/go%s.src.tar.gz.sha256", version))
+	if err != nil {
+		return fmt.Errorf("failed to download golang sha256 file: %v", err)
+	}
+	defer r.Body.Close()
+	sha, err := io.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %v", err)
+	}
+	f, err := os.OpenFile("../../../deploy/iso/minikube-iso/go.hash", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open go.hash file: %v", err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(fmt.Sprintf("sha256  %s  go%s.src.tar.gz", sha, version)); err != nil {
+		return fmt.Errorf("failed to write to go.hash file: %v", err)
+	}
+	return nil
 }
