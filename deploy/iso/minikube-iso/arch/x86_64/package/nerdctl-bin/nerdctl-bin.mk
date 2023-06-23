7ac8a65749bdfa65f07a16d9f788b0024ac6ce58
Steven Powell <spowellgrr@gmail.com>
2023-06-23 15:09:04 -0700

    ISO: Add nerdctl

---
+++ deploy/iso/minikube-iso/arch/x86_64/package/nerdctl-bin/nerdctl-bin.mk
@@ -0,0 +1,18 @@
+################################################################################
+# 
+# nerdctl-bin
+# 
+################################################################################
+
+NERDCTL_BIN_VERSION = 1.4.0
+NERDCTL_BIN_COMMIT = 7e8114a82da342cdbec9a518c5c6a1cce58105e9
+NERDCTL_BIN_SITE = https://github.com/containerd/nerdctl/releases/download/v$(NERDCTL_BIN_VERSION)
+NERDCTL_BIN_SOURCE = nerdctl-$(NERDCTL_BIN_AARCH64_VERSION)-linux-amd64.tar.gz
+
+define NERDCTL_BIN_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 0755 \
+                $(@D)/nerdctlctl \
+                $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
