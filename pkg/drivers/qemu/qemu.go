08233d033387502106c7f0fd9db2da79269df036
Raghavendra Talur <raghavendra.talur@gmail.com>
2023-09-08 06:39:55 -0400

    qemu: increase the timeout for detecting the IP address
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>

--- pkg/drivers/qemu/qemu.go
+++ pkg/drivers/qemu/qemu.go
@@ -510,7 +510,7 @@ func (d *Driver) Start() error {
 			return nil
 		}
 		// Implement a retry loop because IP address isn't added to dhcp leases file immediately
-		for i := 0; i < 30; i++ {
+		for i := 0; i < 60; i++ {
 			log.Debugf("Attempt %d", i)
 			err = getIP()
 			if err == nil {
