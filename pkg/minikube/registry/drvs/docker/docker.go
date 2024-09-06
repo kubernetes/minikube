7027336e39572a278b51a37a74434befd4c4853e
Medya Gh <medya@google.com>
2024-09-06 10:41:54 -0700

    Disallow using Docker Desktop 4.34.0 on macos

--- pkg/minikube/registry/drvs/docker/docker.go
+++ pkg/minikube/registry/drvs/docker/docker.go
@@ -191,6 +191,7 @@ var dockerVersionOrState = func() (string, registry.State) {
 }
 
 func checkDockerEngineVersion(o string) registry.State {
+	fmt.Println("**********------------------------------------********", o)
 	parts := strings.SplitN(o, "-", 2)
 	if len(parts) != 2 {
 		return registry.State{
@@ -289,6 +290,17 @@ func checkDockerDesktopVersion(version string) (s registry.State) {
 			Fix:       "Update Docker Desktop to 4.16.1 or greater",
 		}
 	}
+
+	if runtime.GOOS == "darwin" && currSemver.EQ(semver.MustParse("4.34.0")) {
+		return registry.State{
+			Reason:    "PROVIDER_DOCKER_DESKTOP_VERSION_BAD",
+			Running:   true,
+			Error:     errors.New("Docker Desktop 4.34.0 has a regression that prevents minikube from listing the containers"),
+			Installed: true,
+			Fix:       "Use a different Docker desktop version, more info at https://github.com/docker/cli/issues/5412",
+		}
+	}
+
 	return s
 }
 
