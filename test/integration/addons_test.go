1a0ef3c88fd1c73778fa2bf49e7f5849140bc7bd
Ramachandran C <rambits.ai@gmail.com>
2024-10-04 19:56:16 +0530

    Reverting nvidia-gpu-device-plugin to nvidia-device-plugin.
    Reverting nvidia-gpu-device-plugin to nvidia-device-plugin.

--- test/integration/addons_test.go
+++ test/integration/addons_test.go
@@ -951,15 +951,15 @@ func validateDisablingAddonOnNonExistingCluster(ctx context.Context, t *testing.
 	}
 }
 
-// validateNvidiaDevicePlugin tests the nvidia-gpu-device-plugin addon by ensuring the pod comes up and the addon disables
+// validateNvidiaDevicePlugin tests the nvidia-device-plugin addon by ensuring the pod comes up and the addon disables
 func validateNvidiaDevicePlugin(ctx context.Context, t *testing.T, profile string) {
 	defer PostMortemLogs(t, profile)
 
 	if _, err := PodWait(ctx, t, profile, "kube-system", "name=nvidia-device-plugin-ds", Minutes(6)); err != nil {
 		t.Fatalf("failed waiting for nvidia-device-plugin-ds pod: %v", err)
 	}
-	if rr, err := Run(t, exec.CommandContext(ctx, Target(), "addons", "disable", "nvidia-gpu-device-plugin", "-p", profile)); err != nil {
-		t.Errorf("failed to disable nvidia-gpu-device-plugin: args %q : %v", rr.Command(), err)
+	if rr, err := Run(t, exec.CommandContext(ctx, Target(), "addons", "disable", "nvidia-device-plugin", "-p", profile)); err != nil {
+		t.Errorf("failed to disable nvidia-device-plugin: args %q : %v", rr.Command(), err)
 	}
 }
 
