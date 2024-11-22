ad195804f5d17f6d879a029dc20862cdad1ee363
Medya Gh <medya@google.com>
2024-11-22 11:35:15 -0800

    remove kvm arm64 from build and upload script

--- hack/jenkins/release_build_and_upload.sh
+++ hack/jenkins/release_build_and_upload.sh
@@ -61,8 +61,8 @@ env BUILD_IN_DOCKER=y \
   "out/minikube_${DEB_VERSION}-${DEB_REVISION}_armhf.deb" \
   "out/minikube_${DEB_VERSION}-${DEB_REVISION}_ppc64el.deb" \
   "out/minikube_${DEB_VERSION}-${DEB_REVISION}_s390x.deb" \
-  "out/docker-machine-driver-kvm2_${DEB_VERSION}-${DEB_REVISION}_amd64.deb" \
-  "out/docker-machine-driver-kvm2_${DEB_VERSION}-${DEB_REVISION}_arm64.deb"
+  "out/docker-machine-driver-kvm2_${DEB_VERSION}-${DEB_REVISION}_amd64.deb"
+  # "out/docker-machine-driver-kvm2_${DEB_VERSION}-${DEB_REVISION}_arm64.deb"
 
 env BUILD_IN_DOCKER=y
   make \
