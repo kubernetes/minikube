14c679030368174bde9cbd24753f8c141c6e6da0
bugwz <xbugwz@gmail.com>
2025-07-11 22:52:51 +0800

    Fix multi mount error

--- pkg/minikube/node/config.go
+++ pkg/minikube/node/config.go
@@ -77,7 +77,7 @@ func configureMounts(wg *sync.WaitGroup, cc config.ClusterConfig) {
 		return
 	}
 
-	out.Step(style.Mounting, "Creating mount {{.name}} ...", out.V{"name": cc.MountString})
+	out.Step(style.Mounting, "Creating mount {{.name}} ...", out.V{"name": cc.MountStrings})
 	path := os.Args[0]
 	profile := viper.GetString("profile")
 
