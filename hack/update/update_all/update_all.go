7b58e43c48c374ef26b605bf371d9454a046fee6
Medya Ghazizadeh <medya@google.com>
2025-07-31 16:25:10 -0700

    skip kubeadm constant too

--- hack/update/update_all/update_all.go
+++ hack/update/update_all/update_all.go
@@ -55,8 +55,9 @@ func main() {
 			"get_version":                   true,
 			"update_all":                    true,
 			"k8s-lib":                       true,
-			"amd_device_gpu_plugin_version": true, // sem vers issue https://github.com/ROCm/k8s-device-plugin/issues/144
 			"docsy_version":                 true, // this one does not supprt get-dependency-verison
+			"kubeadm_constants":             true, // this one does not supprt get-dependency-verison
+			"amd_device_gpu_plugin_version": true, // sem vers issue https://github.com/ROCm/k8s-device-plugin/issues/144eadm_auto_build
 			"istio_operator_version":        true, // till this is fixed https://github.com/istio/istio/issues/57185
 			"kicbase_version":               true, // This one is not related to auto updating, this is a tool used by kicbae_auto_build
 		}
