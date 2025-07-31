dd0c0443c698a221034cde07c72461d74f35bc67
Medya Ghazizadeh <medya@google.com>
2025-07-31 14:36:45 -0700

    add update all go script

---
+++ hack/update/update_all/update_all.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func getVersion(component string) (string, error) {
+	cmd := exec.Command("go", "run", "update/get_version/get_version.go")
+	cmd.Env = append(os.Environ(), fmt.Sprintf("DEP=%s", component))
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("failed to get version for %s: %v", component, err)
+		return "", err
+	}
+	return strings.TrimSpace(out.String()), nil
+}
+
+func main() {
+	updateDir := "update"
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("starting the update all the current PWD is: ", path)
+
+	dirs, err := os.ReadDir(updateDir)
+	if err != nil {
+		log.Fatalf("failed to read directory %s: %v", updateDir, err)
+	}
+
+	var changes []string
+	i := 0
+	for _, d := range dirs {
+		i++
+		if i > 3 {
+			log.Println("debug - skipping more than 3 components")
+			break
+		}
+		if !d.IsDir() {
+			continue
+		}
+
+		component := d.Name()
+		if component == "get_version" || component == "update_all" || component == "k8s-lib" || component == "amd-gpu-device-plugin-version" {
+			continue
+		}
+
+		fmt.Printf("Processing %s...\n", component)
+
+		oldVersion, err := getVersion(component)
+		if err != nil {
+			log.Fatalf("Could not get old version for %s: %v", component, err)
+		}
+
+		updateCmd := exec.Command("go", "run", filepath.Join(updateDir, component, fmt.Sprintf("%s.go", component)))
+		updateCmd.Stdout = os.Stdout
+		updateCmd.Stderr = os.Stderr
+
+		if err := updateCmd.Run(); err != nil {
+			log.Printf("Failed to update %s: %v", component, err)
+			continue
+		}
+
+		newVersion, err := getVersion(component)
+		if err != nil {
+			log.Printf("Could not get new version for %s: %v", component, err)
+			continue
+		}
+
+		if oldVersion != newVersion {
+			change := fmt.Sprintf("- **%s:** `%s` -> `%s`", component, oldVersion, newVersion)
+			changes = append(changes, change)
+			fmt.Println(change)
+		} else {
+			fmt.Printf("No change for %s.\n", component)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("---")
+	fmt.Printf("Updated components summary:\n%s\n", strings.Join(changes, "\n"))
+
+	// Print a machine-readable summary for GitHub Actions
+	outputChanges := "updates<<EOF\n" + strings.Join(changes, "\n") + "\nEOF"
+	fmt.Println(outputChanges)
+}
