cc08ece78e34404ce91c7d8a86abf0a702241c50
Medya Ghazizadeh <medya@google.com>
2025-09-26 13:26:13 -0700

    update automation update tool for kicbase

--- hack/update/debian_version/debian_version.go
+++ hack/update/debian_version/debian_version.go
@@ -40,25 +40,25 @@ type Data struct {
 	LatestVersion string
 }
 
-func latestJammyTag(tags []string) (string, error) {
+func latestBookwormSlimTag(tags []string) (string, error) {
 	for _, tag := range tags {
-		if strings.Contains(tag, "jammy-") {
+		if strings.Contains(tag, "bookworm-slim") {
 			return tag, nil
 		}
 	}
-	return "", fmt.Errorf("no tag found that matches: jammy-")
+	return "", fmt.Errorf("no tag found that matches: bookworm-slim")
 }
 
 func main() {
-	tags, err := update.ImageTagsFromDockerHub("library/ubuntu")
+	tags, err := update.ImageTagsFromDockerHub("library/debian")
 	if err != nil {
 		klog.Fatal(err)
 	}
-	jammyTag, err := latestJammyTag(tags)
+	tag, err := latestBookwormSlimTag(tags)
 	if err != nil {
 		klog.Fatal(err)
 	}
-	data := Data{LatestVersion: fmt.Sprintf("ubuntu:%s", jammyTag)}
+	data := Data{LatestVersion: fmt.Sprintf("debian:%s", tag)}
 
 	update.Apply(schema, data)
 }
