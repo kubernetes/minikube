c8fd3e6d13446b27fbdc41b7a7ccce2064c5b93b
Medya Ghazizadeh <medya@google.com>
2025-11-19 11:26:19 -0800

    update golang for prow too

--- hack/update/golang_version/golang_version.go
+++ hack/update/golang_version/golang_version.go
@@ -53,6 +53,12 @@ var (
 				`VERSION_TO_INSTALL=.*`: `VERSION_TO_INSTALL={{.StableVersion}}`,
 			},
 		},
+		"hack/prow/installer/check_install_golang.sh": {
+			Replace: map[string]string{
+				`VERSION_TO_INSTALL=.*`: `VERSION_TO_INSTALL={{.StableVersion}}`,
+			},
+		},
+
 		"hack/jenkins/installers/check_install_golang.ps1": {
 			Replace: map[string]string{
 				`GoVersion = ".*"`: `GoVersion = "{{.StableVersion}}"`,
