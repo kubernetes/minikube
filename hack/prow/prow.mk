0a6726201d30930b3641b7f2ef7bce2d41f011c6
Medya Ghazizadeh <medya@google.com>
2025-11-30 12:41:16 -0800

    add makefile

--- hack/prow/prow.mk
+++ hack/prow/prow.mk
@@ -20,7 +20,7 @@ integration-prow-kvm-docker-linux-x86-64: setup-prow-gcp-ssh-keys build-mini-tes
 	./out/minitest  --deployer boskos --tester kvm-docker-linux-amd64-integration --config hack/prow/bosksos-nested.json
 
 .PHONY: integration-prow-kvm-containerd-linux-x86-64
-integration-prow-kvm-containerd-linux-x86-64:
+integration-prow-kvm-containerd-linux-x86-64: setup-prow-gcp-ssh-keys build-mini-test
 	./hack/prow/minikube_cross_build.sh $(GO_VERSION) linux amd64
 	./out/minitest  --deployer boskos --tester kvm-containerd-linux-amd64-integration --config hack/prow/bosksos-nested.json
 
