a167d6e408485ad9c678948b36eb7af78712c867
Michael Adam <obnox@samba.org>
2025-12-04 15:49:35 +0100

    kicbase remove unimplemented image load message for podman.
    minikube start -d podman issued a confusing message that image loading
    is unimplemented for the  podman driver
    
    This change removes this message without changing anythink else in the
    kicbase image loading
    
    Signed-off-by: Michael Adam <obnox@samba.org>

--- pkg/minikube/node/cache.go
+++ pkg/minikube/node/cache.go
@@ -169,9 +169,6 @@ func beginDownloadKicBaseImage(g *errgroup.Group, cc *config.ClusterConfig, down
 				return nil
 			}
 
-			if cc.Driver == driver.Podman {
-				return fmt.Errorf("not yet implemented, see issue #8426")
-			}
 			if driver.IsDocker(cc.Driver) && err == nil {
 				klog.Infof("Loading %s from local cache", img)
 				if finalImg, err = download.CacheToDaemon(img); err == nil {
