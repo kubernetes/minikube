9f075d5bb876ee1fa970befa7955353310f48728
Medya Ghazizadeh <medya@google.com>
2025-12-08 13:15:08 -0800

    ci: bump barmetal functional test timeout to 9m in gha

--- test/integration/iso_test.go
+++ test/integration/iso_test.go
@@ -7,6 +7,39 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
+	//go:build integration
+
+	/*
+	Copyright 2016 The Kubernetes Authors All rights reserved.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+	*/
+
+	package integration
+
+	import (
+		"context"
+		"encoding/json"
+		"fmt"
+		"os/exec"
+		"runtime"
+		"strings"
+		"testing"
+
+		"k8s.io/minikube/pkg/minikube/vmpath"
+	)
+
+	// TestISOImage verifies files and packages installed inside minikube ISO/Base image
 	func TestISOImage(t *testing.T) {
 		if !VMDriver() {
 			t.Skip("This test requires a VM driver")
@@ -110,26 +143,3 @@ You may obtain a copy of the License at
 			}
 		})
 	}
-		t.Logf("Successfully parsed /version.json:")
-		for k, v := range data {
-			t.Logf("  %s: %s", k, v)
-<<<<<<< HEAD
->>>>>>> 2f420fc02 (test: Verify and log iso version in iso_test.go)
-=======
-		}
-	})
-
-	t.Run("eBPFSupport", func(t *testing.T) {
-		// Ensure that BTF type information is available (https://github.com/kubernetes/minikube/issues/21788)
-		btfFile := "/sys/kernel/btf/vmlinux"
-		rr, err := Run(t, exec.CommandContext(ctx, Target(), "-p", profile, "ssh", fmt.Sprintf("test -f %s && echo 'OK' || echo 'NOT FOUND'", btfFile)))
-		if err != nil {
-			t.Errorf("failed to verify existence of %q file: args %q: %v", btfFile, rr.Command(), err)
-		}
-
-		if !strings.Contains(rr.Stdout.String(), "OK") {
-			t.Errorf("expected file %q to exist, but it does not. BTF types are required for CO-RE eBPF programs; set CONFIG_DEBUG_INFO_BTF in kernel configuration.", btfFile)
->>>>>>> fa4d670f7 (run make fmt)
-		}
-	})
-}
