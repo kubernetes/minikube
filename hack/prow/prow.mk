36698002fe4920e47b3becb572efb973c02b1d79
Medya Ghazizadeh <medya@google.com>
2025-12-19 15:09:32 -0800

    feat: Add integration tests for none-containerd on ARM architecture

--- hack/prow/prow.mk
+++ hack/prow/prow.mk
@@ -11,8 +11,9 @@ integration-prow-docker-docker-linux-arm:
 	./hack/prow/minikube_cross_build.sh $(GO_VERSION) linux arm64
 	./hack/prow/util/integration_prow_wrapper.sh ./hack/prow/integration_docker_docker_linux_arm.sh
 
-.PHONY: integration-prow-docker-containerd-linux-arm
-integration-prow-docker-containerd-linux-arm:
+# TODO: rename integration-prow-docker-containerd-linux-arm in upstream test-infra
+.PHONY: integration-prow-docker-containerd-arm
+integration-prow-docker-containerd-arm:
 	./hack/prow/minikube_cross_build.sh $(GO_VERSION) linux arm64
 	./hack/prow/util/integration_prow_wrapper.sh ./hack/prow/integration_docker_containterd_linux_arm.sh
 
