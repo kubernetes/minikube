d6028e961e45ebd05101fa8851c4b4b7fedf6dbd
Nir Soffer <nirsof@gmail.com>
2025-12-20 17:24:39 +0200

    common: Limit waiting for SSH server
    Previously we waited for unlimited time, until minikube start timeout
    expired. At that point minikube delete the cluster as part of retry
    flow, but the wait loop continue to run until the minikube process
    terminated.
    
    We have 2 cases:
    - First start: called after a DHCP lease was created. The host is up and
      has an IP address. The SSH server is accessible in 10-1000
      milliseconds locally, and few seconds in the GitHub macOS runners.
    - Second start: The DHCP lease is found immediately in DHCP leases
      database but the host is not up yet. SSH is accessible in few seconds
      locally, and 2-3 minutes in GitHub macOS runners.
    
    During second start in the CI we see few patterns:
    - Dialing can timeout after 90 seconds
    - Read can block for 20 seconds
    
    We use a deadline to limit dial and read time, and return a timeout
    error if the deadline expired during the wait.
    
    Local - first start:
    
        I1220 22:09:35.992136   41064 main.go:144] libmachine: Waiting until SSH server "192.168.105.6:22" is accessible
        I1220 22:09:35.992143   41064 main.go:144] libmachine: Dialing to SSH server "192.168.105.6:22"
        I1220 22:09:35.993420   41064 main.go:144] libmachine: Reading from SSH server "192.168.105.6:22"
        I1220 22:09:36.001901   41064 main.go:144] libmachine: SSH server "192.168.105.6:22" is accessible in 0.010 seconds
    
    Local - second start:
    
        I1220 22:11:20.658862   41200 main.go:144] libmachine: Waiting until SSH server "192.168.105.6:22" is accessible
        I1220 22:11:20.658865   41200 main.go:144] libmachine: Dialing to SSH server "192.168.105.6:22"
        I1220 22:11:24.020325   41200 main.go:144] libmachine: Reading from SSH server "192.168.105.6:22"
        I1220 22:11:24.029445   41200 main.go:144] libmachine: SSH server "192.168.105.6:22" is accessible in 3.370 seconds
    
    GitHub macOS runner - first start:
    
        I1220 18:06:43.952784   54377 main.go:144] libmachine: Waiting until SSH server "192.168.105.2:22" is accessible
        I1220 18:06:43.952791   54377 main.go:144] libmachine: Dialing to SSH server "192.168.105.2:22"
        I1220 18:06:44.954994   54377 main.go:144] libmachine: Failed to dial: dial tcp 192.168.105.2:22: connect: connection refused
        I1220 18:06:45.955548   54377 main.go:144] libmachine: Dialing to SSH server "192.168.105.2:22"
        I1220 18:06:45.957909   54377 main.go:144] libmachine: Reading from SSH server "192.168.105.2:22"
        I1220 18:06:46.038390   54377 main.go:144] libmachine: SSH server "192.168.105.2:22" is accessible in 2.086 seconds
    
    GitHub macOS runner - second start:
    
        I1220 18:07:33.200093   57832 main.go:144] libmachine: Waiting until SSH server "192.168.105.2:22" is accessible
        I1220 18:07:33.200098   57832 main.go:144] libmachine: Dialing to SSH server "192.168.105.2:22"
        I1220 18:08:48.200668   57832 main.go:144] libmachine: Failed to dial: dial tcp 192.168.105.2:22: connect: operation timed out
        I1220 18:08:49.201499   57832 main.go:144] libmachine: Dialing to SSH server "192.168.105.2:22"
        I1220 18:09:24.207021   57832 main.go:144] libmachine: Reading from SSH server "192.168.105.2:22"
        I1220 18:09:24.229516   57832 main.go:144] libmachine: SSH server "192.168.105.2:22" is accessible in 111.029 seconds

--- pkg/drivers/common/ssh.go
+++ pkg/drivers/common/ssh.go
@@ -17,8 +17,11 @@ limitations under the License.
 package common
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"net"
+	"os"
 	"strconv"
 	"time"
 
@@ -28,9 +31,19 @@ import (
 
 var (
 	retryDelay = time.Second
+
+	// We have 2 cases:
+	// - First start: called after a DHCP lease was created. The host is up and
+	//   has an IP address. The SSH server is accessible in 10-1000
+	//   milliseconds locally, and few seconds in the GitHub macOS runners.
+	// - Second start: The DHCP lease is found immediately in DHCP leases
+	//   database but the host is not up yet. SSH is accessible in few seconds
+	//   locally, and 2-3 minutes in GitHub macOS runners.
+	timeout = 5 * time.Minute
 )
 
-// WaitForSSHAccess waits until remote SSH server is responing.
+// WaitForSSHAccess waits until remote SSH server is responding. Returns an
+// error if the wait timed out or could not be started.
 func WaitForSSHAccess(d drivers.Driver) error {
 	ip, err := d.GetIP()
 	if err != nil {
@@ -41,28 +54,59 @@ func WaitForSSHAccess(d drivers.Driver) error {
 		return err
 	}
 	addr := net.JoinHostPort(ip, strconv.Itoa(port))
-
 	log.Infof("Waiting until SSH server %q is accessible", addr)
+
 	start := time.Now()
+	deadline := start.Add(timeout)
+	dialer := net.Dialer{Deadline: deadline}
 
 	for {
-		log.Debugf("Dialing to SSH server %q", addr)
-		conn, err := net.Dial("tcp", addr)
+		done, err := checkSSHAccess(&dialer, addr)
 		if err != nil {
-			log.Debugf("Failed to dial: %v", err)
-			time.Sleep(retryDelay)
-			continue
+			return err
 		}
-
-		defer conn.Close()
-		log.Debugf("Reading from SSH server %q", addr)
-		if _, err := conn.Read(make([]byte, 1)); err != nil && err != io.EOF {
-			log.Debugf("Failed to read: %v", err)
-			time.Sleep(retryDelay)
-			continue
+		if done {
+			log.Infof("SSH server %q is accessible in %.3f seconds", addr, time.Since(start).Seconds())
+			return nil
 		}
-
-		log.Infof("SSH server %q is accessible in %.3f seconds", addr, time.Since(start).Seconds())
-		return nil
+		time.Sleep(retryDelay)
 	}
+}
+
+// checkSSHAccess performs one check, returning:
+// - (true, nil) we accessed the SSH server
+// - (false, nil) failed to access the server, need to retry again
+// - (false , error) timeout accessing the server, do not retry
+func checkSSHAccess(dialer *net.Dialer, addr string) (bool, error) {
+	if time.Now().After(dialer.Deadline) {
+		return false, fmt.Errorf("timeout waiting for SSH server %q", addr)
+	}
+
+	log.Debugf("Dialing to SSH server %q", addr)
+	conn, err := dialer.Dial("tcp", addr)
+	if err != nil {
+		if errors.Is(err, os.ErrDeadlineExceeded) {
+			return false, fmt.Errorf("timeout dialing to SSH server %q", addr)
+		}
+		log.Debugf("Failed to dial: %v", err)
+		return false, nil
+	}
+
+	defer conn.Close()
+
+	if err := conn.SetReadDeadline(dialer.Deadline); err != nil {
+		log.Debugf("Failed to set timeout: %v", err)
+		return false, nil
+	}
+
+	log.Debugf("Reading from SSH server %q", addr)
+	if _, err := conn.Read(make([]byte, 1)); err != nil && err != io.EOF {
+		if errors.Is(err, os.ErrDeadlineExceeded) {
+			return false, fmt.Errorf("timeout reading from SSH server %q", addr)
+		}
+		log.Debugf("Failed to read: %v", err)
+		return false, nil
+	}
+
+	return true, nil
 }
