f9c23f596f1e6d57e76b5e108bf0136e4752b8f4
Medya Ghazizadeh <medya@google.com>
2025-12-24 16:16:22 -0800

    chore: Quote minikube aliases in .bashrc.sh.

--- .devcontainer/.bashrc.sh
+++ .devcontainer/.bashrc.sh
@@ -15,10 +15,10 @@
 alias k=kubectl
 alias kgp='kubectl get pods -A'
 alias kl='kubectl logs'
-alias m=minikube
-alias ml=minikube profile list
-alias mld=minikube delete --all
-alias mk=/workspaces/minikube/out/minikube
+alias m="minikube"
+alias ml="minikube profile list"
+alias mld="minikube delete --all"
+alias mk="/workspaces/minikube/out/minikube"
 source <(kubectl completion bash)
 complete -o default -F __start_kubectl k
 source <(minikube completion bash)
