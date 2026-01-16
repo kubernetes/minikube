5f9246fb958b2cd54fb718530142ca0429152801
Medya Ghazizadeh <medya.gh@gmail.com>
2026-01-16 21:34:22 +0000

    prow: images build for all archs

--- hack/prow/prow.mk
+++ hack/prow/prow.mk
@@ -69,19 +69,21 @@ setup-prow-gcp-ssh-keys: # set up ssh keys for gcloud cli. These env vars are se
 # ----------------------------------------------------------------
 # Bellow are the image push targets for prow
 # ----------------------------------------------------------------	
+PROW_IMAGE_PLATFORMS ?= linux/amd64,linux/arm64,linux/ppc64le,linux/s390x
+
 .PHONY: push-kubernetes-bootcamp-image-prow
 push-kubernetes-bootcamp-image-prow:
-	docker buildx build --push --platform  linux/amd64,linux/arm64 \
+	docker buildx build --push --platform  $(PROW_IMAGE_PLATFORMS) \
 		-t us-central1-docker.pkg.dev/k8s-staging-images/minikube/kubernetes-bootcamp:$(_GIT_TAG) -t us-central1-docker.pkg.dev/k8s-staging-images/minikube/kubernetes-bootcamp:latest deploy/images/kubernetes-bootcamp
 
 
 .PHONY: push-gvisor-image-prow
 push-gvisor-image-prow:
-	docker buildx build --push --platform  linux/amd64,linux/arm64 \
+	docker buildx build --push --platform  $(PROW_IMAGE_PLATFORMS) \
 		-t us-central1-docker.pkg.dev/k8s-staging-images/minikube/gvisor:$(_GIT_TAG) -t us-central1-docker.pkg.dev/k8s-staging-images/minikube/gvisor:latest -f deploy/images/gvisor/Dockerfile .
 
 .PHONY: push-kube-registry-proxy-image-prow
 push-kube-registry-proxy-image-prow:
-	docker buildx build --push --platform  linux/amd64,linux/arm64 \
+	docker buildx build --push --platform  $(PROW_IMAGE_PLATFORMS) \
 		-t us-central1-docker.pkg.dev/k8s-staging-images/minikube/kube-registry-proxy:$(_GIT_TAG) -t us-central1-docker.pkg.dev/k8s-staging-images/minikube/kube-registry-proxy:latest -f deploy/images/kube-registry-proxy/Dockerfile .
 
