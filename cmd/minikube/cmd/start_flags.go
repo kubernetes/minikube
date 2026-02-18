7a7eef9a0a749045b127e9c2ff6e694dde556f51
Titi Wangsa Damhore <111611+twdamhore@users.noreply.github.com>
2026-02-18 00:08:55 -0800

    Add kubeadm

--- cmd/minikube/cmd/start_flags.go
+++ cmd/minikube/cmd/start_flags.go
@@ -443,7 +443,7 @@ func getExtraOptions() config.ExtraOptionSlice {
 		if loggingFormat != "text" && loggingFormat != "json" {
 			exit.Message(reason.Usage, "--logging-format must be 'text' or 'json', got '{{.format}}'", out.V{"format": loggingFormat})
 		}
-		components := []string{"apiserver", "scheduler", "controller-manager", "kubelet"}
+		components := []string{"scheduler", "kubeadm", "kubelet", "apiserver", "controller-manager"}
 		for _, c := range components {
 			options = append(options, fmt.Sprintf("%s.logging-format=%s", c, loggingFormat))
 		}
