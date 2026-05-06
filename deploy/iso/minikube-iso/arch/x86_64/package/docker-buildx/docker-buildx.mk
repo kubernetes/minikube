977287ce69d9f71e8f494bd4760e723df72ad43b
Medya Ghazizadeh <medya.gh@gmail.com>
2026-05-06 16:23:02 +0000

    ISO: use binaries for docker buildx instead of build from source

--- deploy/iso/minikube-iso/arch/x86_64/package/docker-buildx/docker-buildx.mk
+++ deploy/iso/minikube-iso/arch/x86_64/package/docker-buildx/docker-buildx.mk
@@ -4,43 +4,21 @@
 #   
 ################################################################################
 
-DOCKER_BUILDX_VERSION = v0.30.1
-DOCKER_BUILDX_COMMIT = 9e66234aa13328a5e75b75aa5574e1ca6d6d9c01
-DOCKER_BUILDX_SITE = https://github.com/docker/buildx/archive
-DOCKER_BUILDX_SOURCE = $(DOCKER_BUILDX_VERSION).tar.gz
-DOCKER_BUILDX_GOPATH = $(@D)/_output
-DOCKER_BUILDX_ENV = \
-        $(GO_TARGET_ENV) \
-        CGO_ENABLED=1 \
-        GO111MODULE=on \
-        GOPATH="$(DOCKER_BUILDX_GOPATH)" \
-        GOBIN="$(DOCKER_BUILDX_GOPATH)/bin" \
-        PATH=$(DOCKER_BUILDX_GOPATH)/bin:$(BR_PATH) \
-        GOARCH=amd64 \
-        GOPROXY="https://proxy.golang.org,direct" \
-	GOSUMDB='sum.golang.org'\
-	GOOS=linux
+DOCKER_BUILDX_VERSION = v0.33.0
+DOCKER_BUILDX_SITE = https://github.com/docker/buildx/releases/download/$(DOCKER_BUILDX_VERSION)
+DOCKER_BUILDX_SOURCE = buildx-$(DOCKER_BUILDX_VERSION).linux-amd64
 
-DOCKER_BUILDX_COMPILE_SRC = $(DOCKER_BUILDX_GOPATH)/src/github.com/docker/buildx
-
-define DOCKER_BUILDX_POST_EXTRACT_WORKAROUNDS
-        # Set -buildvcs=false to disable VCS stamping (fails in buildroot)
-        sed -i 's|go build |go build -buildvcs=false |' -i $(@D)/hack/build
+define DOCKER_BUILDX_EXTRACT_CMDS
+	cp $(BR2_DL_DIR)/docker-buildx/$(DOCKER_BUILDX_SOURCE) $(@D)/docker-buildx
 endef
-
-DOCKER_BUILDX_POST_EXTRACT_HOOKS += DOCKER_BUILDX_POST_EXTRACT_WORKAROUNDS
 
 define DOCKER_BUILDX_CONFIGURE_CMDS
         mkdir -p $(TARGET_DIR)/usr/libexec/docker/cli-plugins
 endef
 
-define DOCKER_BUILDX_BUILD_CMDS
-        PWD=$(DOCKER_BUILDX_COMPILE_SRC) $(DOCKER_BUILDX_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) VERSION=$(DOCKER_BUILDX_VERSION) REVISION=$(DOCKER_BUILDX_COMMIT) -C $(@D) build
-endef
-
 define DOCKER_BUILDX_INSTALL_TARGET_CMDS
 	$(INSTALL) -Dm755 \
-                $(@D)/bin/build/docker-buildx \
+                $(@D)/docker-buildx \
                 $(TARGET_DIR)/usr/libexec/docker/cli-plugins/docker-buildx
 endef
 
