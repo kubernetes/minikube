d356948a5b7b2798d824d8090dc11897aeecc040
Nir Soffer <nirsof@gmail.com>
2026-06-12 14:25:41 +0000

    Updating kicbase image to v0.0.50-1781273264-23119
    Signed-off-by: Nir Soffer <nirsof@gmail.com>

--- pkg/drivers/kic/types.go
+++ pkg/drivers/kic/types.go
@@ -24,10 +24,10 @@ import (
 
 const (
 	// Version is the current version of kic
-	Version = "v0.0.50-1780132428-23081"
+	Version = "v0.0.50-1781273264-23119"
 
 	// SHA of the kic base image
-	baseImageSHA = "6b0c380b8b1423d2db7ba784266f582d86626217c1c65353282ff19afdc64764"
+	baseImageSHA = "8af5c89b5430408cb93833f88cd0a4ccae8ccc92704b7dcd3b7f1b61311cea44"
 	// The name of the GCR kicbase repository
 	gcrRepo = "gcr.io/k8s-minikube/kicbase-builds"
 	// The name of the Dockerhub kicbase repository
