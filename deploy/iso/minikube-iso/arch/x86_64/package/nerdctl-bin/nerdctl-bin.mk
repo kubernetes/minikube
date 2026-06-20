203b37648dd1aecd06b940f67a4cdeda17d635bb
Nir Soffer <nirsof@gmail.com>
2026-06-20 09:13:08 +0000

    Kicbase/ISO: Update nerdctl from 2.3.1 to 2.3.3

--- deploy/iso/minikube-iso/arch/x86_64/package/nerdctl-bin/nerdctl-bin.mk
+++ deploy/iso/minikube-iso/arch/x86_64/package/nerdctl-bin/nerdctl-bin.mk
@@ -4,8 +4,8 @@
 # 
 ################################################################################
 
-NERDCTL_BIN_VERSION = 2.3.1
-NERDCTL_BIN_COMMIT = 04f63fe15f3f8d77fe41e56ab7ff1d1f3e312860
+NERDCTL_BIN_VERSION = 2.3.3
+NERDCTL_BIN_COMMIT = b4c5feb4f63eae3e0247c3a354ae11556b9c41ab
 NERDCTL_BIN_SITE = https://github.com/containerd/nerdctl/releases/download/v$(NERDCTL_BIN_VERSION)
 NERDCTL_BIN_SOURCE = nerdctl-$(NERDCTL_BIN_AARCH64_VERSION)-linux-amd64.tar.gz
 NERDCTL_BIN_STRIP_COMPONENTS = 0
