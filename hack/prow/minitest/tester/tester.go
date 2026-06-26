bcd86e43d7ac8397c1b675a92c48cb53199545da
pengqima <pengqima@outlook.com>
2026-06-26 23:38:01 +0800

    chore: fix struct field name in comment
    Signed-off-by: pengqima <pengqima@outlook.com>

--- hack/prow/minitest/tester/tester.go
+++ hack/prow/minitest/tester/tester.go
@@ -29,7 +29,7 @@ type MiniTestRunner interface {
 	Execute(args ...string) error
 	// SyncToRemote copy files from src on host to dst on deployed environment
 	SyncToRemote(src string, dst string, excludedPattern []string) error
-	// SyncToRemote copy files from src on remote to host
+	// SyncToHost copy files from src on remote to host
 	SyncToHost(src string, dst string, excludedPattern []string) error
 }
 
